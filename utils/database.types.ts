64be33fe502f37d93e1cba1a7a215893c927ad15
Austin Chen <akrolsmir@gmail.com>
2023-02-12 16:26:06 -0800

    Add Supabase DB types

---
+++ utils/database.types.ts
@@ -0,0 +1,46 @@
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json }
+  | Json[]
+
+export interface Database {
+  public: {
+    Tables: {
+      posts: {
+        Row: {
+          content: string | null
+          created_at: string
+          id: string
+          title: string | null
+        }
+        Insert: {
+          content?: string | null
+          created_at?: string
+          id?: string
+          title?: string | null
+        }
+        Update: {
+          content?: string | null
+          created_at?: string
+          id?: string
+          title?: string | null
+        }
+      }
+    }
+    Views: {
+      [_ in never]: never
+    }
+    Functions: {
+      [_ in never]: never
+    }
+    Enums: {
+      [_ in never]: never
+    }
+    CompositeTypes: {
+      [_ in never]: never
+    }
+  }
+}
