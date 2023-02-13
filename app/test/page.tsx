ab3f58c2f59199bc3fd6ae8c26e3d16a0179873d
rachelweinberg12 <rachel.weinberg12@gmail.com>
2023-02-12 17:40:21 -0800

    Try using supabase's auth-ui-react

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,11 +1,18 @@
-import { Button } from './button'
+// import { Button } from './button'
 import ServerComponent from './test-server-component'
+import { Login } from '@/components/login'
+import { Auth } from '@supabase/auth-ui-react'
+import { useSupabase } from '@/components/supabase-provider'
 
 export default function Home() {
+  const { supabase } = useSupabase()
+
   return (
     <div className="text-blue-500">
       Hello world!
-      <Button />
+      {/* <Button /> */}
+      <Login />
+      <Auth supabaseClient={supabase} />
       {/* Async components cause a Typescript error, see https://github.com/vercel/next.js/issues/42292 */}
       {/* @ts-expect-error Server Component */}
       <ServerComponent />
