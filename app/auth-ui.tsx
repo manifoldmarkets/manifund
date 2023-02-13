e5d98bd2701bbb0f2d90851e5d4ff19e6db513f6
Austin Chen <akrolsmir@gmail.com>
2023-02-13 10:19:23 -0800

    Add auth theming

--- app/auth-ui.tsx
+++ app/auth-ui.tsx
@@ -1,10 +1,25 @@
 'use client'
 
 import { useSupabase } from '@/components/supabase-provider'
-import { Auth } from '@supabase/auth-ui-react'
+import { Auth, ThemeSupa } from '@supabase/auth-ui-react'
+import { Session } from '@supabase/supabase-js'
+import { useEffect, useState } from 'react'
 
 export default function ClientAuth() {
   const { supabase } = useSupabase()
 
-  return <Auth supabaseClient={supabase} />
+  const [session, setSession] = useState<Session | null>(null)
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data }) => setSession(data?.session))
+  }, [supabase])
+
+  return (
+    <div className="max-w-md bg-dark-200">
+      <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />
+
+      <div className="p-4">
+        <pre>{JSON.stringify(session?.user, null, 2)}</pre>
+      </div>
+    </div>
+  )
 }
