223343074b4175d71073243d421088fc8b14c088
rachelweinberg12 <rachel.weinberg12@gmail.com>
2023-02-12 17:13:33 -0800

    Add post component

--- app/test/test-server-component.tsx
+++ app/test/test-server-component.tsx
@@ -1,6 +1,7 @@
 import 'server-only'
 
 import { createClient } from '@/utils/supabase-server'
+import { Database } from '@/utils/database.types'
 
 // do not cache this page
 export const revalidate = 0
@@ -8,10 +9,24 @@ export const revalidate = 0
 export default async function ServerComponent() {
   const supabase = createClient()
   const { data } = await supabase.from('posts').select('*')
+  const posts = data ? data.map((post) => Post(post)) : <li>no posts</li>
 
   return (
     <>
       <pre>{JSON.stringify({ data }, null, 2)}</pre>
+      <ul>{posts}</ul>
     </>
+  )
+}
+
+type PostProps = Database['public']['Tables']['posts']['Row']
+
+function Post(props: PostProps) {
+  return (
+    <li className="bg-rose-200 p-5 m-5 flex-col gap-5 text-center">
+      <h1 className="text-xl">{props.title}</h1>
+      <div>{props.content}</div>
+      <div>{props.created_at}</div>
+    </li>
   )
 }
