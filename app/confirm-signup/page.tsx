e3de013dd867a7f1fa7a676a3f724f3f70c17bcd
rachelweinberg12 <rachel.weinberg12@gmail.com>
2023-02-14 10:46:44 -0800

    Clean confirm-signup url code

--- app/confirm-signup/page.tsx
+++ app/confirm-signup/page.tsx
@@ -3,18 +3,13 @@ import { useSearchParams } from 'next/navigation'
 
 export default function ConfirmSignup() {
   const confirmation_url = useSearchParams().get('confirmation_url')
-  const type = useSearchParams().get('type')
-  const redirect_to = useSearchParams().get('redirect_to')
-  console.log(confirmation_url)
   if (!confirmation_url) return <div>no confirmation url</div>
+  const href = `${confirmation_url}&type=signup&redirect_to=${useSearchParams().get(
+    'redirect_to'
+  )}`
   return (
     <div className="max-w-md bg-dark-200">
-      <a
-        className="p-5 bg-rose-200"
-        href={
-          confirmation_url + '&type=' + type + '&redirect_to=' + redirect_to
-        }
-      >
+      <a className="p-5 bg-rose-200" href={href}>
         confirmation link
       </a>
     </div>
