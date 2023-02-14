f6a9023288963e48043fa3b6db24f294e2123715
rachelweinberg12 <rachel.weinberg12@gmail.com>
2023-02-14 10:24:12 -0800

    Initialize sql tables

---
+++ seed.sql
@@ -0,0 +1,40 @@
+-- add public profiles table
+create table public.profiles (
+  id uuid not null references auth.users on delete cascade,
+  username text,
+  primary key (id)
+);
+
+-- add RLS policies to profiles
+alter table public.profiles enable row level security;
+
+create policy "Public profiles are viewable by everyone."
+  on profiles for select
+  using ( true );
+
+create policy "Users can insert their own profile."
+  on profiles for insert
+  with check ( auth.uid() = id );
+
+create policy "Users can update own profile."
+  on profiles for update
+  using ( auth.uid() = id );
+
+-- New user trigger
+-- inserts a row into public.profiles
+create function public.handle_new_user()
+returns trigger
+language plpgsql
+security definer set search_path = public
+as $$
+begin
+  insert into public.profiles (id)
+  values (new.id);
+  return new;
+end;
+$$;
+
+-- trigger the function every time a user is created
+create trigger on_auth_user_created
+  after insert on auth.users
+  for each row execute procedure public.handle_new_user();
