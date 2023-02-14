b556fb7d23dada28fe3ae81ef191bc0641e50a07
Austin Chen <akrolsmir@gmail.com>
2023-02-14 11:33:37 -0800

    WIP

--- app/edit-profile/page.tsx
+++ app/edit-profile/page.tsx
@@ -1,41 +1,22 @@
-'use client'
-
-import { useUser } from '@/utils/hooks/use-user'
-import { useSupabase } from '@/components/supabase-provider'
 import { Database } from '@/utils/database.types'
-import { useEffect, useState } from 'react'
 import { SupabaseClient } from '@supabase/supabase-js'
-type Profile = Database['public']['Tables']['profiles']['Row']
+import { GetProfile } from './get-profile'
 
-export default function EditProfile() {
-  const user = useUser()
-  const id = user?.id ?? ''
+export type Profile = Database['public']['Tables']['profiles']['Row']
 
-  const [profile, setProfile] = useState<Profile>({ id, username: null })
-  const { supabase } = useSupabase()
-
-  useEffect(() => {
-    if (id) {
-      getProfile(supabase, id).then((profile) => {
-        setProfile(profile)
-      })
-    }
-  }, [id])
-
-  if (!user) {
-    return <div>you are not logged in</div>
+export default function Page() {
+  {
+    return (
+      <div>
+        <div className="text-blue-400">This is a server component</div>
+        {/* @ts-expect-error Server Component */}
+        <GetProfile />
+      </div>
+    )
   }
-
-  return (
-    <div>
-      this will be the edit profile page
-      {JSON.stringify(user, null, 2)}
-      <EditProfileForm id={id} profile={profile} supabase={supabase} />
-    </div>
-  )
 }
 
-async function getProfile(
+export async function getProfile(
   supabase: SupabaseClient,
   id: string
 ): Promise<{ id: string; username: string | null }> {
@@ -47,38 +28,4 @@ async function getProfile(
     throw error
   }
   return data[0] ? data[0] : { id, username: null }
-}
-
-async function saveProfile(new_profile: Profile, supabase: SupabaseClient) {
-  const { error } = await supabase
-    .from('profiles')
-    .update({ username: new_profile.username })
-    .eq('id', new_profile.id)
-  if (error) {
-    throw error
-  }
-}
-
-function EditProfileForm(props: {
-  id: string
-  profile: Profile
-  supabase: SupabaseClient
-}) {
-  const { id, profile, supabase } = props
-  const [username, setUsername] = useState<string | null>(profile.username)
-
-  return (
-    <form onSubmit={() => saveProfile({ id, username }, supabase)}>
-      <label htmlFor="username">Name</label>
-      <input
-        type="text"
-        id="username"
-        autoComplete="off"
-        required
-        value={username ? username : ''}
-        onChange={(event) => setUsername(event.target.value)}
-      />
-      <button type="submit">Save</button>
-    </form>
-  )
 }
