9eb60f597a8ac33e46a33fed466aa0934d1e076e
rachelweinberg12 <rachel.weinberg12@gmail.com>
2023-02-16 11:36:37 -0800

    Get project data onto project details page

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -1,3 +1,25 @@
-export default function ProjectPage() {
-  return <div className="text-blue-400">To be project page</div>
+import { Database } from '@/utils/database.types'
+import { createClient, getUser } from '@/utils/supabase-server'
+import { SupabaseClient } from '@supabase/supabase-js'
+
+export default async function ProjectPage(props: { params: { slug: string } }) {
+  const { slug } = props.params
+  const project = await getProject(slug)
+  return (
+    <div>
+      {project.title} was made by {project.creator}
+    </div>
+  )
+}
+
+async function getProject(slug: string) {
+  const supabase = createClient()
+  const { data, error } = await supabase
+    .from('projects')
+    .select('*')
+    .eq('slug', slug)
+  if (error) {
+    throw error
+  }
+  return data[0] as Database['public']['Tables']['projects']['Row']
 }
