27bab47fe31b0aae2a2d0e4eb2d2efda1aa47d86
rachelweinberg12 <rachel.weinberg12@gmail.com>
2023-02-15 19:54:23 -0800

    Add create impact cert form

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -1,8 +1,47 @@
-export default function CreateImpactCert() {
+'use client'
+
+import { useSupabase } from '@/components/supabase-provider'
+import { Database } from '@/utils/database.types'
+import { SupabaseClient } from '@supabase/supabase-js'
+import { useState } from 'react'
+
+export type Profile = Database['public']['Tables']['profiles']['Row']
+
+export default function CreateCertForm() {
+  const { supabase, session } = useSupabase()
+  const [title, setTitle] = useState<string>('')
+  const user = session?.user
+
+  if (!user) {
+    return <div>log in to create a cert.</div>
+  }
   return (
-    <div>
-      <h1>Create Impact Certificate</h1>
-      <p>Here you can create an impact certificate.</p>
+    <div className="text-red-500">
+      <label htmlFor="title">Title</label>
+      <input
+        type="text"
+        id="title"
+        autoComplete="off"
+        required
+        value={title ? title : ''}
+        onChange={(event) => setTitle(event.target.value)}
+      />
+      <button type="submit" onClick={() => saveCert(title, user.id, supabase)}>
+        Save
+      </button>
     </div>
   )
+}
+
+async function saveCert(
+  title: string,
+  creator: string,
+  supabase: SupabaseClient
+) {
+  const { error } = await supabase
+    .from('projects')
+    .insert({ title: title, creator: creator })
+  if (error) {
+    throw error
+  }
 }
