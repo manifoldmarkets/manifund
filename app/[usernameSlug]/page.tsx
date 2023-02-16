7fd8ab198e345e95446aad5bce24f5f6f93b6f5d
rachelweinberg12 <rachel.weinberg12@gmail.com>
2023-02-16 14:30:23 -0800

    Add isOwnProfile check to profile page

--- app/[usernameSlug]/page.tsx
+++ app/[usernameSlug]/page.tsx
@@ -1,12 +1,18 @@
-import { createClient } from '@/db/supabase-server'
-import { Profile } from '@/db/profile'
 import { getProfileByUsername } from '@/db/profile'
+import { createClient, getUser } from '@/db/supabase-server'
 
 export default async function UserProfilePage(props: {
   params: { usernameSlug: string }
 }) {
   const { usernameSlug } = props.params
   const supabase = createClient()
-  const user = await getProfileByUsername(supabase, usernameSlug)
-  return <div>{user?.username} profile page</div>
+  const user = await getUser(supabase)
+  const profile = await getProfileByUsername(supabase, usernameSlug)
+  const isOwnProfile = user?.id === profile?.id
+  return (
+    <div>
+      {profile?.username} profile page
+      {isOwnProfile && <div>edit profile</div>}
+    </div>
+  )
 }
