bec28b0d319555012d0c55a683e07730e5a8de6b
rachelweinberg12 <rachel.weinberg12@gmail.com>
2023-02-15 16:53:59 -0800

    Add uuid package

--- app/edit-profile/edit-profile.tsx
+++ app/edit-profile/edit-profile.tsx
@@ -4,6 +4,7 @@ import { useSupabase } from '@/components/supabase-provider'
 import { Database } from '@/utils/database.types'
 import { SupabaseClient } from '@supabase/supabase-js'
 import { useState } from 'react'
+import uuid from 'react-uuid'
 
 export type Profile = Database['public']['Tables']['profiles']['Row']
 
@@ -11,6 +12,7 @@ export function EditProfileForm(props: { profile: Profile }) {
   const { profile } = props
   const { supabase } = useSupabase()
   const [username, setUsername] = useState<string | null>(profile.username)
+  const [avatar, setAvatar] = useState<File | null>(null)
 
   return (
     <div className="text-red-500">
@@ -23,9 +25,21 @@ export function EditProfileForm(props: { profile: Profile }) {
         value={username ? username : ''}
         onChange={(event) => setUsername(event.target.value)}
       />
+      <label htmlFor="avatar">Choose a profile picture:</label>
+      <input
+        type="file"
+        id="avatar"
+        name="avatar"
+        accept="image/png, image/jpeg"
+        onChange={(event) => {
+          setAvatar(event.target.files ? event.target.files[0] : null)
+        }}
+      ></input>
       <button
         type="submit"
-        onClick={() => saveProfile({ id: profile.id, username }, supabase)}
+        onClick={() =>
+          saveProfile({ id: profile.id, username }, avatar, supabase)
+        }
       >
         Save
       </button>
@@ -33,7 +47,12 @@ export function EditProfileForm(props: { profile: Profile }) {
   )
 }
 
-async function saveProfile(new_profile: Profile, supabase: SupabaseClient) {
+async function saveProfile(
+  new_profile: Profile,
+  avatar: File | null,
+  supabase: SupabaseClient
+) {
+  saveAvatar(avatar, new_profile.id, supabase)
   const { error } = await supabase
     .from('profiles')
     .update({ username: new_profile.username })
@@ -41,4 +60,19 @@ async function saveProfile(new_profile: Profile, supabase: SupabaseClient) {
   if (error) {
     throw error
   }
+}
+
+async function saveAvatar(
+  avatar: File | null,
+  id: string,
+  supabase: SupabaseClient
+) {
+  if (!avatar) return
+  const { data, error } = await supabase.storage
+    .from('avatars')
+    .upload(`${id}/avatar`, avatar)
+  if (error) {
+    throw error
+  }
+  console.log(data)
 }
