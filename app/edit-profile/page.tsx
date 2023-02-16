db8bd89b6f360af916145f4a32248b814d332373
Austin Chen <akrolsmir@gmail.com>
2023-02-15 18:23:51 -0800

    Remove unneeded export

--- app/edit-profile/page.tsx
+++ app/edit-profile/page.tsx
@@ -25,7 +25,7 @@ export default async function Page() {
   )
 }
 
-export async function getProfile(
+async function getProfile(
   supabase: SupabaseClient,
   id: string
 ): Promise<{ id: string; username: string | null }> {
