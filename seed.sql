341a736c44807622b3bb83f296a631c059225a67
Austin Chen <akrolsmir@gmail.com>
2023-02-17 20:21:49 -0800

    Add txn table

--- seed.sql
+++ seed.sql
@@ -54,3 +54,19 @@ USING (true)
 -- add RLS policies to avatar bucket
 CREATE POLICY "Give users access to own folder 1oj01fe_0" ON storage.objects FOR SELECT TO public USING (bucket_id = 'avatars' AND auth.uid()::text = (storage.foldername(name))[1]);CREATE POLICY "Allow users to add/change their avatar 1oj01fe_0" ON storage.objects FOR INSERT TO public WITH CHECK (bucket_id = 'avatars' AND auth.uid()::text = (storage.foldername(name))[1]);
 CREATE POLICY "Give users access to own folder 1oj01fe_1" ON storage.objects FOR INSERT TO public WITH CHECK (bucket_id = 'avatars' AND auth.uid()::text = (storage.foldername(name))[1]);
+
+
+---- Txns ----
+create table if not exists public.txns (
+  id uuid not null default gen_random_uuid(),
+  from_id uuid not null references auth.users(id) on delete cascade,
+  to_id uuid not null references auth.users(id) on delete cascade,
+  amount numeric not null,
+  token text not null,
+  created_at timestamp not null default now(),
+  primary key (id)
+);
+alter table public.txns enable row level security;
+-- Allow anyone to read txns
+drop policy if exists "Allow anyone to read txns" on public.txns;
+create policy "Allow anyone to read txns" on public.txns for select using (true);
