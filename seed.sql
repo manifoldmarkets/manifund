668cdfc7c1bc254af4d87cb1ec97b435d2d2fc16
Austin Chen <akrolsmir@gmail.com>
2023-02-19 23:00:34 -0800

    Default username to user's id

--- seed.sql
+++ seed.sql
@@ -2,7 +2,7 @@
 -- add public profiles table
 create table public.profiles (
   id uuid not null references auth.users on delete cascade,
-  username text,
+  username text not null unique,
   primary key (id)
 );
 
@@ -23,19 +23,21 @@ create policy "Users can update own profile."
 
 -- New user trigger
 -- inserts a row into public.profiles
+drop function if exists public.handle_new_user() cascade;
 create function public.handle_new_user()
 returns trigger
 language plpgsql
 security definer set search_path = public
 as $$
 begin
-  insert into public.profiles (id)
-  values (new.id);
+  insert into public.profiles (id, username)
+  values (new.id, new.id);
   return new;
 end;
 $$;
 
 -- trigger the function every time a user is created
+drop trigger if exists on_auth_user_created on auth.users;
 create trigger on_auth_user_created
   after insert on auth.users
   for each row execute procedure public.handle_new_user();
