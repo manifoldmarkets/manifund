0516e9e2f4f192a3223e113f3f08bb9adac657d1
rachelweinberg12 <rachel.weinberg12@gmail.com>
2023-02-20 14:39:19 -0800

    Add advanced settings toggle to create project page

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -9,6 +9,7 @@ import { useRouter } from 'next/navigation'
 import MySlider from '@/components/slider'
 import { TOTAL_SHARES } from '@/db/project'
 import { TextEditor, useTextEditor } from '@/components/editor'
+import clsx from 'clsx'
 
 export type Profile = Database['public']['Tables']['profiles']['Row']
 
@@ -19,6 +20,7 @@ export default function CreateCertForm() {
   const [blurb, setBlurb] = useState<string>('')
   const [minFunding, setMinFunding] = useState<string>('0')
   const [founderPortion, setFounderPortion] = useState<number>(0)
+  const [advancedSettings, setAdvancedSettings] = useState<boolean>(false)
   const editor = useTextEditor()
 
   const user = session?.user
@@ -35,7 +37,35 @@ export default function CreateCertForm() {
     return <div>log in to create a cert.</div>
   }
   return (
-    <div className="flex flex-col gap-3 p-4">
+    <div className="flex flex-col gap-3 p-5">
+      <div className="flex flex-col md:flex-row md:justify-between">
+        <h1 className="text-3xl font-bold">Create a Project Proposal</h1>
+        <div className="mt-2 flex flex-row gap-2">
+          <label htmlFor="advanced-settings" className="text-gray-600">
+            Advanced Settings
+          </label>
+          <button
+            type="button"
+            className={clsx(
+              'relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-offset-2' +
+                (advancedSettings ? ' bg-orange-500' : ' bg-gray-200'),
+              'focus:ring-offset-gray-100'
+            )}
+            role="switch"
+            aria-checked="false"
+            onClick={() => setAdvancedSettings(!advancedSettings)}
+          >
+            <span className="sr-only">Use setting</span>
+            <span
+              aria-hidden="true"
+              className={clsx(
+                'pointer-events-none inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out',
+                advancedSettings ? 'translate-x-5' : 'translate-x-0'
+              )}
+            ></span>
+          </button>
+        </div>
+      </div>
       <label htmlFor="title">Title</label>
       <Input
         type="text"
@@ -66,15 +96,19 @@ export default function CreateCertForm() {
         onChange={(event) => setMinFunding(event.target.value)}
       />
 
-      <label htmlFor="founderPortion">Founder Portion</label>
-      <div className="flex justify-center">
-        <MySlider
-          marks={marks}
-          value={founderPortion ?? 0}
-          onChange={(value) => setFounderPortion(value as number)}
-          step={5}
-        />
-      </div>
+      {advancedSettings && (
+        <>
+          <label htmlFor="founderPortion">Founder Portion</label>
+          <div className="flex justify-center">
+            <MySlider
+              marks={marks}
+              value={founderPortion ?? 0}
+              onChange={(value) => setFounderPortion(value as number)}
+              step={5}
+            />
+          </div>
+        </>
+      )}
       <Button
         type="submit"
         onClick={async () => {
