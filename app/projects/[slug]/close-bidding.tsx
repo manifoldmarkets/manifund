d37f360e5dfc28ab6ee8264957229d068221a5e6
Austin Chen <akrolsmir@gmail.com>
2023-02-19 19:41:54 -0800

    Clean up bid panel

--- app/projects/[slug]/close-bidding.tsx
+++ app/projects/[slug]/close-bidding.tsx
@@ -1,31 +1,32 @@
 //this component is temporary for testing purposes
-"use client"
+'use client'
 import { Button } from '@/components/button'
 import { Database } from '@/db/database.types'
 
 type Project = Database['public']['Tables']['projects']['Row']
-export function CloseBidding(props: {project: Project}){
-    const { project } = props
-    console.log('project', project)
-    return <Button onClick={() => closeBidding(project)}>Close Bidding</Button>
+export function CloseBidding(props: { project: Project }) {
+  const { project } = props
+  return (
+    <Button className="max-w-md" onClick={() => closeBidding(project)}>
+      Close Bidding
+    </Button>
+  )
 }
 
-async function closeBidding(
-    project: Project
-) {
-    const response = await fetch('/api/close-bidding', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        id: project.id,
-        title: project.title,
-        blurb: project.blurb,
-        min_funding: project.min_funding,
-        founder_portion: project.founder_portion,
-      }),
-    })
-    const res = await response.json()
-    return res
+async function closeBidding(project: Project) {
+  const response = await fetch('/api/close-bidding', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      id: project.id,
+      title: project.title,
+      blurb: project.blurb,
+      min_funding: project.min_funding,
+      founder_portion: project.founder_portion,
+    }),
+  })
+  const res = await response.json()
+  return res
 }
