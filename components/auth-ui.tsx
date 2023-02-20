02fdb090b1cfc1f4c38645cb5d238196bd8808ae
Austin Chen <akrolsmir@gmail.com>
2023-02-19 23:12:00 -0800

    Enable Google

--- components/auth-ui.tsx
+++ components/auth-ui.tsx
@@ -23,7 +23,12 @@ export default function ClientAuth() {
           </button>
         </div>
       ) : (
-        <Auth supabaseClient={supabase} appearance={{ theme: manifundTheme }} />
+        <Auth
+          supabaseClient={supabase}
+          appearance={{ theme: manifundTheme }}
+          providers={['google']}
+          // redirectTo={getURL()}
+        />
       )}
     </div>
   )
@@ -40,3 +45,15 @@ const manifundTheme: Theme = {
     fonts: {},
   },
 }
+
+// const getURL = () => {
+//   let url =
+//     process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+//     process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+//     'http://localhost:3000/'
+//   // Make sure to include `https://` when not localhost.
+//   url = url.includes('http') ? url : `https://${url}`
+//   // Make sure to including trailing `/`.
+//   url = url.charAt(url.length - 1) === '/' ? url : `${url}/`
+//   return url
+// }
