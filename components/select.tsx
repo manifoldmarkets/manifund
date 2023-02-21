99eef804d28b14c9d8459762cd54ebf0824ca208
Austin Chen <akrolsmir@gmail.com>
2023-02-20 23:13:18 -0800

    Fix Input theming

--- components/select.tsx
+++ components/select.tsx
@@ -6,7 +6,7 @@ export const Select = (props: JSX.IntrinsicElements['select']) => {
   return (
     <select
       className={clsx(
-        'h-12 cursor-pointer self-start overflow-hidden rounded-md border border-gray-300 pl-4 pr-10 text-sm shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500',
+        'h-12 cursor-pointer self-start overflow-hidden rounded-md border border-gray-300 pl-4 pr-10 text-sm shadow-sm focus:border-orange-500 focus:outline-none focus:ring-orange-500',
         className
       )}
       {...rest}
