f527c9fff49200d09fb379584491187c78fdbfb7
Austin Chen <akrolsmir@gmail.com>
2023-02-20 22:53:40 -0800

    Move getURL to a constants file

--- components/auth-ui.tsx
+++ components/auth-ui.tsx
@@ -4,11 +4,11 @@ import { useSupabase } from '@/db/supabase-provider'
 import { Auth, ThemeSupa } from '@supabase/auth-ui-react'
 import { Theme } from '@supabase/auth-ui-react/dist/esm/src/types'
 import { Button } from '@/components/button'
+import { getURL } from '@/utils/constants'
 
 export default function ClientAuth() {
   const { supabase, session } = useSupabase()
   const user = session?.user
-  console.log('getUrl', getURL())
 
   return (
     <div className="bg-dark-200 max-w-md">
@@ -45,16 +45,4 @@ const manifundTheme: Theme = {
     },
     fonts: {},
   },
-}
-
-const getURL = () => {
-  let url =
-    process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
-    process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
-    'http://localhost:3000/'
-  // Make sure to include `https://` when not localhost.
-  url = url.includes('http') ? url : `https://${url}`
-  // Make sure to including trailing `/`.
-  url = url.charAt(url.length - 1) === '/' ? url : `${url}/`
-  return url
 }
