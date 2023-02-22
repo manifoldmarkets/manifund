15790c7194c9c3d0e99bfa2232e99f395a5bb43f
rachelweinberg12 <rachel.weinberg12@gmail.com>
2023-02-22 12:53:30 -0800

    Add round tag to project card & rearrange data

--- components/project-card.tsx
+++ components/project-card.tsx
@@ -3,26 +3,32 @@ import { Profile } from '@/db/profile'
 import { formatLargeNumber, getValuation, Project } from '@/db/project'
 import Link from 'next/link'
 import { Avatar } from './avatar'
+import { RoundTag } from './round-tag'
 
 export function ProjectCard(props: { project: Project; creator: Profile }) {
   const { creator, project } = props
   console.log('creator', creator)
   return (
     <Link
-      className="rounded-md border border-orange-200 bg-white p-4 shadow hover:cursor-pointer hover:bg-orange-200"
+      className="rounded-md border border-orange-200 bg-white px-4 pb-1 shadow hover:cursor-pointer hover:bg-orange-200"
       href={`projects/${project.slug}`}
     >
-      <h1 className="text-2xl font-bold">{project.title}</h1>
-      <div className="mt-2 flex items-center">
-        <Avatar
-          className="mr-2"
-          username={creator?.username}
-          id={creator?.id}
-          noLink
-          size={'xs'}
-        />
-        <p>{creator?.username}</p>
+      <div className="flex justify-between">
+        <div className="mt-2 flex items-center">
+          <Avatar
+            className="mr-2"
+            username={creator?.username}
+            id={creator?.id}
+            noLink
+            size={'xs'}
+          />
+          <p>{creator?.username}</p>
+        </div>
+        <div className="relative top-2">
+          <RoundTag round={project.round} />
+        </div>
       </div>
+      <h1 className="mt-2 text-xl font-bold">{project.title}</h1>
       <p className="mb-2 font-light text-gray-500">{project.blurb}</p>
       <p>
         Raising ${formatLargeNumber(project.min_funding)} @ $
