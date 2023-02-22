0e39895c538d2670addcc3ca5d7d140423d8c285
rachelweinberg12 <rachel.weinberg12@gmail.com>
2023-02-21 17:05:31 -0800

    Get bids to profile with associated projects and render only if they have proposal bids

--- db/bid.ts
+++ db/bid.ts
@@ -2,14 +2,16 @@ import { Database } from '@/db/database.types'
 import { SupabaseClient, User } from '@supabase/supabase-js'
 
 export type Bid = Database['public']['Tables']['bids']['Row']
+export type Project = Database['public']['Tables']['projects']['Row']
+type BidAndProject = Bid & { projects: Project }
 
 export async function getBidsByUser(supabase: SupabaseClient, user: string) {
   const { data, error } = await supabase
     .from('bids')
-    .select('*')
+    .select('*, projects(*)')
     .eq('bidder', user)
   if (error) {
     throw error
   }
-  return data as Bid[]
+  return data as BidAndProject[]
 }
