5b69bcaa05d8f05404411a4f862c49a6525356c1
rachelweinberg12 <rachel.weinberg12@gmail.com>
2023-02-22 19:47:50 -0800

    Separate active and proposal projects on homepage and conditionally render

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -11,22 +11,45 @@ type ProjectAndCreatorBidsTxns = Project & { profiles: Profile } & {
 
 export default async function Projects() {
   const projects = await listProjects()
-
+  const proposalProjects = projects.filter(
+    (project) => project.stage == 'proposal'
+  )
+  const activeProjects = projects.filter((project) => project.stage == 'active')
   return (
     <div className="bg-dark-200 max-w-4xl">
-      <div className="p-4">
-        <h1 className="text-2xl font-bold">Projects</h1>
-        <div className="mt-2 grid grid-cols-1 gap-4 lg:grid-cols-2">
-          {projects.map((project) => (
-            <ProjectCard
-              key={project.id}
-              project={project}
-              creator={project.profiles}
-              bids={project.bids}
-              txns={project.txns}
-            />
-          ))}
-        </div>
+      <div className="flex flex-col gap-10 p-4">
+        {proposalProjects.length > 0 && (
+          <div>
+            <h1 className="text-2xl font-bold">Project Proposals</h1>
+            <div className="mt-2 grid grid-cols-1 gap-4 lg:grid-cols-2">
+              {proposalProjects.map((project) => (
+                <ProjectCard
+                  key={project.id}
+                  project={project}
+                  creator={project.profiles}
+                  bids={project.bids}
+                  txns={project.txns}
+                />
+              ))}
+            </div>
+          </div>
+        )}
+        {activeProjects.length > 0 && (
+          <div>
+            <h1 className="text-2xl font-bold">Active Projects</h1>
+            <div className="mt-2 grid grid-cols-1 gap-4 lg:grid-cols-2">
+              {activeProjects.map((project) => (
+                <ProjectCard
+                  key={project.id}
+                  project={project}
+                  creator={project.profiles}
+                  bids={project.bids}
+                  txns={project.txns}
+                />
+              ))}
+            </div>
+          </div>
+        )}
       </div>
     </div>
   )
