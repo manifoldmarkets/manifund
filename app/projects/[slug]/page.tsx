971bcaa6c00c0119cbaafd4ad8d61615f1da3890
rachelweinberg12 <rachel.weinberg12@gmail.com>
2023-02-23 10:09:05 -0800

    Add project header to project page

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -5,24 +5,38 @@ import { RichContent } from '@/components/editor'
 import { CloseBidding } from './close-bidding'
 import { EditDescription } from './edit-description'
 import { BidsTable } from '../bids-table'
-import { getProjectBySlug } from '@/db/project'
+import { formatLargeNumber, getValuation } from '@/db/project'
+import { ProjectHeader } from '@/components/project-header'
+import { getFullProjectBySlug } from '@/db/project'
+import { calculateValuation } from '@/utils/math'
 
 export default async function ProjectPage(props: { params: { slug: string } }) {
   const { slug } = props.params
 
   const supabase = createServerClient()
-  const project = await getProjectBySlug(supabase, slug)
-  const creator = await getProfileById(supabase, project.creator)
+  const project = await getFullProjectBySlug(supabase, slug)
   const user = await getUser(supabase)
   const profile = await getProfileById(supabase, user?.id)
 
-  const isOwnProject = user?.id === creator?.id
+  const isOwnProject = user?.id === project.profiles.id
+
+  const valuation =
+    project.stage == 'proposal'
+      ? getValuation(project)
+      : formatLargeNumber(
+          calculateValuation(project.txns, project.founder_portion)
+        )
 
   return (
     <div className="flex flex-col gap-4">
+      <ProjectHeader
+        round={project.round}
+        creator={project.profiles}
+        valuation={valuation}
+      />
       <div>
         <h2 className="text-2xl font-bold">{project.title}</h2>
-        <p className="text-gray-500">by {creator?.username}</p>
+        <p className="text-gray-500">by {project.profiles.username}</p>
       </div>
       {project.description && <RichContent content={project.description} />}
       {isOwnProject && <EditDescription project={project} />}
--- components/project-card.tsx
+++ components/project-card.tsx
@@ -1,4 +1,3 @@
-'use client'
 import { Bid } from '@/db/bid'
 import { Profile } from '@/db/profile'
 import {
@@ -8,15 +7,13 @@ import {
   Project,
 } from '@/db/project'
 import Link from 'next/link'
-import { Avatar } from './avatar'
-import { RoundTag } from './round-tag'
 import {
   EllipsisHorizontalCircleIcon,
   CalendarIcon,
 } from '@heroicons/react/24/solid'
 import { Txn } from '@/db/txn'
-import { ValuationBox } from './valuation-box'
 import { ProjectHeader } from './project-header'
+import { calculateValuation } from '@/utils/math'
 
 export function ProjectCard(props: {
   project: Project
@@ -92,40 +89,4 @@ function FundingProgressBar(props: { percent: number }) {
       ></div>
     </div>
   )
-}
-
-//bad because depends on USD and shares txns being right next to each other?
-function calculateValuation(txns: Txn[], founder_portion: number) {
-  let i = txns.length - 1
-  let price_usd = 0
-  let num_shares = 0
-  while (i > 0) {
-    if (txns[i].project) {
-      if (txns[i].token == 'USD') {
-        price_usd = txns[i].amount
-        if (
-          txns[i - 1].project == txns[i].project &&
-          txns[i - 1].token != 'USD' &&
-          txns[i - 1].from_id == txns[i].to_id &&
-          txns[i - 1].to_id == txns[i].from_id
-        ) {
-          num_shares = txns[i - 1].amount
-          return (price_usd / num_shares) * (10000000 - founder_portion)
-        }
-      } else {
-        num_shares = txns[i].amount
-        if (
-          txns[i - 1].project == txns[i].project &&
-          txns[i - 1].token == 'USD' &&
-          txns[i - 1].from_id == txns[i].to_id &&
-          txns[i - 1].to_id == txns[i].from_id
-        ) {
-          price_usd = txns[i - 1].amount
-          return (price_usd / num_shares) * (10000000 - founder_portion)
-        }
-      }
-    }
-    i--
-  }
-  return -1
 }
