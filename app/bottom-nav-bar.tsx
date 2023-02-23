f7cae9e734f58c8eddb7c513685a1638631ef04d
rachelweinberg12 <rachel.weinberg12@gmail.com>
2023-02-23 15:27:25 -0800

    Fix profile link in bottom nav bar

--- app/bottom-nav-bar.tsx
+++ app/bottom-nav-bar.tsx
@@ -1,3 +1,5 @@
+import { getProfileById, getUser } from '@/db/profile'
+import { createServerClient } from '@/db/supabase-server'
 import { NavBarItem } from './bottom-nav-bar-item'
 
 export const BOTTOM_NAV_BAR_HEIGHT = 58
@@ -8,18 +10,19 @@ export type Item = {
   icon?: React.ComponentType<{ className?: string }>
 }
 
-const signedOutNavigation = () => [
-  { name: 'Home', href: '/' },
-  { name: 'About', href: '/about' },
-  { name: 'Profile', href: '/login' },
-  {
-    name: 'Discord',
-    href: 'https://discord.gg/zPnPtx6jBS',
-  },
-]
-
-export function BottomNavBar() {
-  const navigationOptions = signedOutNavigation()
+export async function BottomNavBar() {
+  const supabase = createServerClient()
+  const user = await getUser(supabase)
+  const profile = await getProfileById(supabase, user?.id)
+  const navigationOptions = [
+    { name: 'Home', href: '/' },
+    { name: 'About', href: '/about' },
+    { name: 'Profile', href: user ? `/${profile?.username}` : '/login' },
+    {
+      name: 'Discord',
+      href: 'https://discord.gg/zPnPtx6jBS',
+    },
+  ]
 
   return (
     <nav className="fixed inset-x-0 bottom-0 z-20 flex select-none items-center justify-between border-t-2 bg-white text-xs text-gray-700 md:hidden">
