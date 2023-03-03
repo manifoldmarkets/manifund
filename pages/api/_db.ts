a868f4308b1c532e9d01c4d31e7a31d98dd8b703
Austin Chen <akrolsmir@gmail.com>
2023-03-02 21:57:17 -0800

    Mark _db.ts as server-only

--- pages/api/_db.ts
+++ pages/api/_db.ts
@@ -1,3 +1,4 @@
+import 'server-only'
 import { NextRequest, NextResponse } from 'next/server'
 import { createClient } from '@supabase/supabase-js'
 import { createMiddlewareSupabaseClient } from '@supabase/auth-helpers-nextjs'
