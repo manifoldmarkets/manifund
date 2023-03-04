0916873ecbba5a1cdaca2687229ba7f4f0add656
Austin Chen <akrolsmir@gmail.com>
2023-03-03 22:08:15 -0800

    Separate out indie projects

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -5,8 +5,11 @@ import { listProjects } from '@/db/project'
 export default async function Projects() {
   const supabase = createServerClient()
   const projects = await listProjects(supabase)
-  const proposalProjects = projects.filter(
-    (project) => project.stage == 'proposal'
+  const proposalProjects = projects
+    .filter((project) => project.stage == 'proposal')
+    .filter((project) => project.round == 'ACX Mini-Grants')
+  const indieProjects = projects.filter(
+    (project) => project.round == 'Indepedent'
   )
   const activeProjects = projects.filter((project) => project.stage == 'active')
   return (
@@ -14,9 +17,27 @@ export default async function Projects() {
       <div className="flex flex-col gap-10 p-4">
         {proposalProjects.length > 0 && (
           <div>
-            <h1 className="text-2xl font-bold">Project Proposals</h1>
+            <h1 className="text-2xl font-bold">
+              ACX Forecasting Mini-grants Round
+            </h1>
             <div className="mt-2 grid grid-cols-1 gap-4 lg:grid-cols-2">
               {proposalProjects.map((project) => (
+                <ProjectCard
+                  key={project.id}
+                  project={project}
+                  creator={project.profiles}
+                  bids={project.bids}
+                  txns={project.txns}
+                />
+              ))}
+            </div>
+          </div>
+        )}
+        {indieProjects.length > 0 && (
+          <div>
+            <h1 className="text-2xl font-bold">Independent Proposals</h1>
+            <div className="mt-2 grid grid-cols-1 gap-4 lg:grid-cols-2">
+              {indieProjects.map((project) => (
                 <ProjectCard
                   key={project.id}
                   project={project}
