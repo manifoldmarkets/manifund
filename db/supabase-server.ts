1e2bd5a5e8d66cf2835e7883d347bbd6f0003740
Austin Chen <akrolsmir@gmail.com>
2023-03-12 17:35:19 -0700

    WIP DevDB

--- db/supabase-server.ts
+++ db/supabase-server.ts
@@ -4,9 +4,12 @@ import { headers, cookies } from 'next/headers'
 import { createServerComponentSupabaseClient } from '@supabase/auth-helpers-nextjs'
 
 import type { Database } from './database.types'
+import { SUPABASE_ANON_KEY, SUPABASE_URL } from './keys'
 
 export const createServerClient = () =>
   createServerComponentSupabaseClient<Database>({
+    supabaseUrl: SUPABASE_URL,
+    supabaseKey: SUPABASE_ANON_KEY,
     headers,
     cookies,
   })
--- pages/api/_db.ts
+++ pages/api/_db.ts
@@ -2,6 +2,7 @@ import { NextRequest, NextResponse } from 'next/server'
 import { createClient } from '@supabase/supabase-js'
 import { createMiddlewareSupabaseClient } from '@supabase/auth-helpers-nextjs'
 import { Database } from '@/db/database.types'
+import { SUPABASE_SERVICE_ROLE_KEY, SUPABASE_URL } from '@/db/keys'
 
 export function createEdgeClient(req: NextRequest) {
   const res = NextResponse.next()
@@ -13,7 +14,7 @@ export function createEdgeClient(req: NextRequest) {
 
 export function createAdminClient() {
   return createClient<Database>(
-    process.env.NEXT_PUBLIC_SUPABASE_URL ?? '',
-    process.env.SUPABASE_SERVICE_ROLE_KEY ?? ''
+    SUPABASE_URL ?? '',
+    SUPABASE_SERVICE_ROLE_KEY ?? ''
   )
 }
