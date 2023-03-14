cc615f89c70d22fd0cce2dc8b55a285ef1f5af3d
rachelweinberg12 <rachel.weinberg12@gmail.com>
2023-03-14 13:46:56 -0700

    Disallow empty & double comment submissions

--- app/projects/[slug]/write-comment.tsx
+++ app/projects/[slug]/write-comment.tsx
@@ -6,30 +6,46 @@ import { Profile } from '@/db/profile'
 import { useRouter } from 'next/navigation'
 import { useSupabase } from '@/db/supabase-provider'
 import { Project } from '@/db/project'
+import { useState } from 'react'
+import { Button, IconButton } from '@/components/button'
 
 export function WriteComment(props: { project: Project; profile: Profile }) {
   const { project, profile } = props
   const { supabase } = useSupabase()
   const editor = useTextEditor('')
   const router = useRouter()
+  const [isSubmitting, setIsSubmitting] = useState(false)
+  console.log('editor', editor?.getText())
 
   return (
     <div className="w-full">
       <TextEditor editor={editor}></TextEditor>
       <div className="flex justify-end">
-        <PaperAirplaneIcon
-          className="m-2 h-8 w-8 text-orange-500 hover:cursor-pointer"
+        <IconButton
+          loading={isSubmitting}
           onClick={async () => {
-            const content = editor?.getJSON()
-            const htmlContent = editor?.getHTML()
-            if (!content || content.length === 0 || !editor || !htmlContent) {
-              return
+            if (editor?.getText()?.trim()) {
+              setIsSubmitting(true)
+              const content = editor?.getJSON()
+              const htmlContent = editor?.getHTML()
+              if (!content || content.length === 0 || !editor || !htmlContent) {
+                return
+              }
+              await sendComment(
+                supabase,
+                content,
+                htmlContent,
+                project,
+                profile
+              )
+              editor.commands.clearContent()
+              setIsSubmitting(false)
+              router.refresh()
             }
-            await sendComment(supabase, content, htmlContent, project, profile)
-            editor.commands.clearContent()
-            router.refresh()
           }}
-        />
+        >
+          <PaperAirplaneIcon className="m-2 h-8 w-8 text-orange-500 hover:cursor-pointer" />
+        </IconButton>
       </div>
     </div>
   )
