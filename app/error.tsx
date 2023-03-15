28779aa8be7568799f81430a0f3b0bb40ed6c44a
Austin Chen <akrolsmir@gmail.com>
2023-03-15 14:46:30 -0700

    Catalog common env errors

--- app/error.tsx
+++ app/error.tsx
@@ -4,22 +4,25 @@ import { SiteLink } from '@/components/site-link'
 import { useSupabase } from '@/db/supabase-provider'
 
 export default function Error(props: { error: Error; reset: () => void }) {
-  const { session } = useSupabase()
-
   return (
     <div>
-      Error thrown: {JSON.stringify(props.error)}
-      <br />
-      Check console logs for more info.
-      {!!session?.user && (
-        <div className="mt-4">
+      <p>Error thrown: {JSON.stringify(props.error)}</p>
+      <p>Check console logs for more info.</p>
+      <div className="mt-10">
+        <h3 className="mb-2 text-xl">Common errors</h3>
+        <code className="font-bold">JWSError JWSInvalidSignature</code>
+        <p>
           This might be due to an auth mismatch between dev and prod Supabase;
           try{' '}
           <SiteLink followsLinkClass href="/login" className="text-orange-500">
             signing out.
           </SiteLink>
-        </div>
-      )}
+        </p>
+        <code className="font-bold">permission denied for schema public</code>
+        <p>Run the permissions granting SQL snippet on the dev db.</p>
+        <code className="font-bold">cache lookup failed for type 38820</code>
+        <p>Restart the dev database</p>
+      </div>
     </div>
   )
 }
