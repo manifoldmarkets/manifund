cdfd38f7625ec82e2409b532df489736372d861a
Austin Chen <akrolsmir@gmail.com>
2023-03-15 11:33:40 -0700

    Don't show error when logged out

--- app/error.tsx
+++ app/error.tsx
@@ -1,17 +1,25 @@
 'use client'
 
 import { SiteLink } from '@/components/site-link'
+import { useSupabase } from '@/db/supabase-provider'
 
 export default function Error(props: { error: Error; reset: () => void }) {
+  const { session } = useSupabase()
+
   return (
     <div>
       Error thrown: {JSON.stringify(props.error)}
       <br />
-      <br />
-      This might be due to an auth mismatch between dev and prod Supabase; try{' '}
-      <SiteLink followsLinkClass href="/login" className="text-orange-500">
-        signing out.
-      </SiteLink>
+      Check console logs for more info.
+      {!!session?.user && (
+        <div className="mt-4">
+          This might be due to an auth mismatch between dev and prod Supabase;
+          try{' '}
+          <SiteLink followsLinkClass href="/login" className="text-orange-500">
+            signing out.
+          </SiteLink>
+        </div>
+      )}
     </div>
   )
 }
