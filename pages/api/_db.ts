b0187f737085ac2f532469a17fe644825499ea05
Austin Chen <akrolsmir@gmail.com>
2023-03-15 14:46:19 -0700

    Add correct supabase keys to edge client

--- pages/api/_db.ts
+++ pages/api/_db.ts
@@ -2,14 +2,24 @@ import { NextRequest, NextResponse } from 'next/server'
 import { createClient } from '@supabase/supabase-js'
 import { createMiddlewareSupabaseClient } from '@supabase/auth-helpers-nextjs'
 import { Database } from '@/db/database.types'
-import { SUPABASE_SERVICE_ROLE_KEY, SUPABASE_URL } from '@/db/env'
+import {
+  SUPABASE_ANON_KEY,
+  SUPABASE_SERVICE_ROLE_KEY,
+  SUPABASE_URL,
+} from '@/db/env'
 
 export function createEdgeClient(req: NextRequest) {
   const res = NextResponse.next()
-  return createMiddlewareSupabaseClient<Database>({
-    req,
-    res,
-  })
+  return createMiddlewareSupabaseClient<Database>(
+    {
+      req,
+      res,
+    },
+    {
+      supabaseUrl: SUPABASE_URL,
+      supabaseKey: SUPABASE_ANON_KEY,
+    }
+  )
 }
 
 export function createAdminClient() {
