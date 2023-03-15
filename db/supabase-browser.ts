532f5cee228a9304db1f435ce8ca81ccba9a6095
Austin Chen <akrolsmir@gmail.com>
2023-03-15 14:59:20 -0700

    Set up a dev database via Snaplet (#16)
    * WIP DevDB
    
    * Set up alternate db via cross-env
    
    * Rename to env.ts
    
    * Show "Devifund" on dev
    
    * Catch errors on a root-level error.tsx
    
    * Add instructions on how to back up prod db
    
    * Add snaplet dep
    
    * Don't show error when logged out
    
    * Add correct supabase keys to edge client
    
    * Catalog common env errors
    
    * Add instructions on how to run the db update process
    
    * Pull latest config from snaplet cloud

--- db/supabase-browser.ts
+++ db/supabase-browser.ts
@@ -1,4 +1,9 @@
 import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
 import { Database } from './database.types'
+import { SUPABASE_ANON_KEY, SUPABASE_URL } from './env'
 
-export const createClient = () => createBrowserSupabaseClient<Database>()
+export const createClient = () =>
+  createBrowserSupabaseClient<Database>({
+    supabaseUrl: SUPABASE_URL,
+    supabaseKey: SUPABASE_ANON_KEY,
+  })
