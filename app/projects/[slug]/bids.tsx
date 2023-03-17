93c99c64c2b1bcea7b599a1b210e569bc9b9c6b9
rachelweinberg12 <rachel.weinberg12@gmail.com>
2023-03-17 11:22:52 -0700

    Move Modal into its own component

--- app/projects/[slug]/bids.tsx
+++ app/projects/[slug]/bids.tsx
@@ -13,6 +13,7 @@ import { Fragment, useRef, useState } from 'react'
 import { MySlider } from '@/components/slider'
 import { Input } from '@/components/input'
 import { useSupabase } from '@/db/supabase-provider'
+import { Modal } from '@/components/modal'
 
 export function Bids(props: {
   bids: BidAndProfile[]
@@ -173,127 +174,88 @@ function Trade(props: {
       >
         {bid.type === 'buy' ? 'Sell' : 'Buy'}
       </Button>
-      <Transition.Root show={open} as={Fragment}>
-        <Dialog
-          as="div"
-          className="relative z-10"
-          initialFocus={cancelButtonRef}
-          onClose={setOpen}
-        >
-          <Transition.Child
-            as={Fragment}
-            enter="ease-out duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
-          </Transition.Child>
-
-          <div className="fixed inset-0 z-10 overflow-y-auto">
-            <div className="flex min-h-full items-center justify-center p-4 text-center sm:p-0">
-              <Transition.Child
-                as={Fragment}
-                enter="ease-out duration-300"
-                enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                enterTo="opacity-100 translate-y-0 sm:scale-100"
-                leave="ease-in duration-200"
-                leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-                leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-              >
-                <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
-                  <div>
-                    <div className="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-orange-100">
-                      <CircleStackIcon
-                        className="h-6 w-6 text-orange-600"
-                        aria-hidden="true"
-                      />
-                    </div>
-                    <div className="mt-3 text-center sm:mt-5">
-                      <Dialog.Title
-                        as="h3"
-                        className="text-base font-semibold leading-6 text-gray-900"
-                      >
-                        Select trade amount
-                      </Dialog.Title>
-                      <div className="mt-2">
-                        <p className="text-sm text-gray-500">
-                          Choose what portion of this offer to accept.
-                        </p>
-                      </div>
-                      <div className="mt-5 flex justify-center">
-                        <div className="flex w-11/12 justify-center gap-2">
-                          <p className="relative top-3">$</p>
-                          <Input
-                            value={tradeAmount}
-                            type="number"
-                            className="w-1/3"
-                            onChange={(event) =>
-                              setTradeAmount(Number(event.target.value))
-                            }
-                          />
-                          <MySlider
-                            marks={marks}
-                            value={(tradeAmount / bid.amount) * 100}
-                            onChange={(value) =>
-                              setTradeAmount(
-                                ((value as number) * bid.amount) / 100
-                              )
-                            }
-                            step={5}
-                          />
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="text-center text-red-500">{errorMessage}</div>
-                  <div className="sm:flex-2 mt-5 flex flex-col gap-3 sm:mt-6 sm:flex-row">
-                    <Button
-                      type="button"
-                      color={'gray'}
-                      className="inline-flex w-full justify-center sm:col-start-1"
-                      onClick={() => setOpen(false)}
-                    >
-                      Cancel
-                    </Button>
-                    <Button
-                      type="button"
-                      className="sm:flex-2 inline-flex w-full justify-center"
-                      loading={isSubmitting}
-                      disabled={errorMessage !== null}
-                      onClick={async () => {
-                        const response = await fetch('/api/trade', {
-                          method: 'POST',
-                          headers: {
-                            'Content-Type': 'application/json',
-                          },
-                          body: JSON.stringify({
-                            oldBidId: bid.id,
-                            usdTraded: tradeAmount,
-                            tradePartnerId: userId,
-                          }),
-                        })
-                        const res = await response.json()
-                        setIsSubmitting(false)
-                        setOpen(false)
-                        router.refresh()
-                      }}
-                    >
-                      {`${
-                        bid.type === 'buy' ? 'Sell' : 'Buy'
-                      } ${formatLargeNumber(
-                        (tradeAmount / bid.valuation) * 100
-                      )}% for ${formatMoney(tradeAmount)}`}
-                    </Button>
-                  </div>
-                </Dialog.Panel>
-              </Transition.Child>
+      <Modal open={open}>
+        <div>
+          <div className="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-orange-100">
+            <CircleStackIcon
+              className="h-6 w-6 text-orange-600"
+              aria-hidden="true"
+            />
+          </div>
+          <div className="mt-3 text-center sm:mt-5">
+            <Dialog.Title
+              as="h3"
+              className="text-base font-semibold leading-6 text-gray-900"
+            >
+              Select trade amount
+            </Dialog.Title>
+            <div className="mt-2">
+              <p className="text-sm text-gray-500">
+                Choose what portion of this offer to accept.
+              </p>
+            </div>
+            <div className="mt-5 flex justify-center">
+              <div className="flex w-11/12 justify-center gap-2">
+                <p className="relative top-3">$</p>
+                <Input
+                  value={tradeAmount}
+                  type="number"
+                  className="w-1/3"
+                  onChange={(event) =>
+                    setTradeAmount(Number(event.target.value))
+                  }
+                />
+                <MySlider
+                  marks={marks}
+                  value={(tradeAmount / bid.amount) * 100}
+                  onChange={(value) =>
+                    setTradeAmount(((value as number) * bid.amount) / 100)
+                  }
+                  step={5}
+                />
+              </div>
             </div>
           </div>
-        </Dialog>
-      </Transition.Root>
+        </div>
+        <div className="text-center text-red-500">{errorMessage}</div>
+        <div className="sm:flex-2 mt-5 flex flex-col gap-3 sm:mt-6 sm:flex-row">
+          <Button
+            type="button"
+            color={'gray'}
+            className="inline-flex w-full justify-center sm:col-start-1"
+            onClick={() => setOpen(false)}
+          >
+            Cancel
+          </Button>
+          <Button
+            type="button"
+            className="sm:flex-2 inline-flex w-full justify-center"
+            loading={isSubmitting}
+            disabled={errorMessage !== null}
+            onClick={async () => {
+              const response = await fetch('/api/trade', {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                  oldBidId: bid.id,
+                  usdTraded: tradeAmount,
+                  tradePartnerId: userId,
+                }),
+              })
+              const res = await response.json()
+              setIsSubmitting(false)
+              setOpen(false)
+              router.refresh()
+            }}
+          >
+            {`${bid.type === 'buy' ? 'Sell' : 'Buy'} ${formatLargeNumber(
+              (tradeAmount / bid.valuation) * 100
+            )}% for ${formatMoney(tradeAmount)}`}
+          </Button>
+        </div>
+      </Modal>
     </div>
   )
 }
