9cfa04e1716dbf57f0b54184672cb3c5637de181
rachelweinberg12 <116405486+rachelweinberg12@users.noreply.github.com>
2023-03-16 20:35:12 -0700

    Add comment replies (#17)
    * Add icon & change comment display layout
    
    * Make reply button functional (show write comment box on click)
    
    * Send comment reply with correct data
    
    * Display comments in threads
    
    * Add CommentWrapper component
    
    * Fix comment spacing
    
    * Create general "sendTemplateEmail"
    
    * Add email to root commenter & add avatar to write comment always
    
    * Add notification email for other commenters in the thread
    
    * Fix thread sorting in comment display
    
    * Add "await" when sending comment email notifs

--- app/projects/[slug]/comments.tsx
+++ app/projects/[slug]/comments.tsx
@@ -8,6 +8,12 @@ import { RichContent } from '@/components/editor'
 import { Avatar } from '@/components/avatar'
 import { Divider } from '@/components/divider'
 import { Project } from '@/db/project'
+import { ArrowUturnRightIcon } from '@heroicons/react/24/outline'
+import { Row } from '@/components/layout/row'
+import { IconButton } from '@/components/button'
+import { ReactNode, useEffect, useState } from 'react'
+import { orderBy, sortBy } from 'lodash'
+import clsx from 'clsx'
 
 export function Comments(props: {
   project: Project
@@ -15,6 +21,13 @@ export function Comments(props: {
   user: Profile | null
 }) {
   const { project, comments, user } = props
+  const [replyingTo, setReplyingTo] = useState<CommentAndProfile | null>(null)
+  const rootComments = comments.filter(
+    (comment) => comment.replying_to === null
+  )
+  const replyComments = comments.filter(
+    (comment) => comment.replying_to !== null
+  )
   if (comments.length === 0 && !user)
     return (
       <p className="text-center italic text-gray-500">
@@ -25,26 +38,38 @@ export function Comments(props: {
         to create one!
       </p>
     )
-  const sortedComments = comments.sort((a, b) =>
-    a.created_at < b.created_at ? 1 : -1
-  )
-  const commentsDisplay = sortedComments.map((comment) => (
-    <div key={comment.id}>
-      <div className="my-6 flex flex-row gap-2">
-        <div>
-          <div className="flex flex-row gap-2">
-            <UserAvatarAndBadge profile={comment.profiles} />
-            <div className="text-gray-500">
-              {formatDistanceToNow(new Date(comment.created_at), {
-                addSuffix: true,
-              })}
+  const threads = genThreads(rootComments, replyComments)
+  const commentsDisplay = threads.map((thread) => (
+    <div key={thread.root.id}>
+      <Row className="w-full">
+        <div className="MT w-full">
+          <CommentWrapper className="mt-2 pb-0">
+            <Comment comment={thread.root} />
+            {user && (
+              <Row className="w-full justify-end">
+                <IconButton onClick={() => setReplyingTo(thread.root)}>
+                  <ArrowUturnRightIcon className="h-5 w-5 rotate-180 text-gray-500 hover:text-gray-700" />
+                </IconButton>
+              </Row>
+            )}
+          </CommentWrapper>
+
+          {thread.replies.map((reply) => (
+            <CommentWrapper key={reply.id} className="ml-8">
+              <Comment comment={reply} />
+            </CommentWrapper>
+          ))}
+          {replyingTo?.id === thread.root.id && user && (
+            <div className="mt-1 ml-8">
+              <WriteComment
+                project={project}
+                commenter={user}
+                replyingToId={thread.root.id}
+              />
             </div>
-          </div>
-          <div className="relative left-8">
-            <RichContent content={comment.content} />
-          </div>
+          )}
         </div>
-      </div>
+      </Row>
     </div>
   ))
   return (
@@ -52,13 +77,69 @@ export function Comments(props: {
       {user && (
         <div>
           <div className="flex gap-3">
-            <Avatar profile={user} />
-            <WriteComment project={project} profile={user} />
+            <WriteComment project={project} commenter={user} />
           </div>
           <Divider />
         </div>
       )}
       {commentsDisplay}
+    </div>
+  )
+}
+
+type Thread = {
+  root: CommentAndProfile
+  replies: CommentAndProfile[]
+}
+function genThreads(
+  rootComments: CommentAndProfile[],
+  replyComments: CommentAndProfile[]
+) {
+  const threads = Object.fromEntries(
+    rootComments.map((comment) => [
+      comment.id,
+      { root: comment, replies: [] } as Thread,
+    ])
+  )
+  replyComments.forEach((reply) => {
+    threads[reply.replying_to ?? 0].replies.push(reply)
+  })
+  const threadsArray = Object.values(threads)
+  threadsArray.forEach((thread) => {
+    thread.replies = sortBy(thread.replies, 'created_at')
+  })
+  return orderBy(threadsArray, 'root.created_at', 'desc')
+}
+
+function Comment(props: { comment: CommentAndProfile }) {
+  const { comment } = props
+  return (
+    <div className="w-full">
+      <Row className="justify-between gap-2">
+        <UserAvatarAndBadge profile={comment.profiles} />
+        <div className="text-sm text-gray-500">
+          {formatDistanceToNow(new Date(comment.created_at), {
+            addSuffix: true,
+          })}
+        </div>
+      </Row>
+      <div className="relative left-8">
+        <RichContent content={comment.content} />
+      </div>
+    </div>
+  )
+}
+
+function CommentWrapper(props: { className?: string; children?: ReactNode }) {
+  const { className, children } = props
+  return (
+    <div
+      className={clsx(
+        className,
+        'my-1 rounded-md border border-gray-300 bg-white p-3'
+      )}
+    >
+      {children}
     </div>
   )
 }
