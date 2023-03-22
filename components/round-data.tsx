58d45b8942c510fee7482e461973d2bf2b254596
rachelweinberg12 <rachel.weinberg12@gmail.com>
2023-03-22 12:28:33 -0700

    Add 3rd data point for active rounds & proposal rounds without auction due date

--- components/round-data.tsx
+++ components/round-data.tsx
@@ -8,13 +8,22 @@ import { Col } from './layout/col'
 
 export function RoundData(props: { round: Round; projects: Project[] }) {
   const { round, projects } = props
-  const closeDate = new Date(`${round.auction_close_date}T23:59:59-12:00`)
+  const auctionCloseDate = new Date(
+    `${round.auction_close_date}T23:59:59-12:00`
+  )
+  const proposalDueDate = new Date(`${round.proposal_due_date}T23:59:59-12:00`)
+  const evalDate = new Date(`${round.eval_date}T23:59:59-12:00`)
   const now = new Date()
-  const daysLeft = dateDiff(now.getTime(), closeDate.getTime())
+  const daysTilAuctionClose = dateDiff(
+    now.getTime(),
+    auctionCloseDate.getTime()
+  )
+  const daysTilProposalsDue = dateDiff(now.getTime(), proposalDueDate.getTime())
+  const daysTilEvals = dateDiff(now.getTime(), evalDate.getTime())
   return (
     <Col className="w-full">
       <div className="flex justify-between">
-        {round.title === 'Independent' || daysLeft > 0 ? (
+        {round.title === 'Independent' || daysTilAuctionClose > 0 ? (
           <DataPoint
             value={projects
               .filter((project) => project.stage === 'proposal')
@@ -23,7 +32,16 @@ export function RoundData(props: { round: Round; projects: Project[] }) {
             theme={getRoundTheme(round.title)}
           />
         ) : null}
-        {round.title === 'Independent' || daysLeft < 0 ? (
+        {round.title === 'Independent' && (
+          <DataPoint
+            value={projects
+              .filter((project) => project.stage === 'not funded')
+              .length.toString()}
+            label="unfunded projects"
+            theme={getRoundTheme(round.title)}
+          />
+        )}
+        {round.title === 'Independent' || daysTilAuctionClose < 0 ? (
           <DataPoint
             value={projects
               .filter((project) => project.stage === 'active')
@@ -39,13 +57,27 @@ export function RoundData(props: { round: Round; projects: Project[] }) {
             theme={getRoundTheme(round.title)}
           />
         )}
-        {daysLeft > 0 && (
+        {daysTilAuctionClose > 0 && round.auction_close_date !== null ? (
           <DataPoint
-            value={showPrecision(daysLeft, 3)}
+            value={showPrecision(daysTilAuctionClose, 3)}
             label="days left to bid"
             theme={getRoundTheme(round.title)}
           />
-        )}
+        ) : null}
+        {daysTilProposalsDue > 0 && round.proposal_due_date !== null ? (
+          <DataPoint
+            value={showPrecision(daysTilProposalsDue, 3)}
+            label="days left to submit proposals"
+            theme={getRoundTheme(round.title)}
+          />
+        ) : null}
+        {daysTilAuctionClose < 0 && daysTilEvals > 0 ? (
+          <DataPoint
+            value={showPrecision(daysTilEvals, 3)}
+            label="days until close"
+            theme={getRoundTheme(round.title)}
+          />
+        ) : null}
       </div>
     </Col>
   )
--- db/database.types.ts
+++ db/database.types.ts
@@ -144,6 +144,7 @@ export interface Database {
         Row: {
           auction_close_date: string | null
           description: Json | null
+          eval_date: string | null
           header_image_url: string | null
           proposal_due_date: string | null
           retro_pool: number | null
@@ -153,6 +154,7 @@ export interface Database {
         Insert: {
           auction_close_date?: string | null
           description?: Json | null
+          eval_date?: string | null
           header_image_url?: string | null
           proposal_due_date?: string | null
           retro_pool?: number | null
@@ -162,6 +164,7 @@ export interface Database {
         Update: {
           auction_close_date?: string | null
           description?: Json | null
+          eval_date?: string | null
           header_image_url?: string | null
           proposal_due_date?: string | null
           retro_pool?: number | null
