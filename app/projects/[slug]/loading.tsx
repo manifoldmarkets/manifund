a1502cfd3a4c14083e88f530f9b214b971bb6a86
Austin Chen <akrolsmir@gmail.com>
2023-03-23 21:19:09 -0700

    Improve loading skeleton

--- app/projects/[slug]/loading.tsx
+++ app/projects/[slug]/loading.tsx
@@ -1,3 +1,19 @@
 export default function Loading() {
-  return <div>Loading...</div>
+  return (
+    <div>
+      <div className="flex animate-pulse space-x-4">
+        <div className="h-10 w-10 rounded-full bg-slate-200"></div>
+        <div className="flex-1 space-y-6 py-1">
+          <div className="h-2 rounded bg-slate-200"></div>
+          <div className="space-y-3">
+            <div className="grid grid-cols-3 gap-4">
+              <div className="col-span-2 h-2 rounded bg-slate-200"></div>
+              <div className="col-span-1 h-2 rounded bg-slate-200"></div>
+            </div>
+            <div className="h-2 rounded bg-slate-200"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
