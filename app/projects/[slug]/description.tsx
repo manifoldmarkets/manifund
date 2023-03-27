5053bf35c85f735d69c5cb00031058d2859bf780
rachelweinberg12 <rachel.weinberg12@gmail.com>
2023-03-27 16:43:48 -0700

    :lipstick: expandable project description

---
+++ app/projects/[slug]/description.tsx
@@ -0,0 +1,27 @@
+'use client'
+import clsx from 'clsx'
+import { ReactNode, useState } from 'react'
+import { ChevronUpIcon, ChevronDownIcon } from '@heroicons/react/24/outline'
+import { Row } from '@/components/layout/row'
+
+export function Description(props: { children: ReactNode }) {
+  const { children } = props
+  const [expanded, setExpanded] = useState(false)
+  return (
+    <div className="text-gray-500">
+      <div className={clsx(expanded ? 'line-clamp-none' : 'line-clamp-[20]')}>
+        {children}
+      </div>
+      <Row className="mt-3 justify-center text-sm">
+        <button onClick={() => setExpanded(!expanded)} className="flex">
+          See {expanded ? 'less' : 'more'}
+          {expanded ? (
+            <ChevronUpIcon className="relative top-1 h-4 w-4 " />
+          ) : (
+            <ChevronDownIcon className="relative top-1 h-4 w-4" />
+          )}
+        </button>
+      </Row>
+    </div>
+  )
+}
