ed606192ca7a741f0dd9d5bd8bdc8f9259580822
rachelweinberg12 <rachel.weinberg12@gmail.com>
2023-03-30 17:37:57 -0700

    :bug: stale data on home & round pages

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -4,6 +4,8 @@ import { AllProjectsDisplay } from './all-projects-display'
 import { getRounds } from '@/db/round'
 import { AllRoundsDisplay } from './all-rounds-display'
 
+export const revalidate = 0
+
 export default async function Projects() {
   const supabase = createServerClient()
   const projects = await listProjects(supabase)
