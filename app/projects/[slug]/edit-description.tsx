6518f90af04e600c68073c2516b65145c7e1ea82
rachelweinberg12 <rachel.weinberg12@gmail.com>
2023-04-04 12:32:06 -0700

    :lipstick: edit description button to icon button and allow subtitle edits

--- app/projects/[slug]/edit-description.tsx
+++ app/projects/[slug]/edit-description.tsx
@@ -1,16 +1,21 @@
 'use client'
 
-import { Button } from '@/components/button'
+import { Button, IconButton } from '@/components/button'
 import { useSupabase } from '@/db/supabase-provider'
 import { useTextEditor, TextEditor } from '@/components/editor'
 import { Project } from '@/db/project'
 import { useState } from 'react'
+import { PencilIcon } from '@heroicons/react/24/outline'
+import { Row } from '@/components/layout/row'
+import { Tooltip } from '@/components/tooltip'
+import { Input } from '@/components/input'
 
 export function EditDescription(props: { project: Project }) {
   const { project } = props
   const { supabase, session } = useSupabase()
   const user = session?.user
   const [showEditor, setShowEditor] = useState(false)
+  const [subtitle, setSubtitle] = useState(project.blurb ?? '')
   const [saving, setSaving] = useState(false)
   const editor = useTextEditor(project.description)
 
@@ -26,6 +31,7 @@ export function EditDescription(props: { project: Project }) {
       .from('projects')
       .update({
         description: content,
+        blurb: subtitle,
       })
       .eq('id', project.id)
     if (error) {
@@ -41,22 +47,37 @@ export function EditDescription(props: { project: Project }) {
       {showEditor ? (
         <div>
           <h3 className="mb-2 text-xl font-bold text-gray-500">
-            Edit project description
+            Project subtitle:
+          </h3>
+          <Input
+            className="mb-2 w-full"
+            value={subtitle}
+            onChange={(event) => setSubtitle(event.target.value)}
+          />
+
+          <h3 className="mb-2 text-xl font-bold text-gray-500">
+            Project description:
           </h3>
           <TextEditor editor={editor} />
-          <div className="mt-3 flex flex-row gap-2">
+          <Row className="mt-3 justify-center gap-2">
             <Button onClick={saveText} disabled={saving} loading={saving}>
               Save
             </Button>
             <Button color="gray" onClick={() => setShowEditor(false)}>
               Cancel
             </Button>
-          </div>
+          </Row>
         </div>
       ) : (
-        <Button size="sm" color="gray" onClick={() => setShowEditor(true)}>
-          Edit project description
-        </Button>
+        <Row className=" justify-end">
+          <IconButton size="sm" onClick={() => setShowEditor(true)}>
+            <Tooltip text="Edit project description">
+              <div className="h-10 w-10 rounded-full bg-orange-500 hover:bg-orange-600">
+                <PencilIcon className="h-10 w-10 p-2 text-white" aria-hidden />
+              </div>
+            </Tooltip>
+          </IconButton>
+        </Row>
       )}
     </div>
   )
