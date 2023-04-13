0e424b28ee27c4ecc300f08a1ead5a82cb9204a8
rachelweinberg12 <rachel.weinberg12@gmail.com>
2023-04-13 12:02:32 -0700

    :bug: withdrawable balance calculation

--- app/[usernameSlug]/page.tsx
+++ app/[usernameSlug]/page.tsx
@@ -2,11 +2,13 @@ import { getProfileByUsername, getUser, Profile } from '@/db/profile'
 import { createServerClient } from '@/db/supabase-server'
 import { ProfileHeader } from './profile-header'
 import { SignOutButton } from './sign-out-button'
-import { getTxnAndProjectsByUser, TxnAndProject } from '@/db/txn'
+import { getTxnAndProjectsByUser, Txn, TxnAndProject } from '@/db/txn'
 import { Bid, getBidsByUser } from '@/db/bid'
 import { getProjectsByUser, Project } from '@/db/project'
 import { ProfileTabs } from './profile-tabs'
 import { calculateUserBalance } from '@/utils/math'
+import { BANK_ID } from '@/db/env'
+import { sortBy } from 'lodash'
 
 export const revalidate = 0
 
@@ -33,7 +35,7 @@ export default async function UserProfilePage(props: {
   const investments = await compileInvestments(txns, profile.id)
   const balance = calculateUserBalance(txns, profile.id)
   const withdrawBalance = calculateWithdrawBalance(
-    investments,
+    txns,
     bids,
     profile.id,
     balance,
@@ -101,28 +103,113 @@ async function compileInvestments(txns: TxnAndProject[], userId: string) {
 }
 
 function calculateWithdrawBalance(
-  investments: Investment[],
+  txns: Txn[],
   bids: Bid[],
   userId: string,
   balance: number,
-  accredited: boolean
+  accreditationStatus: boolean
 ) {
-  let withdrawableAmount = accredited ? balance : 0
-  if (!accredited) {
-    investments.forEach((investment) => {
-      if (investment.project?.creator === userId) {
-        withdrawableAmount += investment.price_usd
+  let nonWithdrawBalance = 0
+  const sortedTxns = sortBy(txns, 'created_at')
+  sortedTxns.forEach((txn) => {
+    const txnType = categorizeTxn(txn, userId)
+    if (txnType === 'non-dollar') return
+    if (txn.to_id === userId) {
+      if (!txnWithdrawable(txnType, accreditationStatus)) {
+        nonWithdrawBalance += txn.amount
       }
-    })
-  }
-  let availableNonWithdrawAmount = balance - withdrawableAmount
+    } else {
+      if (txnCharitable(txnType, accreditationStatus)) {
+        nonWithdrawBalance -= txn.amount
+      }
+    }
+    nonWithdrawBalance = Math.max(nonWithdrawBalance, 0)
+  })
   bids.forEach((bid) => {
     if (bid.status === 'pending' && bid.type === 'buy') {
-      availableNonWithdrawAmount -= bid.amount
+      nonWithdrawBalance += bid.amount
     }
   })
-  if (availableNonWithdrawAmount < 0) {
-    withdrawableAmount += availableNonWithdrawAmount
+
+  return Math.max(balance - nonWithdrawBalance, 0)
+}
+
+type TxnType =
+  | 'incoming cash transfer'
+  | 'outgoing cash transfer'
+  | 'share purchase'
+  | 'share sale'
+  | 'own-project share sale'
+  | 'withdraw'
+  | 'deposit'
+  | 'incoming project donation'
+  | 'outgoing project donation'
+  | 'non-dollar'
+
+function categorizeTxn(txn: TxnAndProject, userId: string) {
+  if (txn.token === 'USD') {
+    if (txn.to_id === userId) {
+      if (txn.project) {
+        if (txn.projects?.creator === userId) {
+          if (txn.bundle) {
+            return 'own-project share sale'
+          } else {
+            return 'incoming project donation'
+          }
+        } else {
+          return 'share sale'
+        }
+      } else {
+        if (txn.from_id === BANK_ID) {
+          return 'deposit'
+        } else {
+          return 'incoming cash transfer'
+        }
+      }
+    } else {
+      if (txn.project) {
+        if (txn.bundle) {
+          return 'share purchase'
+        } else {
+          return 'outgoing project donation'
+        }
+      } else {
+        if (txn.to_id === BANK_ID) {
+          return 'withdraw'
+        } else {
+          return 'outgoing cash transfer'
+        }
+      }
+    }
+  } else {
+    return 'non-dollar'
   }
-  return withdrawableAmount > 0 ? withdrawableAmount : 0
+}
+
+// For incoming txns only
+function txnWithdrawable(txnType: TxnType, accreditationStatus: boolean) {
+  if (txnType === 'incoming cash transfer') {
+    return false
+  }
+  if (
+    txnType === 'share sale' ||
+    (txnType === 'deposit' && !accreditationStatus)
+  ) {
+    return false
+  }
+  return true
+}
+
+// For outgoing txns only
+function txnCharitable(txnType: TxnType, accreditationStatus: boolean) {
+  if (
+    txnType === 'outgoing cash transfer' ||
+    txnType === 'outgoing project donation'
+  ) {
+    return true
+  }
+  if (txnType === 'share purchase' && !accreditationStatus) {
+    return true
+  }
+  return false
 }
