5a26c24bf7e71b3813770025980c522f52db3ddc
rachelweinberg12 <rachel.weinberg12@gmail.com>
2023-04-18 16:32:34 -0700

    :construction: add combobox for user select

--- app/create-grant/create-grant-form.tsx
+++ app/create-grant/create-grant-form.tsx
@@ -2,8 +2,13 @@
 import { TextEditor, useTextEditor } from '@/components/editor'
 import { Input } from '@/components/input'
 import { Col } from '@/components/layout/col'
-import { useState } from 'react'
+import { forwardRef, Fragment, Ref, useState } from 'react'
 import Link from 'next/link'
+import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/24/outline'
+import { Combobox, Listbox, Transition } from '@headlessui/react'
+import clsx from 'clsx'
+import { useUser } from '@supabase/auth-ui-react/dist/esm/src/components/Auth/UserContext'
+import { MiniProfile } from '@/db/profile'
 
 const DEFAULT_DESCRIPTION = `
 <h3>What will the recipient use this funding for?</h3>
@@ -23,12 +28,36 @@ const DEFAULT_DESCRIPTION = `
 </br>
 `
 
+const people = [
+  { name: 'Wade Cooper', username: '@wadecooper' },
+  { name: 'Arlene Mccoy', username: '@arlenemccoy' },
+  { name: 'Devon Webb', username: '@devonwebb' },
+  { name: 'Tom Cook', username: '@tomcook' },
+  { name: 'Tanya Fox', username: '@tanyafox' },
+  { name: 'Hellen Schmidt', username: '@hellenschmidt' },
+  { name: 'Caroline Schultz', username: '@carolineschultz' },
+  { name: 'Mason Heaney', username: '@masonheaney' },
+  { name: 'Claudie Smitham', username: '@claudiesmitham' },
+  { name: 'Emil Schaefer', username: '@emilschaefer' },
+]
+
 export function CreateGrantForm(props: { profiles: MiniProfile[] }) {
   const { profiles } = props
+  const [query, setQuery] = useState('')
+  const filteredProfiles =
+    query === ''
+      ? profiles
+      : profiles.filter((profile) => {
+          return (
+            profile.full_name.toLowerCase().includes(query.toLowerCase()) ||
+            profile.username.toLowerCase().includes(query.toLowerCase())
+          )
+        })
+
   const user = useUser()
   const [recipientFullName, setRecipientFullName] = useState('')
   const [recipientOnManifund, setRecipientOnManifund] = useState(true)
-  const [recipientUsername, setRecipientUsername] = useState('')
+  const [recipient, setRecipient] = useState(profiles[0])
   const [recipientEmail, setRecipientEmail] = useState('')
   const [title, setTitle] = useState('')
   const [subtitle, setSubtitle] = useState('')
@@ -57,11 +86,75 @@ export function CreateGrantForm(props: { profiles: MiniProfile[] }) {
       {recipientOnManifund && (
         <>
           <label>Recipient username</label>
-          <Input
-            type="text"
-            value={recipientUsername}
-            onChange={(event) => setRecipientUsername(event.target.value)}
-          />
+          <Combobox as="div" value={recipient} onChange={setRecipient}>
+            <div className="relative mt-2">
+              <Combobox.Input
+                className="w-full rounded-md border-0 bg-white py-1.5 pl-3 pr-12 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-orange-500 sm:text-sm sm:leading-6"
+                onChange={(event) => setQuery(event.target.value)}
+                displayValue={(profile: MiniProfile) => profile.full_name}
+              />
+              <Combobox.Button className="absolute inset-y-0 right-0 flex items-center rounded-r-md px-2 focus:outline-none">
+                <ChevronUpDownIcon
+                  className="h-5 w-5 text-gray-400"
+                  aria-hidden="true"
+                />
+              </Combobox.Button>
+
+              {filteredProfiles.length > 0 && (
+                <Combobox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                  {filteredProfiles.map((profile) => (
+                    <Combobox.Option
+                      key={profile.username}
+                      value={profile}
+                      className={({ active }) =>
+                        clsx(
+                          'relative cursor-default select-none py-2 pl-3 pr-9',
+                          active ? 'bg-orange-500 text-white' : 'text-gray-900'
+                        )
+                      }
+                    >
+                      {({ active, selected }) => (
+                        <>
+                          <div className="flex">
+                            <span
+                              className={clsx(
+                                'truncate',
+                                selected && 'font-semibold'
+                              )}
+                            >
+                              {profile.full_name}
+                            </span>
+                            <span
+                              className={clsx(
+                                'ml-2 truncate text-gray-500',
+                                active ? 'text-orange-100' : 'text-gray-500'
+                              )}
+                            >
+                              {profile.username}
+                            </span>
+                          </div>
+
+                          {selected && (
+                            <span
+                              className={clsx(
+                                'absolute inset-y-0 right-0 flex items-center pr-4',
+                                active ? 'text-white' : 'text-orange-500'
+                              )}
+                            >
+                              <CheckIcon
+                                className="h-5 w-5"
+                                aria-hidden="true"
+                              />
+                            </span>
+                          )}
+                        </>
+                      )}
+                    </Combobox.Option>
+                  ))}
+                </Combobox.Options>
+              )}
+            </div>
+          </Combobox>
         </>
       )}
 
@@ -111,115 +204,5 @@ export function CreateGrantForm(props: { profiles: MiniProfile[] }) {
         </div>
       </div>
     </Col>
-  )
-}
-
-import { Fragment } from 'react'
-import { Listbox, Transition } from '@headlessui/react'
-import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid'
-import clsx from 'clsx'
-import { MiniProfile } from '@/db/profile'
-import { User } from '@supabase/supabase-js'
-import { useUser } from '@supabase/auth-ui-react/dist/esm/src/components/Auth/UserContext'
-
-const people = [
-  { name: 'Wade Cooper', username: '@wadecooper' },
-  { name: 'Arlene Mccoy', username: '@arlenemccoy' },
-  { name: 'Devon Webb', username: '@devonwebb' },
-  { name: 'Tom Cook', username: '@tomcook' },
-  { name: 'Tanya Fox', username: '@tanyafox' },
-  { name: 'Hellen Schmidt', username: '@hellenschmidt' },
-  { name: 'Caroline Schultz', username: '@carolineschultz' },
-  { name: 'Mason Heaney', username: '@masonheaney' },
-  { name: 'Claudie Smitham', username: '@claudiesmitham' },
-  { name: 'Emil Schaefer', username: '@emilschaefer' },
-]
-
-export function Example() {
-  const [selected, setSelected] = useState(people[3])
-
-  return (
-    <Listbox value={selected} onChange={setSelected}>
-      {({ open }) => (
-        <>
-          <Listbox.Label className="block text-sm font-medium leading-6 text-gray-900">
-            Assigned to
-          </Listbox.Label>
-          <div className="relative mt-2">
-            <Listbox.Button className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6">
-              <span className="inline-flex w-full truncate">
-                <span className="truncate">{selected.name}</span>
-                <span className="ml-2 truncate text-gray-500">
-                  {selected.username}
-                </span>
-              </span>
-              <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-                <ChevronUpDownIcon
-                  className="h-5 w-5 text-gray-400"
-                  aria-hidden="true"
-                />
-              </span>
-            </Listbox.Button>
-
-            <Transition
-              show={open}
-              as={Fragment}
-              leave="transition ease-in duration-100"
-              leaveFrom="opacity-100"
-              leaveTo="opacity-0"
-            >
-              <Listbox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                {people.map((person) => (
-                  <Listbox.Option
-                    key={person.username}
-                    className={({ active }) =>
-                      clsx(
-                        active ? 'bg-indigo-600 text-white' : 'text-gray-900',
-                        'relative cursor-default select-none py-2 pl-3 pr-9'
-                      )
-                    }
-                    value={person}
-                  >
-                    {({ selected, active }) => (
-                      <>
-                        <div className="flex">
-                          <span
-                            className={clsx(
-                              selected ? 'font-semibold' : 'font-normal',
-                              'truncate'
-                            )}
-                          >
-                            {person.name}
-                          </span>
-                          <span
-                            className={clsx(
-                              active ? 'text-indigo-200' : 'text-gray-500',
-                              'ml-2 truncate'
-                            )}
-                          >
-                            {person.username}
-                          </span>
-                        </div>
-
-                        {selected ? (
-                          <span
-                            className={clsx(
-                              active ? 'text-white' : 'text-indigo-600',
-                              'absolute inset-y-0 right-0 flex items-center pr-4'
-                            )}
-                          >
-                            <CheckIcon className="h-5 w-5" aria-hidden="true" />
-                          </span>
-                        ) : null}
-                      </>
-                    )}
-                  </Listbox.Option>
-                ))}
-              </Listbox.Options>
-            </Transition>
-          </div>
-        </>
-      )}
-    </Listbox>
   )
 }
