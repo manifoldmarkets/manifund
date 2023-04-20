ed39e546c50ae49051ced417103d10ec4e7fd95c
rachelweinberg12 <rachel.weinberg12@gmail.com>
2023-04-20 15:00:58 -0700

    :bug: add condition so projects don't get retransferred

--- pages/api/transfer-project.ts
+++ pages/api/transfer-project.ts
@@ -20,13 +20,15 @@ export default async function handler(
     user.email ?? ''
   )
   projectTransfers.forEach(async (transfer) => {
-    await supabaseAdmin.rpc('transfer_project', {
-      project_id: transfer.projects.id,
-      to_id: user.id,
-      from_id: transfer.projects.creator,
-      transfer_id: transfer.id,
-      amount: transfer.grant_amount ?? 0,
-    })
+    if (!transfer.transferred) {
+      await supabaseAdmin.rpc('transfer_project', {
+        project_id: transfer.projects.id,
+        to_id: user.id,
+        from_id: transfer.projects.creator,
+        transfer_id: transfer.id,
+        amount: transfer.grant_amount ?? 0,
+      })
+    }
   })
   res.status(200).json({
     message: `Transferred ${projectTransfers.length} projects to ${user.id}`,
