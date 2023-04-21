06b8e1ad02f8d3460e72663ef239009df36c2e90
rachelweinberg12 <rachel.weinberg12@gmail.com>
2023-04-21 11:50:22 -0700

    :bug: add revalidate to create project page (or else new rounds don't show up)

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -2,6 +2,7 @@ import { getRounds } from '@/db/round'
 import { createServerClient } from '@/db/supabase-server'
 import { CreateProjectForm } from './create-project-form'
 
+export const revalidate = 0
 export default async function CreateProposalPage() {
   const supabase = createServerClient()
   const rounds = await getRounds(supabase)
