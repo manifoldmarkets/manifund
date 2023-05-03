9047fd1091a3edda0087f121313e82bf3b077847
rachelweinberg12 <rachel.weinberg12@gmail.com>
2023-05-03 11:01:58 -0700

    :bug: localhost vs. dev.manifund.org distinction on getURL

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,8 +1,11 @@
 export function getURL() {
+  const fullUrl = window.location.href
   let url =
     process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
     process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
-    'http://localhost:3000/'
+    fullUrl.includes('localhost')
+      ? 'http://localhost:3000/'
+      : 'dev.manifund.org/'
   // Make sure to include `https://` when not localhost.
   url = url.includes('http') ? url : `https://${url}`
   // Make sure to including trailing `/`.
