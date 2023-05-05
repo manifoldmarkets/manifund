8c1a2eb4326d47403e75dd0743377bebc4cc2efc
rachelweinberg12 <rachel.weinberg12@gmail.com>
2023-05-04 18:57:37 -0700

    :lipstick: right look, wrong data

--- app/projects/all-rounds-display.tsx
+++ app/projects/all-rounds-display.tsx
@@ -9,6 +9,7 @@ import { getRoundTheme } from '@/utils/constants'
 import { RoundTag } from '@/components/tags'
 import { orderBy, sortBy } from 'lodash'
 import { Profile } from '@/db/profile'
+import { RichContent } from '@/components/editor'
 
 export function AllRoundsDisplay(props: {
   rounds: Round[]
@@ -18,58 +19,51 @@ export function AllRoundsDisplay(props: {
   const { rounds, projects, regranters } = props
   const sortedRounds = sortRoundsForPreview(rounds)
   return (
-    <div className=" py-24 sm:py-32">
+    <div className="py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto max-w-2xl text-center">
+        <div className="mx-auto max-w-2xl lg:max-w-4xl">
           <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
             Active rounds
           </h2>
           <p className="mt-2 text-lg leading-8 text-gray-600">
-            Organized funding rounds for impact certificates and grants.
+            Organized funding rounds with active projects.
           </p>
-        </div>
-        <div className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-          {sortedRounds.map((round) => (
-            <article
-              key={round.title}
-              className="flex flex-col items-start justify-between"
-            >
-              <div className="relative w-full">
-                <Image
-                  src={round.header_image_url ?? ''}
-                  width="800"
-                  height="500"
-                  alt=""
-                  className="aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]"
-                />
-                <div className="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
-              </div>
-              <div className="max-w-xl">
-                {/* @ts-expect-error server component*/}
-                <RoundData round={round} projects={projects} simple />
-                <div className="group relative">
-                  <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
-                    <a href={`/rounds/${round.slug}`}>
-                      <span className="absolute inset-0" />
-                      {round.title}
-                    </a>
-                  </h3>
-                  <p className="mt-5 text-sm leading-6 text-gray-600 line-clamp-3">
-                    {JSON.stringify(round.description)}
-                  </p>
-                </div>
-                <div className="relative mt-8 flex items-center gap-x-4">
+          <div className="mt-16 space-y-20 lg:mt-20 lg:space-y-20">
+            {sortedRounds.map((round) => (
+              <article
+                key={round.title}
+                className="relative isolate flex flex-col gap-8 lg:flex-row"
+              >
+                <div className="relative aspect-[16/9] sm:aspect-[2/1] lg:aspect-square lg:w-64 lg:shrink-0">
                   <Image
                     src={round.header_image_url ?? ''}
-                    width="800"
                     height="500"
+                    width="800"
                     alt=""
-                    className="h-10 w-10 rounded-full bg-gray-100"
+                    className="absolute inset-0 h-full w-full rounded-2xl bg-gray-50 object-cover"
                   />
+                  <div className="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
                 </div>
-              </div>
-            </article>
-          ))}
+                <div>
+                  <div className="group relative max-w-xl">
+                    <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:underline">
+                      <a href={`/rounds/${round.slug}`}>
+                        <span className="absolute inset-0" />
+                        {round.title}
+                      </a>
+                    </h3>
+                    <p className="mt-5 text-sm leading-6 text-gray-600">
+                      {round.subtitle}
+                    </p>
+                  </div>
+                  <div className="mt-6 border-t border-gray-900/5 pt-6">
+                    {/* @ts-expect-error server component*/}
+                    <RoundData round={round} projects={projects} />
+                  </div>
+                </div>
+              </article>
+            ))}
+          </div>
         </div>
       </div>
     </div>
