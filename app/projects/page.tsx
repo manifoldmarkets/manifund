6bff6629892674e936a32acf26bcd72991c2a6db
rachelweinberg12 <rachel.weinberg12@gmail.com>
2023-05-23 14:52:22 -0700

    :sparkles: v1

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -3,7 +3,15 @@ import { listProjects } from '@/db/project'
 import { getRounds } from '@/db/round'
 import { AllRoundsDisplay } from './all-rounds-display'
 import { ProjectsDisplay } from '@/components/projects-display'
-import { getRegranters } from '@/db/profile'
+import { getRegranters, getUser } from '@/db/profile'
+import { SupabaseClient } from '@supabase/supabase-js'
+import { Row } from '@/components/layout/row'
+import {
+  ArrowPathIcon,
+  ArrowRightIcon,
+  ArrowTrendingUpIcon,
+} from '@heroicons/react/20/solid'
+import { Col } from '@/components/layout/col'
 
 export const revalidate = 30
 
@@ -14,6 +22,7 @@ export default async function Projects() {
   const regranters = await getRegranters(supabase)
   return (
     <div className="bg-dark-200 max-w-4xl px-6 pt-5">
+      <LandingSection />
       <AllRoundsDisplay
         rounds={rounds}
         projects={projects}
@@ -27,5 +36,61 @@ export default async function Projects() {
       </p>
       <ProjectsDisplay projects={projects} />
     </div>
+  )
+}
+
+function LandingSection() {
+  return (
+    <Col className="mb-10 gap-4">
+      <div className="rounded-lg bg-gradient-to-r from-orange-500 to-rose-500 p-5">
+        <p className="text-4xl font-medium text-white shadow-rose-500 text-shadow-lg">
+          Give impactfully,
+        </p>
+        <p className="text-right text-4xl font-medium text-white shadow-orange-500 text-shadow-lg">
+          get funded efficiently.
+        </p>
+        <p className="mt-5 text-center text-white">
+          Manifund offers charitable funding infrastructure designed to improve
+          incentives, efficiency, and transparency.
+        </p>
+      </div>
+      <Row className="justify-between gap-3">
+        <div className="rounded-lg border-2 border-orange-500 bg-white p-3">
+          <Row className="mb-1 gap-1 text-orange-500">
+            <ArrowPathIcon className="h-7 w-7 text-orange-500" />
+            <p className="text-lg font-medium">Regranting</p>
+          </Row>
+          <p className="text-sm text-gray-600">
+            Allows donors to outsource their giving to qualified and trusted
+            regrantors.
+          </p>
+          <LearnMoreButton url="/rounds/regrants?tab=about" />
+        </div>
+        <div className="rounded-lg border-2 border-orange-500 bg-white p-3">
+          <Row className="mb-1 gap-1 text-orange-500">
+            <ArrowTrendingUpIcon className="h-7 w-7" />
+            <p className="text-lg font-medium">Impact certificates</p>
+          </Row>
+          <p className="text-sm text-gray-600">
+            Align incentives with impact by bringing for-profit funding
+            mechanisms to the non-profit space.
+          </p>
+          <LearnMoreButton url="/about" />
+        </div>
+      </Row>
+    </Col>
+  )
+}
+
+function LearnMoreButton(props: { url: string }) {
+  const { url } = props
+  return (
+    <a
+      href={url}
+      className="flex w-full justify-end text-xs text-orange-500 hover:underline"
+    >
+      Learn more
+      <ArrowRightIcon className="ml-1 h-4 w-4" />
+    </a>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require('tailwindcss/defaultTheme')
+const plugin = require('tailwindcss/plugin')
 
 module.exports = {
   content: [
@@ -17,6 +18,11 @@ module.exports = {
         poiret: ['var(--font-poiret-one)', ...defaultTheme.fontFamily.sans],
         josefin: ['var(--font-josefin-slab)', ...defaultTheme.fontFamily.sans],
       },
+      textShadow: {
+        sm: '0 1px 2px var(--tw-shadow-color)',
+        DEFAULT: '0 2px 4px var(--tw-shadow-color)',
+        lg: '0 8px 16px var(--tw-shadow-color)',
+      },
     },
   },
   plugins: [
@@ -24,5 +30,15 @@ module.exports = {
     require('@tailwindcss/typography'),
     require('tailwind-scrollbar-hide'),
     require('@tailwindcss/line-clamp'),
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          'text-shadow': (value) => ({
+            textShadow: value,
+          }),
+        },
+        { values: theme('textShadow') }
+      )
+    }),
   ],
 }
