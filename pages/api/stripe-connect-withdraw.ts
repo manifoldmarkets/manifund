0635416e9cff319776f6c52781dca23422433ca5
rachelweinberg12 <rachel.weinberg12@gmail.com>
2023-05-29 10:45:50 -0700

    :bug: switch create-connect-account to serverless

--- pages/api/stripe-connect-withdraw.ts
+++ pages/api/stripe-connect-withdraw.ts
@@ -1,42 +1,37 @@
-import { NextRequest, NextResponse } from 'next/server'
-import { createEdgeClient } from './_db'
 import { getProfileAndBidsById } from '@/db/profile'
 import { getFullTxnsByUser } from '@/db/txn'
 import { calculateUserBalance, calculateWithdrawBalance } from '@/utils/math'
 import Stripe from 'stripe'
 import { BANK_ID, STRIPE_SECRET_KEY } from '@/db/env'
 import uuid from 'react-uuid'
-
-export const config = {
-  runtime: 'edge',
-  regions: ['sfo1'],
-}
+import { NextApiRequest, NextApiResponse } from 'next'
+import { createServerClient } from '@/db/supabase-server'
 
 const stripe = new Stripe(STRIPE_SECRET_KEY as string, {
   apiVersion: '2022-11-15',
   typescript: true,
 })
 
-export default async function handler(req: NextRequest) {
-  const { amount } = await req.json()
-  const supabase = createEdgeClient(req)
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { amount } = await req.body
+  const supabase = createServerClient()
   const resp = await supabase.auth.getUser()
   const user = resp.data.user
   if (!user) {
     {
-      console.log('no user')
-      return NextResponse.error()
+      return res.status(401).json({ error: 'No user' })
     }
   }
   const profile = await getProfileAndBidsById(supabase, user.id)
   if (!profile.stripe_connect_id) {
-    console.log('no stripe connect id')
-    return NextResponse.error()
+    return res.status(401).json({ error: 'No stripe connect id' })
   }
   const account = await stripe.accounts.retrieve(profile.stripe_connect_id)
   if (!account.payouts_enabled || !account.details_submitted) {
-    console.log('no payouts enabled')
-    return NextResponse.error()
+    return res.status(401).json({ error: 'Payouts not enabled' })
   }
   const txns = await getFullTxnsByUser(supabase, user.id)
   const balance = calculateUserBalance(txns, user.id)
@@ -48,8 +43,7 @@ export default async function handler(req: NextRequest) {
     profile.accreditation_status
   )
   if (amount > withdrawBalance) {
-    console.log('amount too high')
-    return NextResponse.error()
+    return res.status(401).json({ error: 'Insufficient funds' })
   }
   const transfer = await stripe.transfers.create({
     amount: amount,
@@ -77,5 +71,5 @@ export default async function handler(req: NextRequest) {
       txn_id: txnId,
     })
     .throwOnError()
-  return NextResponse.json({ amount })
+  return res.status(200).json({ success: true })
 }
