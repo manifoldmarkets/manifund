fe7710593c6fc7aebc8fdc5b00d5536d46389bc1
rachelweinberg12 <rachel.weinberg12@gmail.com>
2023-06-06 10:54:54 -0700

    :bug: lodash edge build failure

--- pages/api/sign-grant-agreement.ts
+++ pages/api/sign-grant-agreement.ts
@@ -6,6 +6,10 @@ import { maybeActivateGrant } from '@/utils/activate-grant'
 export const config = {
   runtime: 'edge',
   regions: ['sfo1'],
+  // From https://github.com/lodash/lodash/issues/5525
+  unstable_allowDynamic: [
+    '**/node_modules/lodash/lodash.js', // Use a glob to allow anything in the function-bind 3rd party module
+  ],
 }
 
 export default async function handler(req: NextRequest) {
