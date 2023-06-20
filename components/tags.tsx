4d528dacab9b13a8079dd19026bd151fa9163c20
rachelweinberg12 <rachel.weinberg12@gmail.com>
2023-06-20 11:03:51 -0700

    :art: remove unused classNames

--- components/tags.tsx
+++ components/tags.tsx
@@ -102,7 +102,7 @@ export function AccreditedTag() {
     >
       <Row className="max-w-fit gap-0.5 rounded-full bg-emerald-100 px-2 pt-1 text-center">
         <CurrencyDollarIcon className="relative bottom-0.5 m-auto h-4 w-4 stroke-2 text-emerald-500" />
-        <div className="text-md relative bottom-0.5 text-sm leading-tight text-emerald-500">
+        <div className="relative bottom-0.5 text-sm leading-tight text-emerald-500">
           accredited investor
         </div>
       </Row>
@@ -113,9 +113,9 @@ export function AccreditedTag() {
 export function RegranterTag() {
   return (
     <Tooltip text="Regranters can recieve donations from other users and give grants to the projects and organizations of their choice.">
-      <Row className="max-w-fit gap-0.5 rounded-full bg-orange-100 px-2  pt-1 text-center text-sm">
+      <Row className="max-w-fit gap-0.5 rounded-full bg-orange-100 px-2  pt-1 text-center">
         <ArrowPathIcon className="relative bottom-0.5 m-auto h-4 w-4 stroke-2 text-orange-500" />
-        <div className="text-md relative bottom-0.5 leading-tight text-orange-500">
+        <div className="relative bottom-0.5 text-sm leading-tight text-orange-500">
           regrantor
         </div>
       </Row>
