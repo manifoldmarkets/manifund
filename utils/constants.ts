087fce830c5bb456007c92fe8d9d44648d8e2256
Austin Chen <akrolsmir@gmail.com>
2023-06-21 10:16:51 -0700

    Add Gavriel and Zvi

--- utils/constants.ts
+++ utils/constants.ts
@@ -41,13 +41,15 @@ export function getRoundTheme(roundTitle: string) {
 export function getSponsoredAmount(regrantorName: string) {
   // TODO: Switch mapping from full name to username
   const sponsoredRegrantors = {
-    'Isaak Freeman': 50000,
-    'Marcus Abramovitch': 50000,
-    'Rachel Weinberg': 50000,
-    'Austin Chen': 50000,
-    'Qualy the lightbulb ': 50000,
-    'Adam Gleave': 400000,
-    'Dan Hendrycks': 400000,
+    'Isaak Freeman': 50_000,
+    'Marcus Abramovitch': 50_000,
+    'Rachel Weinberg': 50_000,
+    'Austin Chen': 50_000,
+    'Qualy the lightbulb ': 50_000,
+    'Gavriel Kleinwaks': 50_000,
+    'Zvi Mowshowitz': 50_000,
+    'Adam Gleave': 400_000,
+    'Dan Hendrycks': 400_000,
   } as { [key: string]: number }
-  return sponsoredRegrantors[regrantorName] ?? 0
+  return sponsoredRegrantors[regrantorName]
 }
