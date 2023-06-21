f151fe130a67f628c1c97a8d97b0a3f9d257df88
rachelweinberg12 <rachel.weinberg12@gmail.com>
2023-06-21 16:24:45 -0700

    :bug: add lodash unstable allow dynamic to edge

--- pages/api/trade.ts
+++ pages/api/trade.ts
@@ -14,6 +14,10 @@ import { TOTAL_SHARES } from '@/db/project'
 export const config = {
   runtime: 'edge',
   regions: ['sfo1'],
+  // From https://github.com/lodash/lodash/issues/5525
+  unstable_allowDynamic: [
+    '**/node_modules/lodash/lodash.js', // Use a glob to allow anything in the function-bind 3rd party module
+  ],
 }
 
 export type TradeProps = {
