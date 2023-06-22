f7bc60f094254910779b8c682d73ea8016e31525
rachelweinberg12 <rachel.weinberg12@gmail.com>
2023-06-22 13:36:53 -0700

    Revert "⬆️ Update Next & Supabase packages (#44)"
    This reverts commit 83ea936119e202e08f4959ba55463c3e88f04c09.

--- db/supabase-server.ts
+++ db/supabase-server.ts
@@ -1,16 +1,15 @@
 import 'server-only'
 
-import { cookies } from 'next/headers'
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { headers, cookies } from 'next/headers'
+import { createServerComponentSupabaseClient } from '@supabase/auth-helpers-nextjs'
 
 import type { Database } from './database.types'
 import { SUPABASE_ANON_KEY, SUPABASE_URL } from './env'
 
 export const createServerClient = () =>
-  createServerComponentClient<Database>(
-    { cookies },
-    {
-      supabaseUrl: SUPABASE_URL,
-      supabaseKey: SUPABASE_ANON_KEY,
-    }
-  )
+  createServerComponentSupabaseClient<Database>({
+    supabaseUrl: SUPABASE_URL,
+    supabaseKey: SUPABASE_ANON_KEY,
+    headers,
+    cookies,
+  })
--- pages/api/_db.ts
+++ pages/api/_db.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { createClient } from '@supabase/supabase-js'
-import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
+import { createMiddlewareSupabaseClient } from '@supabase/auth-helpers-nextjs'
 import { Database } from '@/db/database.types'
 import {
   SUPABASE_ANON_KEY,
@@ -10,7 +10,7 @@ import {
 
 export function createEdgeClient(req: NextRequest) {
   const res = NextResponse.next()
-  return createMiddlewareClient<Database>(
+  return createMiddlewareSupabaseClient<Database>(
     {
       req,
       res,
