37a92926a736a6130d942afb3cccfc6de6f39c40
Austin Chen <akrolsmir@gmail.com>
2023-06-22 11:18:11 -0700

    Rename imports per convention
    See https://supabase.com/docs/guides/auth/auth-helpers/nextjs#migrating-to-v07x

--- db/supabase-server.ts
+++ db/supabase-server.ts
@@ -1,15 +1,16 @@
 import 'server-only'
 
-import { headers, cookies } from 'next/headers'
-import { createServerComponentSupabaseClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 
 import type { Database } from './database.types'
 import { SUPABASE_ANON_KEY, SUPABASE_URL } from './env'
 
 export const createServerClient = () =>
-  createServerComponentSupabaseClient<Database>({
-    supabaseUrl: SUPABASE_URL,
-    supabaseKey: SUPABASE_ANON_KEY,
-    headers,
-    cookies,
-  })
+  createServerComponentClient<Database>(
+    { cookies },
+    {
+      supabaseUrl: SUPABASE_URL,
+      supabaseKey: SUPABASE_ANON_KEY,
+    }
+  )
