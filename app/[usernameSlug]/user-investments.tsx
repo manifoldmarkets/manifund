d48d8c44b256971a2c00e25d0d55f46f9a7ec83a
rachelweinberg12 <rachel.weinberg12@gmail.com>
2023-06-22 14:44:40 -0700

    :lipstick: mobile-compatible investments table

--- app/[usernameSlug]/user-investments.tsx
+++ app/[usernameSlug]/user-investments.tsx
@@ -9,35 +9,39 @@ export function Investments(props: { investments: Investment[] }) {
   const { investments } = props
   const investmentsDisplay = investments
     .filter((investment) => investment.numShares !== 0 && investment.project)
-    .map((investment) =>
-      investment.project ? (
+    .map((investment) => {
+      const priceText = (
+        <p className="flex items-center text-sm text-gray-500">
+          bought&nbsp;
+          <span className="text-black">
+            {formatMoney(-investment.priceUsd)}
+          </span>
+          &nbsp;@&nbsp;
+          <span className="text-black">
+            {formatMoney(
+              (-investment.priceUsd * 10000000) / investment.numShares
+            )}
+          </span>
+          &nbsp;valuation
+        </p>
+      )
+      return investment.project ? (
         <Link
           href={`/projects/${investment.project.slug}/?tab=shareholders`}
           className="table-row w-full"
           key={investment.project.id}
         >
-          <td className="p-4">{investment.project.title}</td>
-          <td className="py-4">
-            <p className="flex items-center text-sm text-gray-500">
-              bought&nbsp;
-              <span className="text-black">
-                {formatMoney(-investment.priceUsd)}
-              </span>
-              &nbsp;@&nbsp;
-              <span className="text-black">
-                {formatMoney(
-                  (-investment.priceUsd * 10000000) / investment.numShares
-                )}
-              </span>
-              &nbsp;valuation
-            </p>
+          <td className="p-4">
+            {investment.project.title}
+            <div className="mt-2 lg:hidden">{priceText}</div>
           </td>
+          <td className="hidden py-4 lg:table-cell">{priceText}</td>
           <td className="hidden p-4 text-right text-sm text-gray-500 sm:block">
             <RoundTag roundTitle={investment.project.round} />
           </td>
         </Link>
       ) : null
-    )
+    })
   return (
     <div>
       <h1 className="text-xl sm:text-2xl">Investments</h1>
