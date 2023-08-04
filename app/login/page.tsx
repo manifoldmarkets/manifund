631b420e3b4dffb2b17ad2b865d811e7ea53edcc
rachelweinberg12 <rachel.weinberg12@gmail.com>
2023-08-03 20:09:30 -0400

    :bug: redirect after signin if profile is incomplete

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,15 @@
 import Auth from '@/components/auth-ui'
+import { getUser, getProfileById } from '@/db/profile'
+import { createServerClient } from '@/db/supabase-server'
 
-export default function Login() {
-  return <Auth />
+export default async function Login() {
+  const supabase = createServerClient()
+  const user = await getUser(supabase)
+  const profile = user ? await getProfileById(supabase, user?.id) : null
+  const redirectTo = profile
+    ? profile.id === profile.username
+      ? '/edit-profile'
+      : '/projects'
+    : undefined
+  return <Auth redirectTo={redirectTo} />
 }
--- components/auth-ui.tsx
+++ components/auth-ui.tsx
@@ -6,50 +6,46 @@ import { ThemeSupa, Theme } from '@supabase/auth-ui-shared'
 import { Button } from '@/components/button'
 import { getURL } from '@/utils/constants'
 import { useRouter, useSearchParams } from 'next/navigation'
+import Image from 'next/image'
 
-export default function ClientAuth() {
+export default function ClientAuth(props: { redirectTo?: string }) {
+  const { redirectTo } = props
   const { supabase, session } = useSupabase()
   const user = session?.user
   const router = useRouter()
-  if (user) {
-    router.push('/projects')
+  if (user && redirectTo) {
+    router.push(redirectTo)
   }
   const params = useSearchParams()
   const recommendedEmail = params?.get('email')
-
-  return (
-    <div className="bg-dark-200 max-w-md">
-      {user ? (
-        <div className="p-4">
-          <h1 className="text-2xl font-bold">Signed in as {user.email}</h1>
-          <Button
-            onClick={async () => {
-              await supabase.auth.signOut()
-            }}
-          >
-            Sign out
-          </Button>
+  if (user) {
+    return (
+      <div className="flex h-[70vh] w-full items-center justify-center">
+        <div className="flex h-36 w-36 animate-spin items-center justify-center rounded-full bg-gradient-to-tr from-gray-100 to-gray-300">
+          <div className="h-28 w-28 items-center justify-center rounded-full bg-white"></div>
         </div>
-      ) : (
-        <>
-          {recommendedEmail && (
-            <span className="text-gray-600">
-              Make sure to create an account with the same email that your grant
-              notification was sent to (
-              <span className="font-bold text-black">{recommendedEmail}</span>
-              ).
-            </span>
-          )}
-          <Auth
-            supabaseClient={supabase}
-            appearance={{ theme: manifundTheme }}
-            providers={['google']}
-            redirectTo={`${getURL()}/edit-profile`}
-          />
-        </>
-      )}
-    </div>
-  )
+      </div>
+    )
+  } else {
+    return (
+      <div className="bg-dark-200 max-w-md">
+        {recommendedEmail && (
+          <span className="text-gray-600">
+            Make sure to create an account with the same email that your grant
+            notification was sent to (
+            <span className="font-bold text-black">{recommendedEmail}</span>
+            ).
+          </span>
+        )}
+        <Auth
+          supabaseClient={supabase}
+          appearance={{ theme: manifundTheme }}
+          providers={['google']}
+          redirectTo={`${getURL()}/edit-profile`}
+        />
+      </div>
+    )
+  }
 }
 
 const manifundTheme: Theme = {
