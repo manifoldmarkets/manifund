e60df26fb52459a127c650c562dae1343dd9aa78
rachelweinberg12 <rachel.weinberg12@gmail.com>
2023-08-09 18:10:30 -0400

    :lipstick: change sidebar

--- app/sidebar.tsx
+++ app/sidebar.tsx
@@ -28,57 +28,83 @@ export default async function Sidebar() {
     <>
       <nav
         aria-label="Sidebar"
-        className="sticky top-0 hidden max-h-[100vh] divide-gray-300 self-start pt-10 pl-2 pr-2 md:col-span-3 md:flex md:flex-col"
+        className="sticky top-0 hidden h-[100vh] justify-between divide-gray-300 self-start pt-10 pl-2 pr-2 md:col-span-3 md:flex md:flex-col"
       >
-        <Link href="/">
-          <div className="group flex flex-row items-center gap-1">
-            <Image
-              src="/Manifox.png"
-              alt="Manifund fox"
-              className="max-w-[50px] -translate-y-2 transition-all group-hover:-translate-y-3 lg:max-w-[60px]"
-              width={100}
-              height={100}
-            />
-            <span className="bg-gradient-to-r from-orange-500  to-rose-400 bg-clip-text font-josefin text-3xl font-[650] text-transparent lg:text-4xl">
-              {SUPABASE_ENV === 'PROD' ? 'Manifund' : 'Devifund'}
-            </span>
-          </div>
-        </Link>
-        <div className="h-6" />
-        {/* @ts-expect-error Server Component */}
-        {profile && <ProfileSummary profile={profile} />}
-        {user === undefined && <div className="h-[56px]" />}
-        <SidebarItem item={{ name: 'Home', href: '/' }} />
-        {user === null && (
-          <SidebarItem
-            item={{
-              name: 'Login',
-              href: `/login`,
-            }}
-          />
-        )}
-        <SidebarItem item={{ name: 'About', href: '/about' }} />
-        <SidebarItem item={{ name: 'Charity', href: '/charity' }} />
-        <SidebarItem
-          item={{ name: 'Discord', href: 'https://discord.gg/ZGsDMWSA5Q' }}
-        />
-        <SidebarItem
-          item={{
-            name: 'Substack',
-            href: 'https://manifund.substack.com/',
-          }}
-        />
-        {user && (
-          <Link
-            href={isRegranter ? '/create-grant' : '/create'}
-            className={clsx(
-              buttonClass('xl', 'gradient'),
-              'mt-4 w-52 bg-gradient-to-r'
-            )}
-          >
-            {isRegranter ? 'Give a grant' : 'Create a project'}
+        <Col>
+          <Link href="/">
+            <div className="group flex flex-row items-center gap-1">
+              <Image
+                src="/Manifox.png"
+                alt="Manifund fox"
+                className="max-w-[50px] -translate-y-2 transition-all group-hover:-translate-y-3 lg:max-w-[60px]"
+                width={100}
+                height={100}
+              />
+              <span className="bg-gradient-to-r from-orange-500  to-rose-400 bg-clip-text font-josefin text-3xl font-[650] text-transparent lg:text-4xl">
+                {SUPABASE_ENV === 'PROD' ? 'Manifund' : 'Devifund'}
+              </span>
+            </div>
           </Link>
-        )}
+          <div className="h-6" />
+          {/* @ts-expect-error Server Component */}
+          {profile && <ProfileSummary profile={profile} />}
+          {user === undefined && <div className="h-[56px]" />}
+          <SidebarItem item={{ name: 'Home', href: '/' }} />
+          {user === null && (
+            <SidebarItem
+              item={{
+                name: 'Login',
+                href: `/login`,
+              }}
+            />
+          )}
+          <SidebarItem item={{ name: 'About', href: '/about' }} />
+          <SidebarItem item={{ name: 'People', href: '/people' }} />
+          <SidebarItem item={{ name: 'Charity', href: '/charity' }} />
+          {user && (
+            <Link
+              href={isRegranter ? '/create-grant' : '/create'}
+              className={clsx(
+                buttonClass('xl', 'gradient'),
+                'mt-4 w-52 bg-gradient-to-r'
+              )}
+            >
+              {isRegranter ? 'Give a grant' : 'Create a project'}
+            </Link>
+          )}
+        </Col>
+        <Row className="gap-5 px-3 py-10">
+          <a
+            className="text-xs text-orange-500 hover:text-orange-600"
+            href="https://discord.gg/ZGsDMWSA5Q"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="currentColor"
+              className="bi bi-discord"
+              viewBox="0 0 16 16"
+            >
+              <path d="M13.545 2.907a13.227 13.227 0 0 0-3.257-1.011.05.05 0 0 0-.052.025c-.141.25-.297.577-.406.833a12.19 12.19 0 0 0-3.658 0 8.258 8.258 0 0 0-.412-.833.051.051 0 0 0-.052-.025c-1.125.194-2.22.534-3.257 1.011a.041.041 0 0 0-.021.018C.356 6.024-.213 9.047.066 12.032c.001.014.01.028.021.037a13.276 13.276 0 0 0 3.995 2.02.05.05 0 0 0 .056-.019c.308-.42.582-.863.818-1.329a.05.05 0 0 0-.01-.059.051.051 0 0 0-.018-.011 8.875 8.875 0 0 1-1.248-.595.05.05 0 0 1-.02-.066.051.051 0 0 1 .015-.019c.084-.063.168-.129.248-.195a.05.05 0 0 1 .051-.007c2.619 1.196 5.454 1.196 8.041 0a.052.052 0 0 1 .053.007c.08.066.164.132.248.195a.051.051 0 0 1-.004.085 8.254 8.254 0 0 1-1.249.594.05.05 0 0 0-.03.03.052.052 0 0 0 .003.041c.24.465.515.909.817 1.329a.05.05 0 0 0 .056.019 13.235 13.235 0 0 0 4.001-2.02.049.049 0 0 0 .021-.037c.334-3.451-.559-6.449-2.366-9.106a.034.034 0 0 0-.02-.019Zm-8.198 7.307c-.789 0-1.438-.724-1.438-1.612 0-.889.637-1.613 1.438-1.613.807 0 1.45.73 1.438 1.613 0 .888-.637 1.612-1.438 1.612Zm5.316 0c-.788 0-1.438-.724-1.438-1.612 0-.889.637-1.613 1.438-1.613.807 0 1.451.73 1.438 1.613 0 .888-.631 1.612-1.438 1.612Z" />
+            </svg>
+          </a>
+          <a
+            className="text-xs text-orange-500 hover:text-orange-600"
+            href="https://manifund.substack.com/"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="22"
+              height="22"
+              fill="currentColor"
+              className="bi bi-bookmark-fill"
+              viewBox="0 0 16 16"
+            >
+              <path d="M2 2v13.5a.5.5 0 0 0 .74.439L8 13.069l5.26 2.87A.5.5 0 0 0 14 15.5V2a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2z" />
+            </svg>
+          </a>
+        </Row>
       </nav>
     </>
   )
