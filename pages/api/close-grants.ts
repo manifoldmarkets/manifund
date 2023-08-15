bb6a4df4a2bac5db17aa4c2942f14a26e8c3be38
rachelweinberg12 <rachel.weinberg12@gmail.com>
2023-08-15 13:44:39 -0400

    :sparkles: send rejection emails

--- pages/api/close-grants.ts
+++ pages/api/close-grants.ts
@@ -5,6 +5,8 @@ import { getAmountRaised } from '@/utils/math'
 import { Project } from '@/db/project'
 import { Bid } from '@/db/bid'
 import { SupabaseClient } from '@supabase/supabase-js'
+import { sendTemplateEmail } from '@/utils/email'
+import { getURL } from 'next/dist/shared/lib/utils'
 
 export const config = {
   runtime: 'edge',
@@ -15,32 +17,45 @@ export default async function handler(req: NextRequest) {
   const supabase = createAdminClient()
   const { data: proposals } = await supabase
     .from('projects')
-    .select('*, bids(*)')
+    .select('*, bids(*), profiles(full_name)')
     .eq('stage', 'proposal')
   const proposalsPastDeadline = proposals?.filter((proposal) => {
     return isBefore(new Date(proposal.auction_close ?? ''), new Date())
   })
   proposalsPastDeadline?.forEach(async (proposal) => {
-    await closeGrant(proposal, proposal.bids, supabase)
+    await closeGrant(
+      proposal,
+      proposal.bids,
+      proposal.profiles?.full_name ?? '',
+      supabase
+    )
   })
 }
 
 async function closeGrant(
   project: Project,
   bids: Bid[],
+  creatorName: string,
   supabase: SupabaseClient
 ) {
-  // if proposal has enough money, send notif to us or user depending on bottleneck
-  // if proposal doesn't have enough money:
-  // bids declined, project "not funded", notif to creator & bidders
-
   const amountRaised = getAmountRaised(project, bids)
+  const GENERIC_NOTIF_TEMPLATE_ID = 32825293
   if (amountRaised >= project.min_funding) {
     if (!project.signed_agreement) {
-      // Send reminder email to creator
+      await sendTemplateEmail(GENERIC_NOTIF_TEMPLATE_ID, {
+        notifText: `Your project "${project.title}" has enough funding to proceed but is awaiting your signature on the grant agreement. Please sign the agreement to activate your grant.`,
+        buttonUrl: `${getURL()}/projects/${project.slug}/grant-agreement`,
+        buttonText: 'Sign agreement',
+        subject: 'Manifund: Reminder to sign your grant agreement',
+      })
     }
     if (!project.approved) {
-      // Send reminder email to admins
+      await sendTemplateEmail(GENERIC_NOTIF_TEMPLATE_ID, {
+        notifText: `The project "${project.title}" has enough funding but is awaiting admin approval.`,
+        buttonUrl: `${getURL()}/projects/${project.slug}`,
+        buttonText: 'See project',
+        subject: 'Manifund: Reminder to approve project',
+      })
     }
   } else {
     await supabase
@@ -48,7 +63,33 @@ async function closeGrant(
         project_id: project.id,
       })
       .throwOnError()
-    // Grant verdict email to creator
-    // Bid resolution email to bidders
+    const VERDICT_TEMPLATE_ID = 31974162
+    const recipientPostmarkVars = {
+      recipientFullName: creatorName,
+      verdictMessage: `We regret to inform you that your project, "${project.title}," has not recieved enough funding to proceed. Please let us know on our discord if you have any questions or feedback about the process.`,
+      projectUrl: `${getURL()}/projects/${project.slug}`,
+      subject: `Manifund project not funded: "${project.title}"`,
+      adminName: 'Rachel',
+    }
+    await sendTemplateEmail(
+      VERDICT_TEMPLATE_ID,
+      recipientPostmarkVars,
+      project.creator
+    )
+    const OFFER_RESOLVED_TEMPLATE_ID = 31316141
+    bids.forEach(async (bid) => {
+      const bidderPostmarkVars = {
+        projectTitle: project.title,
+        result: 'declined',
+        projectUrl: `${getURL()}/projects/${project.slug}`,
+        auctionResolutionText: `This project was not funded, because it recieved only ${amountRaised} in funding, which is less than its' minimum funding goal of ${project.min_funding}.}`,
+        bidResolutionText: `Your offer was declined.`,
+      }
+      await sendTemplateEmail(
+        OFFER_RESOLVED_TEMPLATE_ID,
+        bidderPostmarkVars,
+        bid.bidder
+      )
+    })
   }
 }
