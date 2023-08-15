c110a8b83935459dce71889f4cda1940b8bf4fd1
rachelweinberg12 <rachel.weinberg12@gmail.com>
2023-08-15 14:52:40 -0400

    :bug: fix build error

--- pages/api/close-grants.ts
+++ pages/api/close-grants.ts
@@ -11,6 +11,9 @@ import { getURL } from 'next/dist/shared/lib/utils'
 export const config = {
   runtime: 'edge',
   regions: ['sfo1'],
+  unstable_allowDynamic: [
+    '**/node_modules/lodash/_root.js', // Use a glob to allow anything in the function-bind 3rd party module
+  ],
 }
 
 export default async function handler(req: NextRequest) {
