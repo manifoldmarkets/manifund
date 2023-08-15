088b06228ae6a9c583e22b1203af8c1cd31f3625
rachelweinberg12 <rachel.weinberg12@gmail.com>
2023-08-15 11:30:38 -0400

    :construction:

--- pages/api/close-grants.ts
+++ pages/api/close-grants.ts
@@ -4,6 +4,7 @@ import { createAdminClient } from './_db'
 import { getAmountRaised } from '@/utils/math'
 import { Project } from '@/db/project'
 import { Bid } from '@/db/bid'
+import { SupabaseClient } from '@supabase/supabase-js'
 
 export const config = {
   runtime: 'edge',
@@ -19,15 +20,35 @@ export default async function handler(req: NextRequest) {
   const proposalsPastDeadline = proposals?.filter((proposal) => {
     return isBefore(new Date(proposal.auction_close ?? ''), new Date())
   })
+  proposalsPastDeadline?.forEach(async (proposal) => {
+    await closeGrant(proposal, proposal.bids, supabase)
+  })
 }
 
-function closeGrant(project: Project, bids: Bid[]) {
+async function closeGrant(
+  project: Project,
+  bids: Bid[],
+  supabase: SupabaseClient
+) {
   // if proposal has enough money, send notif to us or user depending on bottleneck
   // if proposal doesn't have enough money:
   // bids declined, project "not funded", notif to creator & bidders
 
   const amountRaised = getAmountRaised(project, bids)
   if (amountRaised >= project.min_funding) {
+    if (!project.signed_agreement) {
+      // Send reminder email to creator
+    }
+    if (!project.approved) {
+      // Send reminder email to admins
+    }
   } else {
+    await supabase
+      .rpc('reject_grant', {
+        project_id: project.id,
+      })
+      .throwOnError()
+    // Grant verdict email to creator
+    // Bid resolution email to bidders
   }
 }
