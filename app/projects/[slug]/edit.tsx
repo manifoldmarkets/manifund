8ad5a6c6a653823801c682bfc92e375d56a437d5
rachelweinberg12 <rachel.weinberg12@gmail.com>
2023-08-17 15:59:11 -0400

    :lipstick:

--- app/projects/[slug]/edit.tsx
+++ app/projects/[slug]/edit.tsx
@@ -130,7 +130,7 @@ export function Edit(props: {
       ) : (
         <Row className=" justify-end">
           <IconButton size="sm" onClick={() => setShowEditor(true)}>
-            <Tooltip text="Edit project description">
+            <Tooltip text="Edit project">
               <div className="h-10 w-10 rounded-full bg-orange-500 hover:bg-orange-600">
                 <PencilIcon className="h-10 w-10 p-2 text-white" aria-hidden />
               </div>
