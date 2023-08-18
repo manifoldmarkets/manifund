fd93c3de83e4ba3ee41b373972e8c0c2a267e9be
rachelweinberg12 <rachel.weinberg12@gmail.com>
2023-08-18 12:28:29 -0400

    :lipstick: prettier layout

--- app/topics/page.tsx
+++ app/topics/page.tsx
@@ -6,7 +6,7 @@ export default async function TopicsPage() {
   const supabase = createServerClient()
   const topicsList = await listTopics(supabase)
   return (
-    <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
+    <div className="grid grid-cols-1 gap-4 p-5 sm:grid-cols-2 lg:grid-cols-3 lg:gap-8">
       {topicsList.map((topic) => (
         <TopicCard key={topic.slug} topic={topic} />
       ))}
@@ -22,11 +22,13 @@ function TopicCard(props: { topic: Topic }) {
         src={topic.header_image_url}
         width={1000}
         height={500}
-        className="relative aspect-[5/3] w-full flex-shrink-0 rounded-t bg-white object-cover"
+        className="relative aspect-[3/1] w-full flex-shrink-0 rounded-t bg-white object-cover sm:aspect-[5/3]"
         alt="round header image"
       />
       <div className="py-2 px-4">
-        <span className="text-lg font-semibold">{topic.title}</span>
+        <span className="sm:text-md text-sm font-semibold leading-tight lg:text-lg">
+          {topic.title}
+        </span>
       </div>
     </div>
   )
