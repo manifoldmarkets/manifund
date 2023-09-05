e10576dea7c3930b4debbb7d56ea5a457ec24350
rachelweinberg12 <rachel.weinberg12@gmail.com>
2023-09-05 11:17:00 -0400

    :lipstick: take avatars out of comment boxes and reshape

--- components/comment.tsx
+++ components/comment.tsx
@@ -1,4 +1,4 @@
-import { UserAvatarAndBadge } from '@/components/user-link'
+import { UserLink } from '@/components/user-link'
 import { formatDistanceToNow } from 'date-fns'
 import { RichContent } from '@/components/editor'
 import { Row } from '@/components/layout/row'
@@ -10,6 +10,7 @@ import Link from 'next/link'
 import clsx from 'clsx'
 import { Project } from '@/db/project'
 import { Card } from './layout/card'
+import { Avatar } from './avatar'
 
 export function Comment(props: {
   comment: Comment
@@ -28,48 +29,63 @@ export function Comment(props: {
     children,
   } = props
   return (
-    <Card id={comment.id} className="relative">
-      <Row className="w-full items-center justify-between gap-2">
-        <Row className="min-w-fit items-center gap-1">
-          <UserAvatarAndBadge
-            profile={commenter}
-            creatorBadge={writtenByCreator}
-            className="text-sm text-gray-800"
-          />
-          <p
-            className={clsx(
-              'min-w-fit text-xs text-gray-500',
-              project ? 'inline' : 'hidden sm:inline'
-            )}
-          >
-            {formatDistanceToNow(new Date(comment.created_at), {
-              addSuffix: true,
-            })}
-          </p>
-        </Row>
-        {!project && (
-          <Col className="items-center">
-            {contributionText && <Tag text={contributionText} color="orange" />}
-            <p className={clsx('text-xs text-gray-500 sm:hidden')}>
+    <Row className="gap-2">
+      <Link href={`/${commenter.username}`}>
+        <Avatar
+          username={commenter.username}
+          avatarUrl={commenter.avatar_url}
+          id={commenter.id}
+          className="mt-1 h-8 w-8"
+          noLink
+        />
+      </Link>
+      <Card
+        id={comment.id}
+        className="relative rounded-xl rounded-tl-sm px-6 pt-2"
+      >
+        <Row className="mb-2 w-full items-center justify-between gap-2">
+          <Row className="min-w-fit items-center gap-1">
+            <UserLink
+              name={commenter.full_name}
+              username={commenter.username}
+              creatorBadge={writtenByCreator}
+              className="text-sm font-semibold"
+            />
+            <p
+              className={clsx(
+                'min-w-fit text-xs text-gray-500',
+                project ? 'inline' : 'hidden sm:inline'
+              )}
+            >
               {formatDistanceToNow(new Date(comment.created_at), {
                 addSuffix: true,
               })}
             </p>
-          </Col>
-        )}
-        {project && (
-          <Link
-            href={`/projects/${project.slug}?tab=comments#${comment.id}`}
-            className="truncate overflow-ellipsis text-xs font-semibold text-orange-600 hover:underline"
-          >
-            {project.title}
-          </Link>
-        )}
-      </Row>
-      <div className="relative left-8 w-11/12">
+          </Row>
+          {!project && (
+            <Col className="items-center">
+              {contributionText && (
+                <Tag text={contributionText} color="orange" />
+              )}
+              <p className={clsx('text-xs text-gray-500 sm:hidden')}>
+                {formatDistanceToNow(new Date(comment.created_at), {
+                  addSuffix: true,
+                })}
+              </p>
+            </Col>
+          )}
+          {project && (
+            <Link
+              href={`/projects/${project.slug}?tab=comments#${comment.id}`}
+              className="truncate overflow-ellipsis text-xs font-semibold text-orange-600 hover:underline"
+            >
+              {project.title}
+            </Link>
+          )}
+        </Row>
         <RichContent content={comment.content} className="text-sm" />
-      </div>
-      {children}
-    </Card>
+        {children}
+      </Card>
+    </Row>
   )
 }
