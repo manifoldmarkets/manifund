7ae2151321337994f3393def819e2094ca20ac65
rachelweinberg12 <rachel.weinberg12@gmail.com>
2023-09-13 15:20:30 -0700

    :lipstick:

--- app/evals/evals-project-card.tsx
+++ app/evals/evals-project-card.tsx
@@ -1,3 +1,5 @@
+import { Card } from '@/components/layout/card'
+import { Col } from '@/components/layout/col'
 import { Draggable } from 'react-beautiful-dnd'
 
 export function EvalsProjectCard(props: { title: string; index: number }) {
@@ -10,9 +12,9 @@ export function EvalsProjectCard(props: { title: string; index: number }) {
           {...dragProvided.draggableProps}
           ref={dragProvided.innerRef}
         >
-          <div className="m-2 rounded bg-rose-500 p-3 text-white shadow">
-            {title}
-          </div>
+          <Card className="m-2 h-28">
+            <p className="line-clamp-3">{title}</p>
+          </Card>
         </div>
       )}
     </Draggable>
--- app/evals/tier.tsx
+++ app/evals/tier.tsx
@@ -8,16 +8,16 @@ export function Tier(props: { tierId: string; projectTitles: string[] }) {
   return (
     <Droppable
       droppableId={tierId}
-      type={'CARD'}
+      type="CARD"
       direction="horizontal"
       isCombineEnabled={false}
     >
       {(dropProvided) => (
         <div {...dropProvided.droppableProps}>
-          <div className="grid w-full grid-cols-4 items-center gap-3 rounded border-2 border-dashed border-gray-500 p-4">
-            {tierId}
+          <Row className="flex-2 w-full items-center gap-3 rounded border-2 border-dashed border-gray-500 p-4">
+            <p className="w-24 text-center">{tierId}</p>
             <Row
-              className="col-span-3 w-full flex-wrap"
+              className="col-span-3 w-full overflow-auto"
               ref={dropProvided.innerRef}
             >
               {projectTitles.map((title, index) => (
@@ -25,7 +25,7 @@ export function Tier(props: { tierId: string; projectTitles: string[] }) {
               ))}
               {dropProvided.placeholder}
             </Row>
-          </div>
+          </Row>
         </div>
       )}
     </Droppable>
