b5927954bb7df7e348dbcad477654fa178421f05
rachelweinberg12 <rachel.weinberg12@gmail.com>
2023-09-13 16:49:38 -0700

    :lipstick: style cards

--- app/evals/evals-project-card.tsx
+++ app/evals/evals-project-card.tsx
@@ -1,8 +1,10 @@
 import { Avatar } from '@/components/avatar'
 import { Card } from '@/components/layout/card'
 import { Row } from '@/components/layout/row'
-import { UserAvatarAndBadge } from '@/components/user-link'
+import { UserLink } from '@/components/user-link'
 import { MiniProject } from '@/db/project'
+import { formatMoney } from '@/utils/formatting'
+import { getAmountRaised } from '@/utils/math'
 import { Draggable } from 'react-beautiful-dnd'
 
 export function EvalsProjectCard(props: {
@@ -10,6 +12,8 @@ export function EvalsProjectCard(props: {
   index: number
 }) {
   const { project, index } = props
+  const creator = project.profiles
+  const amountRaised = getAmountRaised(project, [], project.txns)
   return (
     <Draggable key={project.slug} draggableId={project.slug} index={index}>
       {(dragProvided) => (
@@ -18,19 +22,27 @@ export function EvalsProjectCard(props: {
           {...dragProvided.draggableProps}
           ref={dragProvided.innerRef}
         >
-          <Card className="m-2 flex h-28 flex-col justify-between">
+          <Card className="m-2 flex h-32 flex-col justify-between !p-3">
             <p className="line-clamp-3 w-40 text-sm font-semibold">
               {project.title}
             </p>
-            <Row className="gap-1">
-              <Avatar
-                username={project.profiles.username}
-                avatarUrl={project.profiles.avatar_url}
-                id={project.profiles.id}
-                size="xxs"
-              />
-              <p className="text-xs text-gray-600">
-                {project.profiles.full_name}
+            <Row className="flex-2 items-center justify-between gap-2">
+              <Row className="gap-1">
+                <Avatar
+                  username={creator.username}
+                  avatarUrl={creator.avatar_url}
+                  id={creator.id}
+                  size="xxs"
+                />
+                <UserLink
+                  name={creator.full_name}
+                  username={creator.username}
+                  short
+                  className="inline  truncate text-xs text-gray-600"
+                />
+              </Row>
+              <p className="rounded-2xl bg-orange-100 px-1 py-0.5 text-center text-xs font-medium text-orange-600">
+                {formatMoney(amountRaised)}
               </p>
             </Row>
           </Card>
