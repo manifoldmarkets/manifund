081e63ccbf0e82edf04a90f2976fcd405c1aa06c
rachelweinberg12 <rachel.weinberg12@gmail.com>
2023-09-13 17:15:51 -0700

    :lipstick: handles

--- app/evals/evals-project-card.tsx
+++ app/evals/evals-project-card.tsx
@@ -23,8 +23,9 @@ export function EvalsProjectCard(props: {
           {...dragProvided.draggableProps}
           ref={dragProvided.innerRef}
         >
-          <Card className="m-2 flex h-32 flex-col justify-between !p-3">
-            <Link className="line-clamp-3 w-40 text-sm font-semibold hover:underline" href={`/projects/${project.slug}`} target="_blank">
+          <Card className="m-2 flex h-32 flex-col justify-between py-2 px-3 relative">
+          <svg className="absolute top-2 left-1 opacity-50" height={16} width={16} xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" id="drag-indicator"><path fill="none" d="M0 0h24v24H0V0z"></path><path d="M11 18c0 1.1-.9 2-2 2s-2-.9-2-2 .9-2 2-2 2 .9 2 2zm-2-8c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0-6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm6 4c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z"></path></svg>
+            <Link className="pl-3 line-clamp-3 w-40 text-sm font-semibold hover:underline" href={`/projects/${project.slug}`} target="_blank">
               {project.title}
             </Link>
             <Row className="flex-2 items-center justify-between gap-2">
