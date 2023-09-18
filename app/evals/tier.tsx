ad0d6ceec0a614cb85da4ab296d9a87a3ae6e39a
rachelweinberg12 <rachel.weinberg12@gmail.com>
2023-09-18 16:15:52 -0700

    :lipstick: add back gap in tier title

--- app/evals/tier.tsx
+++ app/evals/tier.tsx
@@ -21,7 +21,7 @@ export function Tier(props: {
     >
       <Row
         className={clsx(
-          'h-8 w-fit items-center rounded-br px-2 text-sm text-white',
+          'h-8 w-fit items-center gap-2 rounded-br px-2 text-sm text-white',
           `bg-${tier.color}`
         )}
       >
