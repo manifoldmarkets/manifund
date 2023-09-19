5778c559edf30401e6f100749aa3c2b6fa8f0d57
rachelweinberg12 <rachel.weinberg12@gmail.com>
2023-09-19 13:58:11 -0700

    :bug: prevent localStorage hydration error

--- hooks/use-local-storage.ts
+++ hooks/use-local-storage.ts
@@ -1,31 +1,44 @@
 'use client'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+
+const isServer = typeof window === 'undefined'
 
 // Like useState, but first checks for a JSON object stored under the key in the browser's localStorage
 // If key is not set, does nothing.
 const useLocalStorage = <type>(initialValue: type, key?: string) => {
-  const [state, setState] = useState<type>(() => {
+  const [value, setValue] = useState<type>(initialValue)
+
+  const initialize = () => {
+    if (isServer) {
+      return initialValue
+    }
     try {
       const initialValueString = JSON.stringify(initialValue)
       const value = key ? window.localStorage.getItem(key) : initialValueString
       return value ? JSON.parse(value) : initialValue
     } catch (error) {
       console.log(error)
+      return initialValue
     }
-  })
+  }
+
+  useEffect(() => {
+    if (!isServer) {
+      setValue(initialize())
+    }
+  }, [])
 
   const saveValue = (value: type) => {
     try {
-      if (key) {
+      setValue(value)
+      if (typeof window !== 'undefined' && key) {
         window.localStorage.setItem(key, JSON.stringify(value))
       }
-      console.log('saving', value)
-      setState(value)
     } catch (error) {
       console.log(error)
     }
   }
-  return { value: state, saveValue }
+  return { value: value, saveValue }
 }
 
 export function clearLocalStorageItem(key: string) {
