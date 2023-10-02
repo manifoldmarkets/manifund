e64e44c9d77fe16c4adff93584e8027af1f97c21
rachelweinberg12 <rachel.weinberg12@gmail.com>
2023-10-02 14:57:31 -0700

    :sparkles: link to evals form from results

--- app/[usernameSlug]/evals/page.tsx
+++ app/[usernameSlug]/evals/page.tsx
@@ -1,10 +1,11 @@
 import { createServerClient } from '@/db/supabase-server'
 import { SupabaseClient } from '@supabase/supabase-js'
-import { getProfileByUsername } from '@/db/profile'
+import { getProfileByUsername, getUser } from '@/db/profile'
 import { Project } from '@/db/project'
 import Link from 'next/link'
 import { Col } from '@/components/layout/col'
 import { ProfileTrust, ProjectEval } from '@/app/evals-form/page'
+import { ArrowLongRightIcon } from '@heroicons/react/20/solid'
 
 type Result = {
   id: string
@@ -24,6 +25,7 @@ export default async function ResultsPage(props: {
   if (!evaluator) {
     return <div>User not found</div>
   }
+  const user = await getUser(supabase)
   const evals = await getAllEvals(supabase)
   const profileTrusts = await getAllProfileTrusts(supabase)
   const userEvals = evals.filter((e) => e.evaluator_id === evaluator.id)
@@ -52,7 +54,7 @@ export default async function ResultsPage(props: {
   })
   return (
     <div className="p-4">
-      <h1 className="text-xl font-bold">Results</h1>
+      <h1 className="text-xl font-bold">{evaluator.full_name}&apos;s evals</h1>
       <Col className="divide-y divide-gray-200">
         <div className="grid grid-cols-7 gap-2 py-2 text-sm text-gray-600">
           <p className="col-span-3">Project</p>
@@ -63,6 +65,13 @@ export default async function ResultsPage(props: {
         </div>
         {resultRows}
       </Col>
+      <Link
+        href={user ? '/evals-form' : '/login'}
+        className="mt-10 flex items-center justify-end gap-1 text-sm font-semibold text-orange-600"
+      >
+        {user ? 'My evals form' : 'Log in to add my evals'}
+        <ArrowLongRightIcon className="h-5 w-5 stroke-2" />
+      </Link>
     </div>
   )
 }
