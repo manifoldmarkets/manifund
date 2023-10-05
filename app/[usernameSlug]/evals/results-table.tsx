27a0fffc159a7998b4d75377683cd7efe9a33933
rachelweinberg12 <rachel.weinberg12@gmail.com>
2023-10-04 17:33:22 -0700

    :sparkles: sort evals table

--- app/[usernameSlug]/evals/results-table.tsx
+++ app/[usernameSlug]/evals/results-table.tsx
@@ -1,53 +1,74 @@
-"use client"
-import { Col } from "@/components/layout/col";
-import { Row } from "@/components/layout/row";
-import { Project } from "@/db/project";
-import { ChevronUpIcon } from "@heroicons/react/20/solid";
-import Link from "next/link";
-import { useState } from "react";
-import { Result } from "./page";
+'use client'
+import { Col } from '@/components/layout/col'
+import { Row } from '@/components/layout/row'
+import { Project } from '@/db/project'
+import { ChevronUpDownIcon } from '@heroicons/react/20/solid'
+import clsx from 'clsx'
+import { sortBy } from 'lodash'
+import Link from 'next/link'
+import { useState } from 'react'
+import { Result } from './page'
 
-const columnNames = [
-  'Inside',
-  'Outside',
-  'Conf',
-  'Overall',
-]
+type ResultKey = 'insideScore' | 'outsideScore' | 'confidence' | 'overallScore'
 
-export function ResultsTable(props: {resultsMap: {[k: string]: Result | null} , projects: Project[]}){
-    const {resultsMap, projects} = props
-    const [sort, setSort] = useState('overall')
-    return(<Col className="mt-5 divide-y divide-gray-200">
-    <div className="grid grid-cols-7 gap-2 py-2 text-sm text-gray-600">
-      <p className="col-span-3">Project</p>
-      {columnNames.map((name) => 
-      <Row key={name}>
-      {name}
-      <button onClick={() => setSort(name)}>
-          <ChevronUpIcon className="h-3 w-3" />
-        </button>
-      </Row>
-      )
-      }
-    </div>
-    {projects.map((project) => {
-      if (!!resultsMap[project.id]) {
-        return (<ResultRow key={project.id} project={project} result={resultsMap[project.id] as Result} />)
-      }
-    })}
-  </Col>)
+const columns: Record<ResultKey, string> = {
+  insideScore: 'Inside',
+  outsideScore: 'Outside',
+  confidence: 'Conf',
+  overallScore: 'Overall',
 }
 
-function ResultRow(props: {
-  project: Project
-  result: Result
+export function ResultsTable(props: {
+  resultsMap: { [k: string]: Result | null }
+  projects: Project[]
 }) {
+  const { resultsMap, projects } = props
+  const [sort, setSort] = useState<ResultKey>('overallScore')
+  const sortedProjects = sortBy(projects, (project) => {
+    const result = resultsMap[project.id]
+    if (!result) return 0
+    return -result[sort]
+  })
+  return (
+    <Col className="mt-5 divide-y divide-gray-200">
+      <div className="grid grid-cols-7 gap-2 py-2 text-sm">
+        <p className="col-span-3">Project</p>
+        {Object.keys(columns).map((key) => (
+          <button
+            key={key}
+            onClick={() => setSort(key as ResultKey)}
+            className={clsx(
+              'flex items-center',
+              sort === key ? 'text-orange-600' : ' text-gray-600'
+            )}
+          >
+            {columns[key as ResultKey]}
+            <ChevronUpDownIcon className="h-4 w-4" />
+          </button>
+        ))}
+      </div>
+      {sortedProjects.map((project) => {
+        if (!!resultsMap[project.id]) {
+          return (
+            <ResultRow
+              key={project.id}
+              project={project}
+              result={resultsMap[project.id] as Result}
+            />
+          )
+        }
+      })}
+    </Col>
+  )
+}
+
+function ResultRow(props: { project: Project; result: Result }) {
   const { result, project } = props
-  
+
   return (
     <div className="grid grid-cols-7 gap-4 py-2 text-sm">
       <Link
-        className="line-clamp-1 col-span-3 hover:underline"
+        className="col-span-3 line-clamp-1 hover:underline"
         href={`/projects/${project.slug}`}
       >
         {project.title}
@@ -59,9 +80,7 @@ function ResultRow(props: {
           : Math.round(result.outsideScore * 10) / 10}
       </p>
       <p>{result.confidence}</p>
-      <p className="font-bold">
-        {Math.round(result.overallScore * 10) / 10}
-      </p>
+      <p className="font-bold">{Math.round(result.overallScore * 10) / 10}</p>
     </div>
   )
 }
