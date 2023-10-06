072f686a78ee0cf3981e8c2a6c89cd2167fa0ab3
rachelweinberg12 <rachel.weinberg12@gmail.com>
2023-10-05 18:05:38 -0700

    :art: universal search bar component

--- app/evals-form/tier.tsx
+++ app/evals-form/tier.tsx
@@ -5,7 +5,7 @@ import { Droppable } from 'react-beautiful-dnd'
 import { EvalsProjectCard } from './evals-project-card'
 import { ConfidenceMap } from './evals-form'
 import clsx from 'clsx'
-import { Input } from '@/components/input'
+import { Input, SearchBar } from '@/components/input'
 import { MagnifyingGlassIcon } from '@heroicons/react/20/solid'
 
 export function Tier(props: {
@@ -54,26 +54,11 @@ export function Tier(props: {
           )}
         </Row>
         {tier.id === 'unsorted' && (
-          <div className="relative mt-1 mr-1 rounded-md shadow-sm lg:w-8/12">
-            <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-              <MagnifyingGlassIcon
-                className="h-3 w-3 text-gray-400"
-                aria-hidden="true"
-              />
-            </div>
-            <input
-              type="text"
-              name="search"
-              id="search"
-              autoComplete="off"
-              className="block w-full rounded-md border-0 py-1.5 pl-8 text-xs text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-orange-500"
-              placeholder="Search..."
-              value={search}
-              onChange={(event) => {
-                setSearch(event.target.value)
-              }}
-            />
-          </div>
+          <SearchBar
+            search={search}
+            setSearch={setSearch}
+            className="mr-1 mt-1 lg:w-8/12"
+          />
         )}
       </Row>
       <div className="flex-inline relative w-full items-center">
