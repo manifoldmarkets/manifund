474eda4719396832a556e0fec95b8dedc45d2978
rachelweinberg12 <rachel.weinberg12@gmail.com>
2023-11-14 12:58:32 -0800

    Link to cause page from create project form

--- components/site-link.tsx
+++ components/site-link.tsx
@@ -3,16 +3,17 @@ import { ReactNode } from 'react'
 import Link from 'next/link'
 
 export const linkClass =
-  'break-anywhere hover:underline hover:decoration-orange-500 hover:decoration-2'
+  'break-anywhere hover:underline hover:decoration-orange-600 hover:decoration-2 text-orange-600'
 
 export const SiteLink = (props: {
   href: string | undefined
+  target?: string
   children?: ReactNode
   onClick?: (event?: any) => void
   className?: string
   followsLinkClass?: boolean
 }) => {
-  const { href, children, onClick, className, followsLinkClass } = props
+  const { href, target, children, onClick, className, followsLinkClass } = props
 
   if (!href) return <>{children}</>
 
@@ -20,15 +21,10 @@ export const SiteLink = (props: {
     <Link
       href={href}
       className={clsx(followsLinkClass ? linkClass : '', className)}
-      target={href.startsWith('http') ? '_blank' : undefined}
+      target={target ?? href.startsWith('http') ? '_blank' : undefined}
       onClick={onClick}
     >
       {children}
     </Link>
   )
-}
-
-export const getLinkTarget = (href: string, newTab?: boolean) => {
-  if (href.startsWith('http')) return '_blank'
-  return newTab ? '_blank' : '_self'
 }
