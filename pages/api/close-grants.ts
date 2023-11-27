389ec430bc5752a0dd14cd80f023fd5e1de08fa5
rachelweinberg12 <rachel.weinberg12@gmail.com>
2023-11-27 14:44:48 -0500

    Add logs to close grants function for testing

--- pages/api/close-grants.ts
+++ pages/api/close-grants.ts
@@ -28,13 +28,15 @@ export default async function handler() {
   const now = new Date()
   const proposalsPastDeadline = proposals?.filter((project) => {
     const closeDate = new Date(`${project.auction_close}T23:59:59-12:00`)
+    console.log('project', project.title, closeDate, now)
+    console.log('is expired?', isBefore(closeDate, now))
     return (
-      isBefore(closeDate, now) &&
-      project.type === 'grant' &&
+      isBefore(closeDate, now) && project.type === 'grant'
       // Only send notifs once per week
-      differenceInDays(closeDate, now) % 7 === 0
+      // differenceInDays(closeDate, now) % 7 === 0
     )
   })
+  console.log('proposalsPastDeadline', proposalsPastDeadline)
   for (const project of proposalsPastDeadline ?? []) {
     await closeGrant(
       project,
