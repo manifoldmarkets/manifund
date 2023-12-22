20ce6c23984df0efc46c5d7abefbeea87909a550
rachelweinberg12 <rachel.weinberg12@gmail.com>
2023-12-21 18:04:15 -0800

    Fix compile error

--- pages/api/reactivate-project.ts
+++ pages/api/reactivate-project.ts
@@ -9,6 +9,9 @@ import { SupabaseClient } from '@supabase/supabase-js'
 export const config = {
   runtime: 'edge',
   regions: ['sfo1'],
+  unstable_allowDynamic: [
+    '**/node_modules/lodash/_root.js', // Use a glob to allow anything in the function-bind 3rd party module
+  ],
 }
 
 export default async function handler(req: NextRequest) {
