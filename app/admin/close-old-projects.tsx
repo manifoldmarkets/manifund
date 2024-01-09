b27ede179fa7611e64e37560907f00a9af3553d9
rachelweinberg12 <rachel.weinberg12@gmail.com>
2024-01-09 14:24:33 -0800

    Close old projects

---
+++ app/admin/close-old-projects.tsx
@@ -0,0 +1,23 @@
+'use client'
+import { Button } from '@/components/button'
+import { useSupabase } from '@/db/supabase-provider'
+import { updateProjectStage } from '@/db/project'
+
+export function CloseOldProjects(props: { projectIds: string[] }) {
+  const { projectIds } = props
+  const { supabase } = useSupabase()
+  return (
+    <Button
+      disabled
+      onClick={async () => {
+        await Promise.all(
+          projectIds.map((projectId) =>
+            updateProjectStage(supabase, projectId, 'complete')
+          )
+        )
+      }}
+    >
+      Close old projects
+    </Button>
+  )
+}
