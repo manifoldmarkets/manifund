b932c188f44db449f0c1f8ae3c52852d8aab8216
rachelweinberg12 <rachel.weinberg12@gmail.com>
2024-01-08 17:37:43 -0800

    Update investor notifs to specify creator update/final report

--- pages/api/comment-notifications.ts
+++ pages/api/comment-notifications.ts
@@ -8,6 +8,7 @@ import { getTxnsByProject } from '@/db/txn'
 import { parseMentions } from '@/utils/parse'
 import { Comment } from '@/db/comment'
 import { JSONContent } from '@tiptap/core'
+import { uniq } from 'lodash'
 import { TIPTAP_EXTENSIONS } from '@/components/editor'
 
 export default async function handler(
@@ -39,22 +40,53 @@ export default async function handler(
     )
   }
 
-  // Send investor email
-  if (
-    fullComment.profiles.id === fullComment.projects.creator &&
-    !comment.replying_to
-  ) {
+  // Send investor / donor emails
+  if (!comment.replying_to) {
     const txnsAndProfiles = await getTxnsByProject(
       supabaseAdmin,
       fullComment.projects.id
     )
-    const shareholders = getShareholders(txnsAndProfiles)
-    shareholders.forEach(async (shareholder) => {
-      if (shareholder.profile.id !== fullComment.projects.creator) {
+    const supporters =
+      fullComment.projects.type === 'cert'
+        ? getShareholders(txnsAndProfiles).map((s) => s.profile)
+        : txnsAndProfiles
+            .filter((txn) => txn.type === 'project donation')
+            .map((t) => t.profiles)
+    const supporterIds = supporters
+      .filter(
+        (supporter) =>
+          supporter && supporter?.id !== fullComment.projects.creator
+      )
+      .map((supporter) => supporter?.id)
+    const uniqueSupporterIds = uniq(supporterIds)
+    uniqueSupporterIds.forEach(async (supporterId) => {
+      if (comment.special_type === 'progress update') {
         await sendTemplateEmail(
-          TEMPLATE_IDS.CREATOR_UPDATE,
+          TEMPLATE_IDS.GENERIC_NOTIF,
+          {
+            notifText: `The creator of "${fullComment.projects.title}" has posted a progress update on their project. Check it out!`,
+            buttonUrl: `https://manifund.org/projects/${fullComment.projects.slug}?tab=comments#${comment.id}`,
+            buttonText: 'View project',
+            subject: `Manifund: Update posted for "${fullComment.projects.title}"`,
+          },
+          supporterId
+        )
+      } else if (comment.special_type === 'final report') {
+        await sendTemplateEmail(
+          TEMPLATE_IDS.GENERIC_NOTIF,
+          {
+            notifText: `The creator of "${fullComment.projects.title}" has completed their project and posted a final report. Check it out!`,
+            buttonUrl: `https://manifund.org/projects/${fullComment.projects.slug}?tab=comments#${comment.id}`,
+            buttonText: 'View project',
+            subject: `Manifund: Final report posted for "${fullComment.projects.title}"`,
+          },
+          supporterId
+        )
+      } else {
+        await sendTemplateEmail(
+          TEMPLATE_IDS.NEW_COMMENT,
           postmarkVars,
-          shareholder.profile.id
+          supporterId
         )
       }
     })
