2dbcf8dd3e60f8e9f40651a21f0b5e01f2830841
rachelweinberg12 <rachel.weinberg12@gmail.com>
2024-01-23 14:50:15 +0900

    Remove unused function

--- db/follows.ts
+++ db/follows.ts
@@ -1,10 +1,8 @@
 import { SupabaseClient } from '@supabase/supabase-js'
 import { Database } from './database.types'
-import { Profile } from './profile'
 
 export type ProjectFollow =
   Database['public']['Tables']['project_follows']['Row']
-export type ProjectFollowAndProfile = ProjectFollow & { profile: Profile }
 
 export async function getProjectFollowerIds(
   supabase: SupabaseClient,
@@ -19,18 +17,4 @@ export async function getProjectFollowerIds(
   }
   const followerIds = data.map((follow) => follow.follower_id) as string[]
   return followerIds
-}
-
-export async function getFullProjectFollowers(
-  supabase: SupabaseClient,
-  projectId: string
-) {
-  const { data, error } = await supabase
-    .from('project_follows')
-    .select('*, profiles(*)')
-    .eq('project_id', projectId)
-  if (error) {
-    throw error
-  }
-  return data as ProjectFollowAndProfile[]
 }
