3098de17d7dfa7d3b2465a6728a7dceca25e8ce6
rachelweinberg12 <rachel.weinberg12@gmail.com>
2024-01-23 15:08:05 +0900

    Fix progress update process so updates are recorded as such in the db

--- pages/api/post-comment.ts
+++ pages/api/post-comment.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { createEdgeClient } from './_db'
 import { JSONContent } from '@tiptap/react'
-import { sendComment } from '@/db/comment'
+import { sendComment, Comment } from '@/db/comment'
 
 export const config = {
   runtime: 'edge',
@@ -15,16 +15,25 @@ export const config = {
 type CommentProps = {
   content: JSONContent
   projectId: string
+  specialType: Comment['special_type']
   replyingTo?: string
 }
 
 export default async function handler(req: NextRequest) {
-  const { content, projectId, replyingTo } = (await req.json()) as CommentProps
+  const { content, projectId, replyingTo, specialType } =
+    (await req.json()) as CommentProps
   const supabase = createEdgeClient(req)
   const resp = await supabase.auth.getUser()
   const user = resp.data.user
   if (!user) return NextResponse.error()
-  await sendComment(supabase, content, projectId, user.id, replyingTo)
+  await sendComment(
+    supabase,
+    content,
+    projectId,
+    user.id,
+    replyingTo,
+    specialType
+  )
   const { error } = await supabase.rpc('follow_project', {
     project_id: projectId,
     follower_id: user.id,
