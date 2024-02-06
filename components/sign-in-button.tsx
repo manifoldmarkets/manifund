d70c3e7f4d1443081609e6795d1a4b087a935a0b
rachelweinberg12 <rachel.weinberg12@gmail.com>
2024-02-05 16:58:10 -0800

    Make sign in redirect to previous page

--- components/sign-in-button.tsx
+++ components/sign-in-button.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { buttonClass } from '@/components/button'
 import clsx from 'clsx'
 import Link from 'next/link'
@@ -6,7 +5,7 @@ import Link from 'next/link'
 export const SignInButton = () => {
   return (
     <Link
-      href="/login"
+      href={`/login?redirect=${window.location.pathname}`}
       className={clsx(
         buttonClass('xl', 'gradient'),
         'mx-auto mt-4 max-w-md bg-gradient-to-r'
