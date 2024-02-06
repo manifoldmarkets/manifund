db3280e6cbcc55a9c0e60371f2d81d3b107a55f2
rachelweinberg12 <rachel.weinberg12@gmail.com>
2024-02-06 15:42:01 -0800

    Add divider with header component

---
+++ components/divider-with-header.tsx
@@ -0,0 +1,12 @@
+export function DividerWithHeader(props: { header: string }) {
+  return (
+    <div className="relative">
+      <div className="absolute inset-0 flex items-center" aria-hidden="true">
+        <div className="w-full border-t border-gray-300" />
+      </div>
+      <div className="relative flex justify-center">
+        <span className="bg-gray-50 px-2 text-gray-500">{props.header}</span>
+      </div>
+    </div>
+  )
+}
