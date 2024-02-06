e000aa1d33ad1ba3b5bb8331537beb08caa48a2d
rachelweinberg12 <rachel.weinberg12@gmail.com>
2024-02-05 16:58:10 -0800

    Make sign in redirect to previous page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -6,10 +6,5 @@ export default async function Login() {
   const supabase = createServerClient()
   const user = await getUser(supabase)
   const profile = user ? await getProfileById(supabase, user?.id) : null
-  const redirectTo = profile
-    ? profile.id === profile.username
-      ? '/edit-profile'
-      : '/projects'
-    : undefined
-  return <Auth redirectTo={redirectTo} />
+  return <Auth profileComplete={!!profile} />
 }
