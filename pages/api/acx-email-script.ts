f0369ec126973dab6643a81216910a4cc50d271b
rachelweinberg12 <rachel.weinberg12@gmail.com>
2024-02-07 13:11:55 -0800

    Add html email content to script

--- pages/api/acx-email-script.ts
+++ pages/api/acx-email-script.ts
@@ -21,16 +21,106 @@ export default async function handler() {
     if (transfer.projects.type === 'grant') {
       if (recipientExists) {
         // email A
-        const htmlContent = `<div><span>Dear ${transfer.recipient_name},</span><br /><br /><span>Congratulations on receiving an ACX Grant! Your ACX project has now been added to your Manifund account. Please sign the grant agreement (link at the top of your project page) and then withdraw your funds using <a href='https://airtable.com/appOfJtzt8yUTBFcD/shrI3XFPivduhbnG'>this form </a>(also available from your profile page).</span><br /><br /><span>If you have any questions, you can reply to this email or join us on Discord!</span><br /><span>Best,</span><br /><br /><span>Rachel from Manifund</span></div>`
+        const htmlContent = `<div>
+        <p>Dear ${transfer.recipient_name},</p>
+        <p>
+          Congratulations on receiving an ACX Grant! Please sign the grant agreement
+          (link at the top of your project page) and then withdraw your funds using<a
+            href="https://airtable.com/appOfJtzt8yUTBFcD/shrI3XFPivduhbnG"
+          >
+            this form </a
+          >(also available from your profile page).
+        </p>
+        <p>
+          Note that your project has it’s own public page which other users can
+          comment on or use to donate to your work. If you had any privacy conditions
+          on your application to ACX Grants, we’ve honored those in creating this
+          page, though feel free to edit your project entry however you like.
+        </p>
+        <p>
+          If you have any questions, you can reply to this email or join us on
+          <a href="https://discord.gg/ZGsDMWSA5Q">Discord</a>!
+        </p>
+        <p>Best,</p>
+        <p>Rachel from Manifund</p>
+      </div>`
       } else {
         // email B
+        const htmlContent = `<div>
+        <p>Dear ${transfer.recipient_name},</p>
+        <p>
+          Congratulations on receiving an ACX Grant! We’ve created an entry for your
+          project on Manifund, which you’ll need to sign in to accept. After creating
+          an account, go to your project page to sign your grant agreement. Lastly,
+          give us your payment details using<a
+            href="https://airtable.com/appOfJtzt8yUTBFcD/shrI3XFPivduhbnG"
+          >
+            this form </a
+          >(also available from your profile page).
+        </p>
+        <p>
+          Note that your project has it’s own public page which other users can
+          comment on or use to donate to your work. If you had any privacy conditions
+          on your application to ACX Grants, we’ve honored those in creating this
+          page, though feel free to edit your project entry however you like.
+        </p>
+        <p>
+          If you have any questions, you can reply to this email or join us on
+          <a href="https://discord.gg/ZGsDMWSA5Q">Discord</a>!
+        </p>
+        <p>Best,</p>
+        <p>Rachel from Manifund</p>
+      </div>`
       }
       // update project stage to proposal
     } else if (transfer.projects.type === 'dummy') {
       if (recipientExists) {
         // email C
+        const htmlContent = `<div>
+        <p>Dear ${transfer.recipient_name},</p>
+        <p>
+          Congratulations on receiving an ACX Grant! Even though Manifund is not
+          handling the payments for your project, we’ve created an entry for this
+          project on Manifund under your account.
+        </p>
+        <p>
+          Note that your project has it’s own public page which other users can
+          comment on or use to donate to your work. If you had any privacy conditions
+          on your application to ACX Grants, we’ve honored those in creating this
+          page, though feel free to edit your project entry however you like.
+        </p>
+        <p>
+          If you have any questions, you can reply to this email or join us on
+          <a href="https://discord.gg/ZGsDMWSA5Q">Discord</a>!
+        </p>
+        <p>Best,</p>
+        <p>Rachel from Manifund</p>
+      </div>`
       } else {
         // email D
+        const htmlContent = `<div>
+        <p>Dear ${transfer.recipient_name},</p>
+        <p>
+          Congratulations on receiving an ACX Grant! Even though Manifund is not
+          handling the payments for your project, we’ve created a page on our site for
+          your project where other users can read about your work or comment. If you’d
+          like to get notified about comments or participate in the discussion, you
+          can create an account on Manifund using this email address, and the project
+          will be automatically transferred to your account.
+        </p>
+        <p>
+          Note that your project has it’s own public page which other users can
+          comment on or use to donate to your work. If you had any privacy conditions
+          on your application to ACX Grants, we’ve honored those in creating this
+          page, though feel free to edit your project entry however you like.
+        </p>
+        <p>
+          If you have any questions, you can reply to this email or join us on
+          <a href="https://discord.gg/ZGsDMWSA5Q">Discord</a>!
+        </p>
+        <p>Best,</p>
+        <p>Rachel from Manifund</p>
+      </div>`
       }
       // update project stage to active
     }
