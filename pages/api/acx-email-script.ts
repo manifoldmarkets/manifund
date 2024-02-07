f1bfdc212002b29d1d6731650584ff0a3f3ee330
rachelweinberg12 <rachel.weinberg12@gmail.com>
2024-02-07 13:26:58 -0800

    Simplify email function structure

--- pages/api/acx-email-script.ts
+++ pages/api/acx-email-script.ts
@@ -1,6 +1,7 @@
 import { createAdminClient } from './_db'
 import { getIncompleteTransfers } from '@/db/project-transfer'
 import { SupabaseClient } from '@supabase/supabase-js'
+import { sendTemplateEmail, TEMPLATE_IDS } from '@/utils/email'
 
 export const config = {
   runtime: 'edge',
@@ -18,66 +19,108 @@ export default async function handler() {
       supabase,
       transfer.recipient_email
     )
+    const isGrant = transfer.projects.type === 'grant'
+    const emailSubject = isGrant
+      ? 'Next steps for receiving your ACX Grant'
+      : 'Your ACX Grant project is now on Manifund'
+    const emailHtmlContent = getEmailHtmlContent(
+      isGrant,
+      recipientExists,
+      transfer.recipient_name
+    )
+    await sendTemplateEmail(TEMPLATE_IDS.GENERIC_NOTIF_HTML, {
+      subject: emailSubject,
+      htmlContent: emailHtmlContent,
+      buttonUrl: 'buttonUrl_Value',
+      buttonText: 'buttonText_Value',
+    })
+    if (recipientExists) {
+      // transfer project to them
+    }
     if (transfer.projects.type === 'grant') {
-      if (recipientExists) {
-        // email A
-        const htmlContent = `<div>
-        <p>Dear ${transfer.recipient_name},</p>
-        <p>
-          Congratulations on receiving an ACX Grant! Please sign the grant agreement
-          (link at the top of your project page) and then withdraw your funds using<a
-            href="https://airtable.com/appOfJtzt8yUTBFcD/shrI3XFPivduhbnG"
-          >
-            this form </a
-          >(also available from your profile page).
-        </p>
-        <p>
-          Note that your project has it’s own public page which other users can
-          comment on or use to donate to your work. If you had any privacy conditions
-          on your application to ACX Grants, we’ve honored those in creating this
-          page, though feel free to edit your project entry however you like.
-        </p>
-        <p>
-          If you have any questions, you can reply to this email or join us on
-          <a href="https://discord.gg/ZGsDMWSA5Q">Discord</a>!
-        </p>
-        <p>Best,</p>
-        <p>Rachel from Manifund</p>
-      </div>`
-      } else {
-        // email B
-        const htmlContent = `<div>
-        <p>Dear ${transfer.recipient_name},</p>
-        <p>
-          Congratulations on receiving an ACX Grant! We’ve created an entry for your
-          project on Manifund, which you’ll need to sign in to accept. After creating
-          an account, go to your project page to sign your grant agreement. Lastly,
-          give us your payment details using<a
-            href="https://airtable.com/appOfJtzt8yUTBFcD/shrI3XFPivduhbnG"
-          >
-            this form </a
-          >(also available from your profile page).
-        </p>
-        <p>
-          Note that your project has it’s own public page which other users can
-          comment on or use to donate to your work. If you had any privacy conditions
-          on your application to ACX Grants, we’ve honored those in creating this
-          page, though feel free to edit your project entry however you like.
-        </p>
-        <p>
-          If you have any questions, you can reply to this email or join us on
-          <a href="https://discord.gg/ZGsDMWSA5Q">Discord</a>!
-        </p>
-        <p>Best,</p>
-        <p>Rachel from Manifund</p>
-      </div>`
-      }
       // update project stage to proposal
-    } else if (transfer.projects.type === 'dummy') {
-      if (recipientExists) {
-        // email C
-        const htmlContent = `<div>
-        <p>Dear ${transfer.recipient_name},</p>
+    } else {
+      // update project stage to active
+    }
+  }
+}
+
+async function doesUserWithEmailExist(
+  supabaseAdmin: SupabaseClient,
+  email: string
+) {
+  const { data, error } = await supabaseAdmin
+    .from('users')
+    .select('id')
+    .eq('email', email)
+    .maybeSingle()
+  if (error) {
+    console.log(error)
+  }
+  return !!data
+}
+
+function getEmailHtmlContent(
+  isGrant: boolean,
+  recipientExists: boolean,
+  recipientName: string
+) {
+  if (isGrant) {
+    if (recipientExists) {
+      return `<div>
+      <p>Dear ${recipientName},</p>
+      <p>
+        Congratulations on receiving an ACX Grant! Please sign the grant agreement
+        (link at the top of your project page) and then withdraw your funds using<a
+          href="https://airtable.com/appOfJtzt8yUTBFcD/shrI3XFPivduhbnG"
+        >
+          this form </a
+        >(also available from your profile page).
+      </p>
+      <p>
+        Note that your project has it’s own public page which other users can
+        comment on or use to donate to your work. If you had any privacy conditions
+        on your application to ACX Grants, we’ve honored those in creating this
+        page, though feel free to edit your project entry however you like.
+      </p>
+      <p>
+        If you have any questions, you can reply to this email or join us on
+        <a href="https://discord.gg/ZGsDMWSA5Q">Discord</a>!
+      </p>
+      <p>Best,</p>
+      <p>Rachel from Manifund</p>
+    </div>`
+    } else {
+      return `<div>
+      <p>Dear ${recipientName},</p>
+      <p>
+        Congratulations on receiving an ACX Grant! We’ve created an entry for your
+        project on Manifund, which you’ll need to sign in to accept. After creating
+        an account, go to your project page to sign your grant agreement. Lastly,
+        give us your payment details using<a
+          href="https://airtable.com/appOfJtzt8yUTBFcD/shrI3XFPivduhbnG"
+        >
+          this form </a
+        >(also available from your profile page).
+      </p>
+      <p>
+        Note that your project has it’s own public page which other users can
+        comment on or use to donate to your work. If you had any privacy conditions
+        on your application to ACX Grants, we’ve honored those in creating this
+        page, though feel free to edit your project entry however you like.
+      </p>
+      <p>
+        If you have any questions, you can reply to this email or join us on
+        <a href="https://discord.gg/ZGsDMWSA5Q">Discord</a>!
+      </p>
+      <p>Best,</p>
+      <p>Rachel from Manifund</p>
+    </div>`
+    }
+  } else {
+    if (recipientExists) {
+      return `<div>
+        <p>Dear ${recipientName},</p>
         <p>
           Congratulations on receiving an ACX Grant! Even though Manifund is not
           handling the payments for your project, we’ve created an entry for this
@@ -96,10 +139,9 @@ export default async function handler() {
         <p>Best,</p>
         <p>Rachel from Manifund</p>
       </div>`
-      } else {
-        // email D
-        const htmlContent = `<div>
-        <p>Dear ${transfer.recipient_name},</p>
+    } else {
+      return `<div>
+        <p>Dear ${recipientName},</p>
         <p>
           Congratulations on receiving an ACX Grant! Even though Manifund is not
           handling the payments for your project, we’ve created a page on our site for
@@ -121,23 +163,6 @@ export default async function handler() {
         <p>Best,</p>
         <p>Rachel from Manifund</p>
       </div>`
-      }
-      // update project stage to active
     }
   }
-}
-
-async function doesUserWithEmailExist(
-  supabaseAdmin: SupabaseClient,
-  email: string
-) {
-  const { data, error } = await supabaseAdmin
-    .from('users')
-    .select('id')
-    .eq('email', email)
-    .maybeSingle()
-  if (error) {
-    console.log(error)
-  }
-  return !!data
 }
