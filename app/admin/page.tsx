7a59986af60226d2a38640d9f18ce14e351538e1
Austin Chen <akrolsmir@gmail.com>
2024-02-15 12:56:19 -0800

    Link to writeup & Supabase entry from admin page

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -19,6 +19,7 @@ import { ActivateProject } from './activate-project'
 import { CreateTxn } from './create-txn'
 import clsx from 'clsx'
 import Link from 'next/link'
+import { CircleStackIcon } from '@heroicons/react/24/solid'
 
 export default async function Admin() {
   const supabase = createServerClient()
@@ -188,6 +189,7 @@ export default async function Admin() {
       <Table>
         <thead>
           <tr>
+            <th className="p-2">DB</th>
             <th className="p-2">Title</th>
             <th className="p-2">Creator</th>
             <th className="p-2">Min funding</th>
@@ -199,7 +201,21 @@ export default async function Admin() {
           {(projects ?? []).map((project) => {
             return (
               <tr key={project.id}>
-                <td className="max-w-sm overflow-hidden">{project.title}</td>
+                <td className="pr-2">
+                  <Link
+                    href={`https://supabase.com/dashboard/project/fkousziwzbnkdkldjper/editor/27111?filter=id%3Aeq%3A${project.id}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-blue-500 hover:underline"
+                  >
+                    <CircleStackIcon className="inline h-3 w-3" />
+                  </Link>
+                </td>
+                <td className="max-w-sm overflow-hidden hover:underline">
+                  <Link href={`/projects/${project.slug}`}>
+                    {project.title}
+                  </Link>
+                </td>
                 <td>{getName(project.creator)}</td>
                 <td>{project.min_funding}</td>
                 <AddTags
