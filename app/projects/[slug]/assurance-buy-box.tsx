647144ebd45906f2ef82470904014bc39ecc4e7e
rachelweinberg12 <rachel.weinberg12@gmail.com>
2024-02-23 16:47:02 -0500

    Add valuation box and errors

--- app/projects/[slug]/assurance-buy-box.tsx
+++ app/projects/[slug]/assurance-buy-box.tsx
@@ -6,7 +6,11 @@ import { Row } from '@/components/layout/row'
 import { Slider } from '@/components/slider'
 import { Tooltip } from '@/components/tooltip'
 import { Project } from '@/db/project'
-import { formatMoneyPrecise, formatPercent } from '@/utils/formatting'
+import {
+  formatMoney,
+  formatMoneyPrecise,
+  formatPercent,
+} from '@/utils/formatting'
 import { getProposalValuation } from '@/utils/math'
 import { useRouter } from 'next/navigation'
 import { useState } from 'react'
@@ -26,7 +30,11 @@ export function AssuranceBuyBox(props: {
   const offerSizePercent = valuation ? offerSizeDollars / valuation : 0
   const router = useRouter()
   let errorMessage = null
-  if (amount && amount > maxBuy) {
+  if (!amount) {
+    errorMessage = 'Please enter an amount.'
+  } else if (!valuation) {
+    errorMessage = 'Please enter a valuation.'
+  } else if (amount && amount > maxBuy) {
     errorMessage = `You don't have enough funds to buy $${amount}. You can buy up to $${maxBuy} worth.`
   } else if (amount && amount > offerSizeDollars) {
     errorMessage = `You can't buy more than ${formatPercent(
@@ -57,47 +65,69 @@ export function AssuranceBuyBox(props: {
       <div>
         <h2 className="text-lg font-bold">Place a buy offer</h2>
         <p className="text-sm text-gray-500">
-          Offer to buy equity directly from the founder at the initial
-          valuation. This order will only go through once the founder has
-          received enough buy offers to cover their minimum costs.
+          Offer to buy equity directly from the founder at{' '}
+          {activeAuction
+            ? 'or above the minimum valuation'
+            : 'the initial valuation'}
+          . This order will only go through once the founder has received enough
+          buy offers to cover their minimum costs.
         </p>
       </div>
       {maxBuy > 0 ? (
-        <Col>
-          <Row className="w-full items-center gap-2">
-            $
-            <AmountInput
-              amount={amount}
-              placeholder="Amount (USD)"
-              onChangeAmount={setAmount}
-              className="w-24 max-w-full"
-            />
-            <Slider
-              amount={amount ?? 0}
-              onChange={setAmount}
-              min={0}
-              max={offerSizeDollars}
-              marks={[
-                { label: formatPercent(0), value: 0 },
-                {
-                  label: formatPercent(offerSizePercent / 4),
-                  value: offerSizeDollars / 4,
-                },
-                {
-                  label: formatPercent(offerSizePercent / 2),
-                  value: offerSizeDollars / 2,
-                },
-                {
-                  label: formatPercent((offerSizePercent / 4) * 3),
-                  value: (offerSizeDollars / 4) * 3,
-                },
-                {
-                  label: formatPercent(offerSizePercent),
-                  value: offerSizeDollars,
-                },
-              ]}
-            />
-          </Row>
+        <Col className="gap-2">
+          {activeAuction && (
+            <Col>
+              <label className="font-medium">Valuation</label>
+              <Row className="items-center gap-2">
+                $
+                <AmountInput
+                  amount={valuation}
+                  onChangeAmount={setValuation}
+                  error={valuation ? valuation < minValuation : undefined}
+                  errorMessage={`Valuation must be at least $${minValuation}.`}
+                  placeholder={`${formatMoney(minValuation)}+`}
+                  className="w-32"
+                />
+              </Row>
+            </Col>
+          )}
+          <Col>
+            <label className="font-medium">Amount</label>
+            <Row className="w-full items-center gap-2">
+              $
+              <AmountInput
+                amount={amount}
+                placeholder={'100'}
+                onChangeAmount={setAmount}
+                className="w-24 max-w-full"
+              />
+              <Slider
+                amount={amount ?? 0}
+                onChange={setAmount}
+                min={0}
+                max={offerSizeDollars}
+                marks={[
+                  { label: formatPercent(0), value: 0 },
+                  {
+                    label: formatPercent(offerSizePercent / 4),
+                    value: offerSizeDollars / 4,
+                  },
+                  {
+                    label: formatPercent(offerSizePercent / 2),
+                    value: offerSizeDollars / 2,
+                  },
+                  {
+                    label: formatPercent((offerSizePercent / 4) * 3),
+                    value: (offerSizeDollars / 4) * 3,
+                  },
+                  {
+                    label: formatPercent(offerSizePercent),
+                    value: offerSizeDollars,
+                  },
+                ]}
+              />
+            </Row>
+          </Col>
         </Col>
       ) : (
         <span className="w-full text-center italic text-gray-600">
