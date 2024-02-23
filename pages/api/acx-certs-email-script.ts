254a955c0bfeeeb96ba325db39f11c2e172e36f4
rachelweinberg12 <rachel.weinberg12@gmail.com>
2024-02-23 16:02:35 -0500

    Remove more unused code from acx cert script

--- pages/api/acx-certs-email-script.ts
+++ pages/api/acx-certs-email-script.ts
@@ -2,7 +2,6 @@ import { createAdminClient } from './_db'
 import { getIncompleteTransfers } from '@/db/project-transfer'
 import { SupabaseClient } from '@supabase/supabase-js'
 import { sendTemplateEmail, TEMPLATE_IDS } from '@/utils/email'
-import { updateProjectStage } from '@/db/project'
 
 export const config = {
   runtime: 'edge',
@@ -21,28 +20,6 @@ export default async function handler() {
     console.log('user id: ', userId)
     const recipientExists = !!userId
     console.log('recipient exists: ', recipientExists)
-    const isGrant = transfer.projects.type === 'grant'
-    const emailHtmlContent = getEmailHtmlContent(
-      recipientExists,
-      transfer.recipient_name
-    )
-    console.log('about to send email!')
-    console.log(emailHtmlContent)
-    await sendTemplateEmail(
-      TEMPLATE_IDS.GENERIC_NOTIF_HTML,
-      {
-        subject: isGrant
-          ? 'Next steps for receiving your ACX Grant'
-          : 'Your ACX Grant project is now on Manifund',
-        htmlContent: emailHtmlContent,
-        buttonUrl: recipientExists
-          ? `https://manifund.org/projects/${transfer.projects.slug}`
-          : `https://manifund.org/login?email=${transfer.recipient_email}`,
-        buttonText: recipientExists ? 'View your project' : 'Create an account',
-      },
-      undefined,
-      transfer.recipient_email
-    )
     if (recipientExists) {
       console.log('recipient exists, transferring project')
       let args = {
@@ -52,11 +29,26 @@ export default async function handler() {
       }
       await supabase.rpc('_transfer_project', args).throwOnError()
     }
-    console.log('updating project stage to ', isGrant ? 'proposal' : 'active')
-    await updateProjectStage(
-      supabase,
-      transfer.project_id,
-      isGrant ? 'proposal' : 'active'
+    const emailHtmlContent = getEmailHtmlContent(
+      recipientExists,
+      transfer.recipient_name
+    )
+    console.log('about to send email!')
+    console.log(emailHtmlContent)
+    await sendTemplateEmail(
+      TEMPLATE_IDS.GENERIC_NOTIF_HTML,
+      {
+        subject: recipientExists
+          ? 'Your ACX impact certificate has been added to your Manifund account.'
+          : 'Your ACX impact certificate is on Manifund. Sign in to accept',
+        htmlContent: emailHtmlContent,
+        buttonUrl: recipientExists
+          ? `https://manifund.org/projects/${transfer.projects.slug}`
+          : `https://manifund.org/login?email=${transfer.recipient_email}`,
+        buttonText: recipientExists ? 'View your project' : 'Create an account',
+      },
+      undefined,
+      transfer.recipient_email
     )
     console.log('------------done with transfer----------')
   }
@@ -79,11 +71,14 @@ async function getUserIdFromEmail(
 }
 
 function getEmailHtmlContent(recipientExists: boolean, recipientName: string) {
-  if (recipientExists) {
-    return `<div>
+  return `<div>
       <p>Hi ${recipientName},</p>
       <p>
-      An impact certificate for your ACX application has been added to your Manifund account. It’s currently in draft mode, which means only you can see it. Feel free to make whatever edits to the description and investment structure that you like before publishing.
+      ${
+        recipientExists
+          ? 'An impact certificate for your ACX application has been added to your Manifund account.'
+          : 'An impact certificate for your ACX application has been added to Manifund. Create an account with this email to accept and get access to the project.'
+      } It'll start out in draft mode, which means only you can see it. Once you publish, all information in the description and on your profile will be publicly accessible, so make sure to make any important changes before publishing.
       </p>
       <p>
       Next week Scott will post about this on ACX and investors will start placing offers on projects. You can read more about how initial funding will work <a href="https://www.notion.so/manifoldmarkets/ACX-Impact-Market-Process-and-Mechanisms-aa71000b9932440ba82a4697a7bcc233?pvs=4">here</a>.
@@ -97,22 +92,4 @@ function getEmailHtmlContent(recipientExists: boolean, recipientName: string) {
       <p>Best,</p>
       <p>Rachel from Manifund</p>
     </div>`
-  } else {
-    return `<div>
-    <p>Hi ${recipientName},</p>
-    <p>
-    An impact certificate for your ACX application has been added to Manifund. Create an account with this email to accept and get access to the project. It'll start out in draft mode, which means only you can see it. Once you publish, all information in the description and on your profile will be publicly accessible, so make sure to make any important changes before publishing.</p>
-    <p>
-    Next week Scott will post about this on ACX and investors will start placing offers on projects. You can read more about how initial funding will work <a href="https://www.notion.so/manifoldmarkets/ACX-Impact-Market-Process-and-Mechanisms-aa71000b9932440ba82a4697a7bcc233?pvs=4">here</a>.
-    </p>
-    <p>
-    If you would not like to participate in the impact certificate portion of the round, no need to take any action. If you’re still unsure, you can read about impact markets <a href="https://www.brasstacks.blog/explain-im/">here</a> or <a href="https://www.astralcodexten.com/p/impact-markets-the-annoying-details">here</a>.
-    </p>
-    <p>
-      You can also ask us questions by replying to this email or joining us on <a href="https://discord.gg/ZGsDMWSA5Q">Discord</a>!
-    </p>
-    <p>Best,</p>
-    <p>Rachel from Manifund</p>
-  </div>`
-  }
 }
