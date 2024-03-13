ac9421b12938103de7d2f1ca551d0dd5f871b67a
Austin Chen <akrolsmir@gmail.com>
2024-03-13 08:31:30 -0700

    Increase padding on loading skeleton

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,7 +2,7 @@ export default function Loading() {
   // return <div>Loading...</div>
   // Return a fancier loading screen using Tailwind's skeleton loaders
   return (
-    <div className="animate-pulse">
+    <div className="animate-pulse p-8">
       <div className="mb-4 h-4 w-3/4 rounded bg-gray-300"></div>
       <div className="mb-4 h-4 w-1/2 rounded bg-gray-300"></div>
       <div className="mb-4 h-4 w-5/6 rounded bg-gray-300"></div>
--- app/projects/[slug]/loading.tsx
+++ app/projects/[slug]/loading.tsx
@@ -1,7 +1,7 @@
 export default function Loading() {
   return (
     <div>
-      <div className="flex animate-pulse space-x-4">
+      <div className="flex animate-pulse space-x-4 p-8">
         <div className="h-10 w-10 rounded-full bg-slate-200"></div>
         <div className="flex-1 space-y-6 py-1">
           <div className="h-2 rounded bg-slate-200"></div>
