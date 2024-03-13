d08877b863979391afdb48ccf41bb26496cb9941
Lily Jordan <None>
2024-03-13 18:03:00 -0400

    create and link to About page for open-call applications

--- app/about/open-call/page.tsx
+++ app/about/open-call/page.tsx
@@ -1,123 +1,67 @@
-export default function OpenCallPage() {
+import Image from 'next/image'
+import Link from 'next/link'
+import { createServerClient } from '@/db/supabase-server'
+import { listCauses } from '@/db/cause'
+
+
+export default async function OpenCallPage() {
+  const supabase = createServerClient()
+  const causesList = await listCauses(supabase)
+  const nonPrizeCauses = causesList.filter(
+    (cause) => cause.open && !cause.prize
+  )
   return (
     <div className="p-5">
       <div className="prose mx-auto font-light">
         <h1>Funding Your Project on Manifund</h1>
         <p>
-          Regranting is a method of giving money whereby a charitable donor
-          delegates a grantmaking budget to different individuals known as
-          “regrantors”. Regrantors are then empowered to make grant decisions
-          based on the objectives of the original donor.
+          Anyone can create a public proposal on Manifund for grant funding. If you&apos;d like to apply, follow these steps:
         </p>
+        <h2>1. Start a new project</h2>
         <p>
-          This model was pioneered by the FTX Future Fund; among the grantmaking
-          models they experimented with in 2022, they{' '}
-          <a href="https://forum.effectivealtruism.org/posts/paMYXYFYbbjpdjgbt/future-fund-june-2022-update#Expectations_vs__reality">
-            considered regranting to be the most promising.
-          </a>
+          Create an proposal <Link href="/create">here</Link>.
+          </p>
+        <p>
+          Most grants on Manifund are related to one of the following causes:
         </p>
-        <h3 className="text-2xl font-bold">Our regrantors</h3>
-      </div>
-      <div className="prose mx-auto font-light">
-        <h3>Why regranting?</h3>
         <ul>
-          <li>
-            Regranting surfaces opportunities that donors and large grantmaking
-            organizations might otherwise miss, as regrantors can tap into their
-            personal networks and fields of expertise.
-          </li>
-          <li>
-            The regranting model requires less overhead than traditional
-            grantmaking, as one person is responsible for the budget rather than
-            a committee. This allows for faster grant turnaround times, solving
-            a key pain point for grantees.
-          </li>
-          <li>
-            Certain cause areas like longtermism can be speculative, opaque, and
-            nascent, making it harder for donors to know where to direct their
-            money. Regranting allows donors to outsource these decisions to
-            trustworthy individuals.
-          </li>
+          {nonPrizeCauses.map((cause) => <li key={cause.title}>{cause.title}</li>)}
         </ul>
-        <h3>How does regranting on Manifund work?</h3>
-        <p>Our website makes the regranting process simple and transparent:</p>
-        <ol>
-          <li>
-            A donor contributes money to Manifold for Charity, a registered
-            501c3 nonprofit, when they add money to their Manifund account.
-          </li>
-          <li>
-            The donor can then allocate the money between regrantors of their
-            choice (or they can give directly to projects).
-          </li>
-          <li>
-            Regrantors choose which opportunities, including projects posted on
-            Manifund through our open call or projects they learn about
-            elsewhere, to spend their budgets on, writing up an explanation for
-            each grant made.
-          </li>
-          <li>
-            Manifold for Charity reviews the grant to make sure it is
-            legitimate, legal, and aligned with our mission.
-          </li>
-          <li>
-            If we approve the grant, the money will be transferred to the
-            grantee&apos;s Manifund account, at which point they request to
-            withdraw and we send them their funds.
-          </li>
-        </ol>
-        <h3>FAQ</h3>
-        <strong>Who can see the information about grants?</strong>
-        <br />
         <p>
-          Currently all grant information is made public. This includes the
-          identity of the regrantor and grant recipient, the project
-          description, the grant size, and the regrantor’s writeup.
+          However, you&apos;re welcome to submit an application for any public-benefit project, as long as it meets legal
+          requirements about what we can fund as a 501(c)(3).
         </p>
+        <h2>2. Complete the proposal form</h2>
         <p>
-          We strongly believe in transparency as it allows for meaningful public
-          feedback, accountability of decisions, and establishment of a
-          regrantor track records. We recognize that not all grants are suited
-          for publishing; for now, we recommend such grants be made through
-          other funders, such as the Long Term Future Fund, the Survival and
-          Flourishing Fund, or Open Philanthropy.
+        Write up a description of your project and set parameters around the amount of funding you&apos;re trying to raise and the deadline
+        for raising it. For examples and inspiration, you can browse the projects on the Manifund <Link href="/">homepage</Link>.
         </p>
-        <strong>What kinds of projects are eligible for regranting?</strong>
-        <br />
+        <h2>3. Sign the grant agreement</h2>
         <p>
-          We have no official cause-area restrictions on grants, though most of
-          our regrantors are focused on mitigating global catastrophic risk.
+        This is the legal paperwork stipulating the terms of any grant funding you receive. It can be found
+        on your project&apos;s page once you publish your proposal.
         </p>
+        <h2>4. Receive funding</h2>
         <p>
-          We support regrants to registered charities and individuals.
-          For-profit organizations may also be eligible, pending due diligence.
-          As a US-registered 501c3, we do not permit donations to political
-          campaigns.
+        If donors collectively offer your project the minimum funding amount you specify before the deadline,
+        then you&apos;ll receive payment through Manifund to start your project!
         </p>
-        <p>
-          We will look over all grants before fulfilling withdrawal requests to
-          make sure they meet these requirements. We reserve the right to veto
-          grants for any reason, though will strongly defer to regrantors’
-          judgement.
-        </p>
-        <strong>Can regrantors send money to themselves?</strong>
-        <br />
-        <p>
-          Regrantors are allowed to donate to their own projects, though we’ll
-          evaluate these projects with more scrutiny before fulfilling
-          withdrawal requests.
-        </p>
-        <strong>How do I become a regrantor?</strong>
-        <p>
-          Apply{' '}
-          <a
-            className="font-bold hover:underline"
-            href="https://airtable.com/appOfJtzt8yUTBFcD/shrZW7S069EmghCSV"
-          >
-            here
-          </a>
-          !
-        </p>
+        <div>
+          <Image
+            src="/ManifundProcessHorizontal.png"
+            alt="Manifund funding process diagram"
+            className="mx-auto hidden sm:block"
+            height={1000}
+            width={1000}
+          />
+          <Image
+            src="/ManifundProcessVertical.png"
+            alt="Manifund funding process diagram"
+            className="mx-auto sm:hidden"
+            height={1000}
+            width={1000}
+          />
+        </div>
       </div>
     </div>
   )
