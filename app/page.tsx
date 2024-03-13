11934e3193e9ec0e39e36eff9518217a0cf603ee
Austin Chen <akrolsmir@gmail.com>
2024-03-13 09:15:50 -0700

    Switch to a test homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,21 @@
-import Projects from './projects/page'
+import { Col } from '@/components/layout/col'
+import { ProjectCard } from '@/components/project-card'
+import { listProjects } from '@/db/project'
+import { createServerClient } from '@/db/supabase-server'
 
-export const revalidate = 60
+// export const revalidate = 60
 
-export default function Home(props: {
+export default async function Home(props: {
   // searchParams: { [key: string]: string | string[] | undefined }
 }) {
+  const supabase = createServerClient()
+  const projects = await listProjects(supabase)
+
   return (
-    // @ts-expect-error Server Component
-    <Projects
-    // searchParams={props.searchParams}
-    />
+    <Col className="gap-16 px-3 py-5 sm:px-6">
+      {projects.map((project) => (
+        <ProjectCard project={project} key={project.id} />
+      ))}
+    </Col>
   )
 }
