7e5925aec5e1d3de17a789427e757017414e51c5
Lily Jordan <None>
2024-03-13 12:56:37 -0400

    About page for open call

---
+++ app/about/open-call/page.tsx
@@ -0,0 +1,124 @@
+export default function OpenCallPage() {
+  return (
+    <div className="p-5">
+      <div className="prose mx-auto font-light">
+        <h1>Funding Your Project on Manifund</h1>
+        <p>
+          Regranting is a method of giving money whereby a charitable donor
+          delegates a grantmaking budget to different individuals known as
+          “regrantors”. Regrantors are then empowered to make grant decisions
+          based on the objectives of the original donor.
+        </p>
+        <p>
+          This model was pioneered by the FTX Future Fund; among the grantmaking
+          models they experimented with in 2022, they{' '}
+          <a href="https://forum.effectivealtruism.org/posts/paMYXYFYbbjpdjgbt/future-fund-june-2022-update#Expectations_vs__reality">
+            considered regranting to be the most promising.
+          </a>
+        </p>
+        <h3 className="text-2xl font-bold">Our regrantors</h3>
+      </div>
+      <div className="prose mx-auto font-light">
+        <h3>Why regranting?</h3>
+        <ul>
+          <li>
+            Regranting surfaces opportunities that donors and large grantmaking
+            organizations might otherwise miss, as regrantors can tap into their
+            personal networks and fields of expertise.
+          </li>
+          <li>
+            The regranting model requires less overhead than traditional
+            grantmaking, as one person is responsible for the budget rather than
+            a committee. This allows for faster grant turnaround times, solving
+            a key pain point for grantees.
+          </li>
+          <li>
+            Certain cause areas like longtermism can be speculative, opaque, and
+            nascent, making it harder for donors to know where to direct their
+            money. Regranting allows donors to outsource these decisions to
+            trustworthy individuals.
+          </li>
+        </ul>
+        <h3>How does regranting on Manifund work?</h3>
+        <p>Our website makes the regranting process simple and transparent:</p>
+        <ol>
+          <li>
+            A donor contributes money to Manifold for Charity, a registered
+            501c3 nonprofit, when they add money to their Manifund account.
+          </li>
+          <li>
+            The donor can then allocate the money between regrantors of their
+            choice (or they can give directly to projects).
+          </li>
+          <li>
+            Regrantors choose which opportunities, including projects posted on
+            Manifund through our open call or projects they learn about
+            elsewhere, to spend their budgets on, writing up an explanation for
+            each grant made.
+          </li>
+          <li>
+            Manifold for Charity reviews the grant to make sure it is
+            legitimate, legal, and aligned with our mission.
+          </li>
+          <li>
+            If we approve the grant, the money will be transferred to the
+            grantee&apos;s Manifund account, at which point they request to
+            withdraw and we send them their funds.
+          </li>
+        </ol>
+        <h3>FAQ</h3>
+        <strong>Who can see the information about grants?</strong>
+        <br />
+        <p>
+          Currently all grant information is made public. This includes the
+          identity of the regrantor and grant recipient, the project
+          description, the grant size, and the regrantor’s writeup.
+        </p>
+        <p>
+          We strongly believe in transparency as it allows for meaningful public
+          feedback, accountability of decisions, and establishment of a
+          regrantor track records. We recognize that not all grants are suited
+          for publishing; for now, we recommend such grants be made through
+          other funders, such as the Long Term Future Fund, the Survival and
+          Flourishing Fund, or Open Philanthropy.
+        </p>
+        <strong>What kinds of projects are eligible for regranting?</strong>
+        <br />
+        <p>
+          We have no official cause-area restrictions on grants, though most of
+          our regrantors are focused on mitigating global catastrophic risk.
+        </p>
+        <p>
+          We support regrants to registered charities and individuals.
+          For-profit organizations may also be eligible, pending due diligence.
+          As a US-registered 501c3, we do not permit donations to political
+          campaigns.
+        </p>
+        <p>
+          We will look over all grants before fulfilling withdrawal requests to
+          make sure they meet these requirements. We reserve the right to veto
+          grants for any reason, though will strongly defer to regrantors’
+          judgement.
+        </p>
+        <strong>Can regrantors send money to themselves?</strong>
+        <br />
+        <p>
+          Regrantors are allowed to donate to their own projects, though we’ll
+          evaluate these projects with more scrutiny before fulfilling
+          withdrawal requests.
+        </p>
+        <strong>How do I become a regrantor?</strong>
+        <p>
+          Apply{' '}
+          <a
+            className="font-bold hover:underline"
+            href="https://airtable.com/appOfJtzt8yUTBFcD/shrZW7S069EmghCSV"
+          >
+            here
+          </a>
+          !
+        </p>
+      </div>
+    </div>
+  )
+}
