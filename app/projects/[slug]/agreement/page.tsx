fd0a99418f8c244ae21783ed5fd6e75799a39605
rachelweinberg12 <rachel.weinberg12@gmail.com>
2024-03-22 15:23:55 -0700

    Get grant agreement onto the grant agreement page

--- app/projects/[slug]/agreement/page.tsx
+++ app/projects/[slug]/agreement/page.tsx
@@ -5,6 +5,8 @@ import React from 'react'
 import { Col } from '@/components/layout/col'
 import { GrantAgreement } from './grant-agreement'
 import { SignAgreement } from './sign-agreement'
+import { SupabaseClient } from '@supabase/supabase-js'
+import { Database } from '@/db/database.types'
 
 export default async function GrantAgreementPage(props: {
   params: { slug: string }
@@ -12,10 +14,12 @@ export default async function GrantAgreementPage(props: {
   const { slug } = props.params
   const supabase = createServerClient()
   const project = await getProjectAndProfileBySlug(supabase, slug)
-  const user = await getUser(supabase)
   if (!project || project.approved === false) {
     return <div>404</div>
   }
+  const grantAgreement = await getGrantAgreement(supabase, project.id)
+  const user = await getUser(supabase)
+
   return (
     <Col className="gap-5 p-5">
       <GrantAgreement project={project} />
@@ -25,4 +29,18 @@ export default async function GrantAgreementPage(props: {
       />
     </Col>
   )
+}
+
+type GrantAgreement =
+  Database['public']['Tables']['grant_agreements']['Row'] & {
+    profiles: { full_name: string; username: string }
+  }
+async function getGrantAgreement(supabase: SupabaseClient, projectId: string) {
+  const { data } = await supabase
+    .from('grant_agreements')
+    .select('*, profiles(full_name, username)')
+    .eq('project_id', projectId)
+    .maybeSingle()
+    .throwOnError()
+  return data ? (data as GrantAgreement) : null
 }
