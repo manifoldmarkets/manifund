05ddc36ff91011a0b68b9767dba38879ff22286d
Austin Chen <akrolsmir@gmail.com>
2024-03-25 11:45:43 -0700

    Update all deps (#122)
    * Update all deps
    
    * Ignore weird typescript error
    
    radix-ui/react-slider seems out of date with newer React types

--- components/investment-structure.tsx
+++ components/investment-structure.tsx
@@ -96,6 +96,7 @@ export function InvestmentStructurePanel(props: {
             </Row>
           )}
         </Row>
+        {/* @ts-ignore */}
         <RxSlider.Root
           className="relative mb-10 mt-5 flex h-5 touch-none select-none items-center"
           value={[founderPercent]}
@@ -110,7 +111,9 @@ export function InvestmentStructurePanel(props: {
           disabled={!editable}
           ref={sliderRef}
         >
+          {/* @ts-ignore */}
           <RxSlider.Track className="relative h-1 grow rounded-full bg-emerald-300">
+            {/* @ts-ignore */}
             <RxSlider.Range className="absolute h-full rounded-full bg-orange-300" />{' '}
             <div className="absolute left-2.5 right-2.5 h-full">
               {SLIDER_MARKS?.map(({ value, label }) => (
@@ -134,6 +137,7 @@ export function InvestmentStructurePanel(props: {
               ))}
             </div>
           </RxSlider.Track>
+          {/* @ts-ignore */}
           <RxSlider.Thumb
             className={clsx(
               editable
