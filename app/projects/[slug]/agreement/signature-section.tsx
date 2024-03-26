45acd308880225bde875587ce9163c24cfb72961
rachelweinberg12 <116405486+rachelweinberg12@users.noreply.github.com>
2024-03-26 13:54:19 -0700

    Improve agreement tracking (#121)
    * Add blank script file
    
    * Add grant agreements table
    
    * Add grant agreements table to db types
    
    * Fix grant agreements table entry in seed.sql
    
    * Finish draft of backlog script
    
    * Run script
    
    * Fix sidebar balance calculation
    
    * Add missing info
    
    * Add missing close dates
    
    * Center text in tags
    
    * Get grant agreement onto the grant agreement page
    
    * Add RLS to agreements table
    
    * Get grant agreement into client component
    
    * Pull some data from the grant agreement instead of from the project if signed already
    
    * Get agreement into sign agreement component
    
    * Add cursive font for signatures
    
    * Add signature display component
    
    * Connect signature display to signed state
    
    * Add Manifund signatory section
    
    * Change spacing
    
    * Update grant agreements table when when recipient signs
    
    * Add create grant agreement fxn
    
    * Fix spacing
    
    * Insert grant agreement when project is created
    
    * Create grant agreement entry only when necessary
    
    * Handle no agreement case on grant agreement page
    
    * Get current agreement version to agreement page
    
    * Only include projects where creator has signed agreement in the admin approval section
    
    * Change display of grant agreement depending on version
    
    * Update snaplet
    
    * Hide agreement when signed off site
    
    * Revert to real check of whether user is owner
    
    * Gen prod types
    
    * Change spacing at the bottom of grant agreement
    
    * Add awaiting counter signature state tag
    
    * Fix approval counter signature
    
    * Change "Manifund" signatory to "Charity" signatory
    
    * Delete extra code from backfill grant agreements script

---
+++ app/projects/[slug]/agreement/signature-section.tsx
@@ -0,0 +1,82 @@
+'use client'
+import { Button } from '@/components/button'
+import { Checkbox } from '@/components/input'
+import { Col } from '@/components/layout/col'
+import { Row } from '@/components/layout/row'
+import { GrantAgreement } from '@/db/grant_agreement'
+import { ProjectAndProfile } from '@/db/project'
+import { useRouter } from 'next/navigation'
+import { useState } from 'react'
+import { SignatureDisplay } from './signature-display'
+
+export function SignatureSection(props: {
+  project: ProjectAndProfile
+  agreement?: GrantAgreement
+  userIsOwner: boolean
+}) {
+  const { project, agreement, userIsOwner } = props
+  const [signedAt, setSignedAt] = useState(
+    agreement?.signed_at ? new Date(agreement.signed_at) : undefined
+  )
+  const [isSubmitting, setIsSubmitting] = useState(false)
+  const router = useRouter()
+  return (
+    <Col className="gap-10">
+      <SignatureDisplay
+        fullName={project.profiles.full_name}
+        signatoryTitle="Recipient"
+        signedAt={signedAt}
+      />
+      {!agreement?.signed_at && userIsOwner && (
+        <Row>
+          <Checkbox
+            id="terms"
+            aria-describedby="terms-description"
+            name="terms"
+            checked={!!signedAt}
+            onChange={(event) =>
+              setSignedAt(event.target.checked ? new Date() : undefined)
+            }
+          />
+          <div className="ml-3 text-sm leading-6">
+            <label htmlFor="terms" className="font-medium text-gray-900">
+              I, <strong>{project.profiles.full_name}</strong>, agree to the
+              terms of this grant as laid out in the above document.
+            </label>
+          </div>
+        </Row>
+      )}
+      {!project.signed_agreement && userIsOwner && (
+        <Row className="mb-10 justify-center">
+          <Button
+            onClick={async () => {
+              setIsSubmitting(true)
+              await fetch(`/api/sign-grant-agreement`, {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                  projectId: project.id,
+                }),
+              })
+              setIsSubmitting(false)
+              router.push(`/projects/${project.slug}`)
+            }}
+            loading={isSubmitting}
+            disabled={!signedAt}
+          >
+            Submit signature
+          </Button>
+        </Row>
+      )}
+      <SignatureDisplay
+        fullName={agreement?.profiles?.full_name ?? ''}
+        signatoryTitle="Charity signatory"
+        signedAt={
+          agreement?.approved_at ? new Date(agreement.approved_at) : undefined
+        }
+      />
+    </Col>
+  )
+}
