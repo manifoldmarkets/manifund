0ca30786f8a7f4c2c36be89cfc59f9b872e8a8f2
rachelweinberg12 <rachel.weinberg12@gmail.com>
2024-03-27 14:29:33 -0700

    Draft react to comment edge function

---
+++ pages/api/react-to-comment.ts
@@ -0,0 +1,33 @@
+import { NextRequest, NextResponse } from 'next/server'
+import { createEdgeClient } from './_db'
+
+export const config = {
+  runtime: 'edge',
+  regions: ['sfo1'],
+  // From https://github.com/lodash/lodash/issues/5525
+  unstable_allowDynamic: [
+    '**/node_modules/lodash/_root.js', // Use a glob to allow anything in the function-bind 3rd party module
+  ],
+}
+
+type RxnProps = {
+  commentId: string
+  reaction: string
+}
+
+export default async function handler(req: NextRequest) {
+  const { commentId, reaction } = (await req.json()) as RxnProps
+  const supabase = createEdgeClient(req)
+  const resp = await supabase.auth.getUser()
+  const user = resp.data.user
+  if (!user) return NextResponse.error()
+  const { error } = await supabase
+    .from('comment_rxns')
+    .insert({ comment_id: commentId, reaction, reactor_id: user.id })
+  if (error) {
+    console.error(error)
+    return NextResponse.error()
+  }
+  console.log(reaction)
+  return NextResponse.json('success')
+}
