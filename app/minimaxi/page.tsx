747363108c6af22e3bcadd1940eb144ed947bed8
Austin Chen <akrolsmir@gmail.com>
2024-04-01 12:08:51 -0700

    Redirect homepage to Minimaxi

--- app/minimaxi/page.tsx
+++ app/minimaxi/page.tsx
@@ -1,13 +1,19 @@
 import Maxifund from '../maxi/page'
 import Minifund from '../mini/page'
 import { ReactCompareSlider } from 'react-compare-slider'
+import Link from 'next/link'
+import { SiteLink } from '@/components/site-link'
 
 export default function MiniMaxi() {
   return (
     <div>
       {/* Banner across the top to note what this is */}
-      <div className="mb-2 w-full bg-gray-200 p-2 text-center text-sm font-light">
-        minifund or MAXIFUND - which should we use?
+      <div className="mb-2 w-full bg-orange-100 p-2 text-center text-sm font-light">
+        minifund vs MAXIFUND - A/B test our homepage! (vs{' '}
+        <SiteLink className="text-gray-500" href="/projects">
+          original
+        </SiteLink>
+        )
       </div>
 
       <ReactCompareSlider
