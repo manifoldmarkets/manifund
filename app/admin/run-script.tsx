85f23963e2decb229e6ea1716f991f779f8a406c
rachelweinberg12 <rachel.weinberg12@gmail.com>
2024-04-02 10:44:24 -0700

    Fix old txn type labels, some deposits mislabeled as profile donations

--- app/admin/run-script.tsx
+++ app/admin/run-script.tsx
@@ -6,7 +6,7 @@ export function RunScript() {
     <Button
       onClick={async () => {
         // Change script here
-        const response = await fetch('/api/change-acx-close-dates', {
+        const response = await fetch('/api/fix-txn-type-labels', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -14,7 +14,7 @@ export function RunScript() {
         })
       }}
     >
-      Change ACX close dates
+      Fix old txn type labels
     </Button>
   )
 }
