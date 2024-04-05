7a11577412f3763800f554af049ed284fc2e6b16
rachelweinberg12 <rachel.weinberg12@gmail.com>
2024-04-04 17:20:48 -0700

    Display close date as midnight PST

--- app/projects/[slug]/time-left-display.tsx
+++ app/projects/[slug]/time-left-display.tsx
@@ -2,11 +2,12 @@ import { Row } from '@/components/layout/row'
 import { differenceInDays, differenceInHours } from 'date-fns'
 
 export function TimeLeftDisplay(props: { closeDate: string }) {
-  // Close it on 23:59:59 in UTC -12 aka "Anywhere on Earth" time
-  const closeDate = new Date(`${props.closeDate}T23:59:59-12:00`)
+  // Close it at midnight PST
+  const closeDate = new Date(`${props.closeDate}T23:59:59-07:00`)
   const now = new Date()
   const daysLeft = differenceInDays(closeDate, now)
   const hoursLeft = daysLeft < 1 ? differenceInHours(closeDate, now) : 0
+  console.log('difference in hours', differenceInHours(closeDate, now))
   return (
     <Row className="items-center gap-1 text-sm text-gray-900">
       <span className="font-semibold">{`${Math.max(
