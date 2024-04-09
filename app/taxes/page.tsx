565165107b3f4eb364d90fc3b673308f179e35d0
rachelweinberg12 <rachel.weinberg12@gmail.com>
2024-04-09 16:02:08 -0700

    Print non-credit card deposits csv

--- app/taxes/page.tsx
+++ app/taxes/page.tsx
@@ -1,9 +1,10 @@
 import { createServerClient } from '@/db/supabase-server'
 import { FullProject, listProjects } from '@/db/project'
-import { getTxnsByUser } from '@/db/txn'
+import { getTxnsByUser, TxnAndProfiles } from '@/db/txn'
 import { isBefore } from 'date-fns'
 import { getAmountRaised } from '@/utils/math'
 import { formatMoney } from '@/utils/formatting'
+import { createAdminClient } from '@/pages/api/_db'
 
 // Note: These options make /projects static, but not when accessed from Home
 export const runtime = 'nodejs'
@@ -12,38 +13,55 @@ export const dynamic = 'force-static'
 export default async function Projects(props: {
   searchParams: { [key: string]: string | string[] | undefined }
 }) {
-  const supabase = createServerClient()
-  const projects = await listProjects(supabase)
-  const roundProjects = projects.filter(
-    (project) =>
-      project.rounds.title === 'Manifold Community Fund' &&
-      isBefore(new Date(project.created_at), new Date('2024-01-01'))
-  )
-  const projectTableData = await Promise.all(
-    roundProjects.map((project) => getProjectTableData(project))
-  )
-  const filteredProjectTableData = projectTableData.filter(
-    (data) => data.amountRaised > 0
-  )
-  const csvString = [
-    [
-      'Project title',
-      'Project ID',
-      'Creator name',
-      'Creator ID',
-      'Total raised 2023',
-      'Total withdrawn 2023',
-      'Location',
-    ],
-    ...filteredProjectTableData.map((data) => [...Object.values(data)]),
+  const supabase = createAdminClient()
+  // const projects = await listProjects(supabase)
+  // const roundProjects = projects.filter(
+  //   (project) =>
+  //     project.rounds.title === 'Manifold Community Fund' &&
+  //     isBefore(new Date(project.created_at), new Date('2024-01-01'))
+  // )
+  // const projectTableData = await Promise.all(
+  //   roundProjects.map((project) => getProjectTableData(project))
+  // )
+  // const filteredProjectTableData = projectTableData.filter(
+  //   (data) => data.amountRaised > 0
+  // )
+  // const roundCsvString = [
+  //   [
+  //     'Project title',
+  //     'Project ID',
+  //     'Creator name',
+  //     'Creator ID',
+  //     'Total raised 2023',
+  //     'Total withdrawn 2023',
+  //     'Location',
+  //   ],
+  //   ...filteredProjectTableData.map((data) => [...Object.values(data)]),
+  // ]
+  //   .map((e) => e.join(','))
+  //   .join('\n')
+  // console.log('=================================')
+  // console.log(roundCsvString)
+
+  const deposits = await get2023Deposits(supabase)
+  console.log(deposits.length)
+  const depositsCsvString = [
+    ['Txn ID', 'Recipient name', 'Recipient ID', 'Amount', 'Created at'],
+    ...deposits.map((txn) => [
+      txn.id,
+      txn.profiles?.full_name,
+      txn.to_id,
+      txn.amount,
+      txn.created_at,
+    ]),
   ]
     .map((e) => e.join(','))
     .join('\n')
   console.log('=================================')
-  console.log(csvString)
+  console.log(depositsCsvString)
   return (
     <div className="p-5">
-      {filteredProjectTableData.map((data) => (
+      {/* {filteredProjectTableData.map((data) => (
         <div className="grid grid-cols-8">
           <span>{data.title}</span>
           <span>{data.id}</span>
@@ -54,20 +72,29 @@ export default async function Projects(props: {
           <span>{data.locationDescription}</span>
           <span>{data.round}</span>
         </div>
-      ))}
-      {}
+      ))} */}
     </div>
   )
 }
 
 async function get2023Deposits(supabase: any) {
-  const { data: txns } = await supabase
+  const { data: txns, error } = await supabase
     .from('txns')
-    .select('*, projects(id, title), profiles!txns_to_id_fkey(id, full_name)')
+    .select('*, profiles!txns_to_id_fkey(id, full_name)')
     .gte('created_at', '2023-01-01')
     .lte('created_at', '2023-12-31')
     .eq('type', 'deposit')
-  return txns
+  if (error) {
+    console.error(error)
+  }
+  const { data: stripe_txns } = await supabase
+    .from('stripe_txns')
+    .select('txn_id')
+  const stripeTxnIds = stripe_txns.map((s: any) => s.txn_id)
+  const nonCreditCardTxns = txns.filter(
+    (txn: any) => !stripeTxnIds.includes(txn.id)
+  )
+  return nonCreditCardTxns as TxnAndProfiles[]
 }
 
 async function getProjectTableData(project: FullProject) {
