ade79a59a9267c6c5dac33a410b7833016fcc55e
Austin Chen <akrolsmir@gmail.com>
2024-08-26 17:18:43 -0700

    Don't send comment notifs to commenter

--- pages/api/comment-notifications.ts
+++ pages/api/comment-notifications.ts
@@ -43,52 +43,50 @@ export default async function handler(
     )
   }
 
+  const notCreatorOrCommenter = (userId: string) =>
+    userId !== fullComment.projects.creator && userId !== comment.commenter
+
   // Send follower emails
   if (!comment.replying_to) {
-    projectFollowerIds.forEach(async (followerId) => {
-      if (followerId === fullComment.projects.creator) {
-        return
-      }
-      if (comment.special_type === 'progress update') {
-        await sendTemplateEmail(
-          TEMPLATE_IDS.GENERIC_NOTIF,
-          {
-            notifText: `The creator of "${fullComment.projects.title}" has posted a progress update on their project. Check it out!`,
-            buttonUrl: `https://manifund.org/projects/${fullComment.projects.slug}?tab=comments#${comment.id}`,
-            buttonText: 'View project',
-            subject: `Manifund: Update posted for "${fullComment.projects.title}"`,
-          },
-          followerId
-        )
-      } else if (comment.special_type === 'final report') {
-        await sendTemplateEmail(
-          TEMPLATE_IDS.GENERIC_NOTIF,
-          {
-            notifText: `The creator of "${fullComment.projects.title}" has completed their project and posted a final report. Check it out!`,
-            buttonUrl: `https://manifund.org/projects/${fullComment.projects.slug}?tab=comments#${comment.id}`,
-            buttonText: 'View project',
-            subject: `Manifund: Final report posted for "${fullComment.projects.title}"`,
-          },
-          followerId
-        )
-      } else {
-        await sendTemplateEmail(
-          TEMPLATE_IDS.NEW_COMMENT,
-          postmarkVars,
-          followerId
-        )
-      }
-    })
+    projectFollowerIds
+      .filter(notCreatorOrCommenter)
+      .forEach(async (followerId) => {
+        if (comment.special_type === 'progress update') {
+          await sendTemplateEmail(
+            TEMPLATE_IDS.GENERIC_NOTIF,
+            {
+              notifText: `The creator of "${fullComment.projects.title}" has posted a progress update on their project. Check it out!`,
+              buttonUrl: `https://manifund.org/projects/${fullComment.projects.slug}?tab=comments#${comment.id}`,
+              buttonText: 'View project',
+              subject: `Manifund: Update posted for "${fullComment.projects.title}"`,
+            },
+            followerId
+          )
+        } else if (comment.special_type === 'final report') {
+          await sendTemplateEmail(
+            TEMPLATE_IDS.GENERIC_NOTIF,
+            {
+              notifText: `The creator of "${fullComment.projects.title}" has completed their project and posted a final report. Check it out!`,
+              buttonUrl: `https://manifund.org/projects/${fullComment.projects.slug}?tab=comments#${comment.id}`,
+              buttonText: 'View project',
+              subject: `Manifund: Final report posted for "${fullComment.projects.title}"`,
+            },
+            followerId
+          )
+        } else {
+          await sendTemplateEmail(
+            TEMPLATE_IDS.NEW_COMMENT,
+            postmarkVars,
+            followerId
+          )
+        }
+      })
   }
 
   // Send mentioned user emails
   await Promise.all(
     mentionedUserIds
-      .filter(
-        (userId) =>
-          userId !== fullComment.profiles.id &&
-          userId !== fullComment.projects.creator
-      )
+      .filter(notCreatorOrCommenter)
       .map((userId) =>
         sendTemplateEmail(
           TEMPLATE_IDS.COMMENT_WITH_MENTION,
@@ -102,10 +100,9 @@ export default async function handler(
   if (comment.replying_to) {
     const parentComment = await getCommentById(supabase, comment.replying_to)
     if (
-      parentComment.commenter !== fullComment.projects.creator &&
+      notCreatorOrCommenter(parentComment.commenter) &&
       !mentionedUserIds.includes(parentComment.commenter) &&
-      !projectFollowerIds.includes(parentComment.commenter) &&
-      parentComment.commenter !== comment.commenter
+      !projectFollowerIds.includes(parentComment.commenter)
     ) {
       await sendTemplateEmail(
         TEMPLATE_IDS.NEW_COMMENT,
