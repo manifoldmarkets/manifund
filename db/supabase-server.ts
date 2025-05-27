c7dc89706f0506f2627c9d0a8dc73ad2e865d54a
Nishu Singh <nishu.builder@gmail.com>
2025-05-27 16:04:52 -0700

    Only use cookies() in a server-side context; maybe resolve issue faced on windows

--- db/supabase-server.ts
+++ db/supabase-server.ts
@@ -6,11 +6,13 @@ import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import type { Database } from './database.types'
 import { SUPABASE_ANON_KEY, SUPABASE_URL } from './env'
 
-export const createServerClient = () =>
-  createServerComponentClient<Database>(
-    { cookies },
+export const createServerClient = () => {
+  const cookieStore = cookies()
+  return createServerComponentClient<Database>(
+    { cookies: () => cookieStore },
     {
       supabaseUrl: SUPABASE_URL,
       supabaseKey: SUPABASE_ANON_KEY,
     }
   )
+}
