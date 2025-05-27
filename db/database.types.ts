f50214f5f6eab3b6c954103bff5f8f585c922254
Nishu <133812901+nishu-builder@users.noreply.github.com>
2025-05-26 22:12:37 -0700

    Developer setup tidying: update README, bunlockb, and sync db schema types (#146)
    * Update README sections about development process

--- db/database.types.ts
+++ db/database.types.ts
@@ -721,13 +721,6 @@ export type Database = {
             referencedColumns: ["id"]
           },
           {
-            foreignKeyName: "stripe_txns_customer_id_fkey"
-            columns: ["customer_id"]
-            isOneToOne: false
-            referencedRelation: "users"
-            referencedColumns: ["id"]
-          },
-          {
             foreignKeyName: "stripe_txns_txn_id_fkey"
             columns: ["txn_id"]
             isOneToOne: false
@@ -813,19 +806,16 @@ export type Database = {
       }
     }
     Functions: {
-      _transfer_project:
-        | {
-            Args: {
+      _transfer_project: {
+        Args:
+          | {
               project_id: string
               to_id: string
               from_id: string
               transfer_id: string
               amount: number
             }
-            Returns: undefined
-          }
-        | {
-            Args: {
+          | {
               project_id: string
               to_id: string
               from_id: string
@@ -834,28 +824,15 @@ export type Database = {
               txn_id: string
               donor_comment_id?: string
             }
-            Returns: undefined
-          }
-        | {
-            Args: {
-              project_id: string
-              to_id: string
-              transfer_id: string
-            }
-            Returns: undefined
-          }
+          | { project_id: string; to_id: string; transfer_id: string }
+        Returns: undefined
+      }
       activate_cert: {
-        Args: {
-          project_id: string
-          project_creator: string
-        }
+        Args: { project_id: string; project_creator: string }
         Returns: undefined
       }
       activate_grant: {
-        Args: {
-          project_id: string
-          project_creator: string
-        }
+        Args: { project_id: string; project_creator: string }
         Returns: undefined
       }
       add_tags: {
@@ -875,19 +852,16 @@ export type Database = {
         }
         Returns: undefined
       }
-      execute_grant_verdict:
-        | {
-            Args: {
+      execute_grant_verdict: {
+        Args:
+          | {
               approved: boolean
               project_id: string
               project_creator: string
               admin_id?: string
               admin_comment_content?: Json
             }
-            Returns: undefined
-          }
-        | {
-            Args: {
+          | {
               approved: boolean
               project_id: string
               project_creator: string
@@ -895,13 +869,10 @@ export type Database = {
               admin_comment_content?: Json
               public_benefit?: string
             }
-            Returns: undefined
-          }
+        Returns: undefined
+      }
       follow_project: {
-        Args: {
-          project_id: string
-          follower_id: string
-        }
+        Args: { project_id: string; follower_id: string }
         Returns: undefined
       }
       get_user_balances: {
@@ -921,37 +892,27 @@ export type Database = {
         Returns: undefined
       }
       reject_grant: {
-        Args: {
-          project_id: string
-        }
+        Args: { project_id: string }
         Returns: undefined
       }
       reject_proposal: {
-        Args: {
-          project_id: string
-        }
+        Args: { project_id: string }
         Returns: undefined
       }
       toggle_follow: {
-        Args: {
-          project_id: string
-          follower_id: string
-        }
+        Args: { project_id: string; follower_id: string }
         Returns: undefined
       }
-      transfer_project:
-        | {
-            Args: {
+      transfer_project: {
+        Args:
+          | {
               project_id: string
               to_id: string
               from_id: string
               transfer_id: string
               amount: number
             }
-            Returns: undefined
-          }
-        | {
-            Args: {
+          | {
               project_id: string
               to_id: string
               from_id: string
@@ -960,10 +921,7 @@ export type Database = {
               donor_comment_id: string
               txn_id?: string
             }
-            Returns: undefined
-          }
-        | {
-            Args: {
+          | {
               project_id: string
               to_id: string
               from_id: string
@@ -971,13 +929,10 @@ export type Database = {
               amount: number
               donor_notes: Json
             }
-            Returns: undefined
-          }
+        Returns: undefined
+      }
       unfollow_project: {
-        Args: {
-          project_id: string
-          follower_id: string
-        }
+        Args: { project_id: string; follower_id: string }
         Returns: undefined
       }
     }
@@ -1051,27 +1006,29 @@ export type Database = {
   }
 }
 
-type PublicSchema = Database[Extract<keyof Database, "public">]
+type DefaultSchema = Database[Extract<keyof Database, "public">]
 
 export type Tables<
-  PublicTableNameOrOptions extends
-    | keyof (PublicSchema["Tables"] & PublicSchema["Views"])
+  DefaultSchemaTableNameOrOptions extends
+    | keyof (DefaultSchema["Tables"] & DefaultSchema["Views"])
     | { schema: keyof Database },
-  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
-    ? keyof (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
-        Database[PublicTableNameOrOptions["schema"]]["Views"])
+  TableName extends DefaultSchemaTableNameOrOptions extends {
+    schema: keyof Database
+  }
+    ? keyof (Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
+        Database[DefaultSchemaTableNameOrOptions["schema"]]["Views"])
     : never = never,
-> = PublicTableNameOrOptions extends { schema: keyof Database }
-  ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
-      Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
+> = DefaultSchemaTableNameOrOptions extends { schema: keyof Database }
+  ? (Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
+      Database[DefaultSchemaTableNameOrOptions["schema"]]["Views"])[TableName] extends {
       Row: infer R
     }
     ? R
     : never
-  : PublicTableNameOrOptions extends keyof (PublicSchema["Tables"] &
-        PublicSchema["Views"])
-    ? (PublicSchema["Tables"] &
-        PublicSchema["Views"])[PublicTableNameOrOptions] extends {
+  : DefaultSchemaTableNameOrOptions extends keyof (DefaultSchema["Tables"] &
+        DefaultSchema["Views"])
+    ? (DefaultSchema["Tables"] &
+        DefaultSchema["Views"])[DefaultSchemaTableNameOrOptions] extends {
         Row: infer R
       }
       ? R
@@ -1079,20 +1036,22 @@ export type Tables<
     : never
 
 export type TablesInsert<
-  PublicTableNameOrOptions extends
-    | keyof PublicSchema["Tables"]
+  DefaultSchemaTableNameOrOptions extends
+    | keyof DefaultSchema["Tables"]
     | { schema: keyof Database },
-  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
-    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+  TableName extends DefaultSchemaTableNameOrOptions extends {
+    schema: keyof Database
+  }
+    ? keyof Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
     : never = never,
-> = PublicTableNameOrOptions extends { schema: keyof Database }
-  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+> = DefaultSchemaTableNameOrOptions extends { schema: keyof Database }
+  ? Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"][TableName] extends {
       Insert: infer I
     }
     ? I
     : never
-  : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
-    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
+  : DefaultSchemaTableNameOrOptions extends keyof DefaultSchema["Tables"]
+    ? DefaultSchema["Tables"][DefaultSchemaTableNameOrOptions] extends {
         Insert: infer I
       }
       ? I
@@ -1100,20 +1059,22 @@ export type TablesInsert<
     : never
 
 export type TablesUpdate<
-  PublicTableNameOrOptions extends
-    | keyof PublicSchema["Tables"]
+  DefaultSchemaTableNameOrOptions extends
+    | keyof DefaultSchema["Tables"]
     | { schema: keyof Database },
-  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
-    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+  TableName extends DefaultSchemaTableNameOrOptions extends {
+    schema: keyof Database
+  }
+    ? keyof Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
     : never = never,
-> = PublicTableNameOrOptions extends { schema: keyof Database }
-  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+> = DefaultSchemaTableNameOrOptions extends { schema: keyof Database }
+  ? Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"][TableName] extends {
       Update: infer U
     }
     ? U
     : never
-  : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
-    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
+  : DefaultSchemaTableNameOrOptions extends keyof DefaultSchema["Tables"]
+    ? DefaultSchema["Tables"][DefaultSchemaTableNameOrOptions] extends {
         Update: infer U
       }
       ? U
@@ -1121,14 +1082,65 @@ export type TablesUpdate<
     : never
 
 export type Enums<
-  PublicEnumNameOrOptions extends
-    | keyof PublicSchema["Enums"]
+  DefaultSchemaEnumNameOrOptions extends
+    | keyof DefaultSchema["Enums"]
     | { schema: keyof Database },
-  EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
-    ? keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
+  EnumName extends DefaultSchemaEnumNameOrOptions extends {
+    schema: keyof Database
+  }
+    ? keyof Database[DefaultSchemaEnumNameOrOptions["schema"]]["Enums"]
     : never = never,
-> = PublicEnumNameOrOptions extends { schema: keyof Database }
-  ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
-  : PublicEnumNameOrOptions extends keyof PublicSchema["Enums"]
-    ? PublicSchema["Enums"][PublicEnumNameOrOptions]
+> = DefaultSchemaEnumNameOrOptions extends { schema: keyof Database }
+  ? Database[DefaultSchemaEnumNameOrOptions["schema"]]["Enums"][EnumName]
+  : DefaultSchemaEnumNameOrOptions extends keyof DefaultSchema["Enums"]
+    ? DefaultSchema["Enums"][DefaultSchemaEnumNameOrOptions]
     : never
+
+export type CompositeTypes<
+  PublicCompositeTypeNameOrOptions extends
+    | keyof DefaultSchema["CompositeTypes"]
+    | { schema: keyof Database },
+  CompositeTypeName extends PublicCompositeTypeNameOrOptions extends {
+    schema: keyof Database
+  }
+    ? keyof Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"]
+    : never = never,
+> = PublicCompositeTypeNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"][CompositeTypeName]
+  : PublicCompositeTypeNameOrOptions extends keyof DefaultSchema["CompositeTypes"]
+    ? DefaultSchema["CompositeTypes"][PublicCompositeTypeNameOrOptions]
+    : never
+
+export const Constants = {
+  public: {
+    Enums: {
+      bid_status: ["deleted", "pending", "accepted", "declined"],
+      bid_type: ["buy", "sell", "donate", "assurance buy", "assurance sell"],
+      comment_type: ["progress update", "final report"],
+      profile_type: ["individual", "org", "fund", "amm"],
+      project_stage: [
+        "active",
+        "proposal",
+        "not funded",
+        "complete",
+        "hidden",
+        "draft",
+      ],
+      project_type: ["grant", "cert", "dummy"],
+      txn_type: [
+        "profile donation",
+        "project donation",
+        "user to user trade",
+        "user to amm trade",
+        "withdraw",
+        "deposit",
+        "cash to charity transfer",
+        "inject amm liquidity",
+        "mint cert",
+        "mana deposit",
+        "tip",
+        "return bank funds",
+      ],
+    },
+  },
+} as const
