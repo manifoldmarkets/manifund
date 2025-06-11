3400a3b961f8ee3ec51b77d100e2b81274a4599d
Nishu Singh <nishu.builder@gmail.com>
2025-06-11 15:50:54 -0700

    Revert "remove custom edge runtime lodash loading config for admin endpoints -- dont need edge"
    This reverts commit 49db5e8a1812e3121e059bcd9463ec0a3cccfadb.

--- pages/api/add-all-followers.ts
+++ pages/api/add-all-followers.ts
@@ -2,6 +2,14 @@ import { listProjects } from '@/db/project'
 import { uniq } from 'lodash'
 import { createAdminClient } from './_db'
 
+export const config = {
+  runtime: 'edge',
+  regions: ['sfo1'],
+  unstable_allowDynamic: [
+    '**/node_modules/lodash/_root.js', // Use a glob to allow anything in the function-bind 3rd party module
+  ],
+}
+
 export default async function handler() {
   const supabase = createAdminClient()
   const projects = await listProjects(supabase)
