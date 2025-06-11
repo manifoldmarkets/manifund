49db5e8a1812e3121e059bcd9463ec0a3cccfadb
Nishu Singh <nishu.builder@gmail.com>
2025-06-11 15:31:34 -0700

    remove custom edge runtime lodash loading config for admin endpoints -- dont need edge

--- pages/api/close-grants.ts
+++ pages/api/close-grants.ts
@@ -12,14 +12,6 @@ import { checkReactivateEligible } from '@/utils/activate-project'
 import { resolveAuction } from '@/utils/resolve-auction'
 import { uniq } from 'lodash'
 
-export const config = {
-  runtime: 'edge',
-  regions: ['sfo1'],
-  unstable_allowDynamic: [
-    '**/node_modules/lodash/_root.js', // Use a glob to allow anything in the function-bind 3rd party module
-  ],
-}
-
 export default async function handler() {
   if (!isProd()) {
     return NextResponse.json('not prod')
