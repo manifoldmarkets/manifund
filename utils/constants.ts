98bf79031c47af1c0305b388a13d119bede0a8e4
Austin Chen <akrolsmir@gmail.com>
2026-01-29 09:36:19 -0800

    Revert "Reenable new signups & projects"
    This reverts commit a2daba0a4992acb0a8709006f10d475c8ffd66d4.

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,7 +1,8 @@
 export const CENTS_PER_DOLLAR = 100
 
 // Temporary flag to disable new signups and projects during spambot attack
-export const DISABLE_NEW_SIGNUPS_AND_PROJECTS = false
+// Set to false to re-enable
+export const DISABLE_NEW_SIGNUPS_AND_PROJECTS = true
 export const SIGNUP_DISABLED_MESSAGE =
   'New projects and accounts disabled as we deal with spambots; contact austin@manifund.org if you have questions.'
 
