bc7e514ae919037bcbd109f4ce97ad68dff959ca
Austin Chen <akrolsmir@gmail.com>
2026-04-20 11:09:27 -0700

    Add /essay redirect

--- next.config.js
+++ next.config.js
@@ -29,6 +29,16 @@ const nextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/essay',
+        destination:
+          'https://manifoldmarkets.notion.site/Manifund-Essay-Prize-34354492ea7a804dbb44dc4fee8cf82f?source=copy_link',
+        permanent: false,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
